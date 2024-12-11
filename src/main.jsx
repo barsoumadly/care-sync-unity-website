043364d37b31a90f68067fda53895ef0462e89b1
bishoy labib <bishoylabib78@gmail.com>
2024-12-11 15:28:47 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/styles/style.css";
 import "../public/styles/feather.css";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
