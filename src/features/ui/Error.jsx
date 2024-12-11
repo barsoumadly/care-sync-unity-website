c6ba6c41b2b300f0ae46ec52ed20c47026168b39
bishoy labib <bishoylabib78@gmail.com>
2024-12-11 12:39:32 +0200

    error.jsx

---
+++ src/features/ui/Error.jsx
@@ -0,0 +1,33 @@
+import { Link, useRouteError } from "react-router-dom";
+
+function Error() {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div className="error-pages">
+      <div className="main-wrapper error-wrapper">
+        <div className="error-box">
+          <img
+            className="img-fluid"
+            src="/images/error/error-01.png"
+            alt="Logo"
+          />
+          <h3>
+            <img
+              className="img-fluid mb-0"
+              src="/images/error/icons/danger.svg"
+              alt="Logo"
+            />
+            {error.statusText}
+          </h3>
+          <p>You may have mistyped the address or the page may have moved.</p>
+          <Link to="/" className="btn btn-primary go-home">
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
