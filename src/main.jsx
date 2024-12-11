406fd414c01dddcfaa10e360e2cc9620521d63b5
Barsoum Adly <barsoumadly@gmail.com>
2024-12-11 13:21:54 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "../public/styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../public/styles/style.css";
+import "../public/styles/feather.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
