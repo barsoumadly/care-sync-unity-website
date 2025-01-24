6a16613ab08120c3e2b9d597fed9b8c23de204f9
bishoy labib <bishoylabib78@gmail.com>
2025-01-24 19:53:35 +0200

    DashboardLayout.jsx

--- src/ui/DashboardLayout.jsx
+++ src/ui/DashboardLayout.jsx
@@ -1,14 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
+import Loader from "../ui/Loader";
+import { useState } from "react";
 
 function DashboardLayout() {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => setIsLoading(false), 2000);
   return (
     <div>
-      <Header />
-
-      <main>
-        <Outlet />
-      </main>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+        </>
+      )}{" "}
     </div>
   );
 }
