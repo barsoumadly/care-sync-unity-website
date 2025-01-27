042c9b7fbac4c232c3cbd4b001430ac413f91490
Barsoum Adly <barsoumadly@gmail.com>
2025-01-27 21:09:49 +0200

    main

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,8 @@ import "./styles/feather.css";
 
 import App from "./App.jsx";
 
-import { Provider } from "react-redux";
-import store from "./store.js";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
