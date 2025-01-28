f79107b3c67b91c5d678c01c75d08bd127ffe5cc
bishoy labib <bishoylabib78@gmail.com>
2025-01-28 12:55:59 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,34 @@ import Home from "./ui/Home";
 
 import Register from "./pages/Register";
 import Login from "./pages/login";
-
+/* Clinic */
 import ClinicLayout from "./features/dashboard/clinic/ClinicLayout";
 import ClinicDashboard from "./features/dashboard/clinic/ClinicDashboard";
-import Doctors from "./features/dashboard/clinic/Doctors";
-import Patients from "./features/dashboard/clinic/Patients";
-import Staff from "./features/dashboard/clinic/Staff";
-import Appointments from "./features/dashboard/clinic/Appointments";
-import DoctorSchedule from "./features/dashboard/clinic/DoctorSchedule";
+import DoctorList from "./features/dashboard/clinic/DoctorList";
+import AddDoctor from "./features/dashboard/clinic/AddDoctor";
+import PatientsList from "./features/dashboard/clinic/PatientsList";
+import AddPatients from "./features/dashboard/clinic/AddPatients";
+import Payments from "./features/dashboard/clinic/Payments";
+import EmployeeSalary from "./features/dashboard/clinic/EmployeeSalary";
+import AddStaff from "./features/dashboard/clinic/AddStaff";
+import Attendance from "./features/dashboard/clinic/Attendance";
+import AppointmentList from "./features/dashboard/clinic/AppointmentList";
+import BookAppointment from "./features/dashboard/clinic/BookAppointment";
+import EditAppointments from "./features/dashboard/clinic/EditAppointments";
+import ScheduleList from "./features/dashboard/clinic/ScheduleList";
+import AddSchedule from "./features/dashboard/clinic/AddSchedule";
+import EditSchedule from "./features/dashboard/clinic/EditSchedule";
 import Chat from "./features/dashboard/clinic/Chat";
 import Reports from "./features/dashboard/clinic/Reports";
 import Invoice from "./features/dashboard/clinic/Invoice";
-
+/* Doctor */
 import DoctorLayout from "./features/dashboard/doctor/DoctorLayout";
 import DoctorDashboard from "./features/dashboard/doctor/DoctorDashboard";
 import DoctorPatientsList from "./features/dashboard/doctor/Patients";
 import DoctorChat from "./features/dashboard/doctor/Chat";
 import MyProfile from "./features/dashboard/doctor/MyProfile";
 import EditProfile from "./features/dashboard/doctor/EditProfile";
-
+/* Patient */
 import PatientLayout from "./features/dashboard/patient/PatientLayout";
 import PatientDashboard from "./features/dashboard/patient/PatientDashboard";
 import ClinicsList from "./features/dashboard/patient/ClinicsList";
@@ -55,24 +64,24 @@ const router = createBrowserRouter([
     children: [
       { path: "/clinic/dashboard", element: <ClinicDashboard /> },
       /* Doctors */
-      { path: "/clinic/doctor-list", element: <Doctors /> },
-      { path: "/clinic/add-doctor", element: <Doctors /> },
+      { path: "/clinic/doctor-list", element: <DoctorList /> },
+      { path: "/clinic/add-doctor", element: <AddDoctor /> },
       /* Patients */
-      { path: "/clinic/patients-list", element: <Patients /> },
-      { path: "/clinic/add-patients", element: <Patients /> },
-      { path: "/clinic/payments", element: <Patients /> },
+      { path: "/clinic/patients-list", element: <PatientsList /> },
+      { path: "/clinic/add-patients", element: <AddPatients /> },
+      { path: "/clinic/payments", element: <Payments /> },
       /* Staff */
-      { path: "/clinic/employee-salary", element: <Staff /> },
-      { path: "/clinic/add-staff", element: <Staff /> },
-      { path: "/clinic/attendance", element: <Staff /> },
+      { path: "/clinic/employee-salary", element: <EmployeeSalary /> },
+      { path: "/clinic/add-staff", element: <AddStaff /> },
+      { path: "/clinic/attendance", element: <Attendance /> },
       /* Appointments */
-      { path: "/clinic/appointment-list", element: <Appointments /> },
-      { path: "/clinic/book-appointment", element: <Appointments /> },
-      { path: "/clinic/edit-appointment", element: <Appointments /> },
+      { path: "/clinic/appointment-list", element: <AppointmentList /> },
+      { path: "/clinic/book-appointment", element: <BookAppointment /> },
+      { path: "/clinic/edit-appointment", element: <EditAppointments /> },
       /* Doctor Schedule */
-      { path: "/clinic/schedule-list", element: <DoctorSchedule /> },
-      { path: "/clinic/add-schedule", element: <DoctorSchedule /> },
-      { path: "/clinic/edit-schedule", element: <DoctorSchedule /> },
+      { path: "/clinic/schedule-list", element: <ScheduleList /> },
+      { path: "/clinic/add-schedule", element: <AddSchedule /> },
+      { path: "/clinic/edit-schedule", element: <EditSchedule /> },
 
       { path: "/clinic/chat", element: <Chat /> },
       { path: "/clinic/reports", element: <Reports /> },
