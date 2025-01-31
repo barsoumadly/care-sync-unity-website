472b3af98d29871aacee530175cbc13cef82decc
bishoy labib <bishoylabib78@gmail.com>
2025-01-31 16:50:41 +0200

    UserAvatar

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,13 +1,19 @@
-function UserAvatar() {
-  const user = "Liam Michael";
-  const type = "Doctor";
-  const avatar = "/images/dashborad/user-06.jpg";
+import { useUserContext } from "../../context/UserContextProvider";
 
+function UserAvatar() {
+  const { user } = useUserContext();
+  const avatar = "/images/dashborad/user-06.jpg";
+  if (user === null) return;
+  else {
+    var { name, role } = user;
+    name = name.split(" ").slice(0, 2).join(" ");
+    role = role.split("_").join(" ");
+  }
   return (
     <>
       <div className="user-names">
-        <h5>{user} </h5>
-        <span>{type}</span>
+        <h5>{name}</h5>
+        <span>{role}</span>
       </div>
       <span className="user-img">
         <img src={avatar} alt="Admin" />
