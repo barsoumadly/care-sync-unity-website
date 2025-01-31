8d46d25584c84d2ba68814c7b97620cc6b437050
bishoy labib <bishoylabib78@gmail.com>
2025-01-31 16:58:57 +0200

    UserAvatar

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,14 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../context/UserContextProvider";
+import { useEffect } from "react";
 
 function UserAvatar() {
   const { user } = useUserContext();
   const avatar = "/images/dashborad/user-06.jpg";
-  if (user === null) return;
-  else {
+  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (user === null) {
+        navigate("/login");
+      }
+    },
+    [user, navigate]
+  );
+
+  if (user !== null) {
     var { name, role } = user;
     name = name.split(" ").slice(0, 2).join(" ");
     role = role.split("_").join(" ");
   }
+
   return (
     <>
       <div className="user-names">
