4e515a1620b45672f29f82c34b6806ace38043f6
bishoy labib <bishoylabib78@gmail.com>
2025-01-31 18:01:26 +0200

    ClinicLayout

--- src/features/dashboard/clinic/ClinicLayout.jsx
+++ src/features/dashboard/clinic/ClinicLayout.jsx
@@ -2,35 +2,26 @@ import { useState } from "react";
 
 import Header from "../../../ui/Header";
 import Sidebar from "../../../ui/Sidebar";
-import Loader from "../../../ui/Loader";
 import ClinicSidebar from "./ClinicSidebar";
 import { Outlet } from "react-router-dom";
 
 function DashboardLayout() {
-  const [isLoading, setIsLoading] = useState(true);
   const [showbar, setShowbar] = useState(true);
   const [minbar, setMinbar] = useState(false);
 
-  setTimeout(() => setIsLoading(false), 2000);
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Header
-            setShowbar={setShowbar}
-            setMinbar={setMinbar}
-            url={"/clinic/dashboard"}
-          />
-          <Sidebar minbar={minbar} showbar={showbar}>
-            <ClinicSidebar minbar={minbar} />
-          </Sidebar>
-          <main>
-            <Outlet />
-          </main>
-        </>
-      )}
+      <Header
+        setShowbar={setShowbar}
+        setMinbar={setMinbar}
+        url={"/clinic/dashboard"}
+      />
+      <Sidebar minbar={minbar} showbar={showbar}>
+        <ClinicSidebar minbar={minbar} />
+      </Sidebar>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
