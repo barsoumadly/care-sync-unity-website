7fdd1c213c638971b4ddab67d62f3da26ce016a5
bishoy labib <bishoylabib78@gmail.com>
2025-01-31 18:01:46 +0200

    PatientLayout

--- src/features/dashboard/patient/PatientLayout.jsx
+++ src/features/dashboard/patient/PatientLayout.jsx
@@ -2,35 +2,25 @@ import { useState } from "react";
 
 import Header from "../../../ui/Header";
 import Sidebar from "../../../ui/Sidebar";
-import Loader from "../../../ui/Loader";
 import { Outlet } from "react-router-dom";
 import PatientSidebar from "./PatientSidebar";
 
 function DashboardLayout() {
-  const [isLoading, setIsLoading] = useState(true);
   const [showbar, setShowbar] = useState(true);
   const [minbar, setMinbar] = useState(false);
-
-  setTimeout(() => setIsLoading(false), 2000);
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Header
-            setShowbar={setShowbar}
-            setMinbar={setMinbar}
-            url={"/patient/dashboard"}
-          />
-          <Sidebar minbar={minbar} showbar={showbar}>
-            <PatientSidebar />
-          </Sidebar>
-          <main>
-            <Outlet />
-          </main>
-        </>
-      )}
+      <Header
+        setShowbar={setShowbar}
+        setMinbar={setMinbar}
+        url={"/patient/dashboard"}
+      />
+      <Sidebar minbar={minbar} showbar={showbar}>
+        <PatientSidebar />
+      </Sidebar>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
