62159be94b2790b8790c2f51627f0a528ce78eb0
bishoy labib <bishoylabib78@gmail.com>
2025-01-31 18:01:35 +0200

    Doctor

--- src/features/dashboard/doctor/DoctorLayout.jsx
+++ src/features/dashboard/doctor/DoctorLayout.jsx
@@ -2,35 +2,26 @@ import { useState } from "react";
 
 import Header from "../../../ui/Header";
 import Sidebar from "../../../ui/Sidebar";
-import Loader from "../../../ui/Loader";
 import { Outlet } from "react-router-dom";
 import DoctorSidebar from "./DoctorSidebar";
 
 function DashboardLayout() {
-  const [isLoading, setIsLoading] = useState(true);
   const [showbar, setShowbar] = useState(true);
   const [minbar, setMinbar] = useState(false);
 
-  setTimeout(() => setIsLoading(false), 2000);
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Header
-            setShowbar={setShowbar}
-            setMinbar={setMinbar}
-            url={"/doctor/dashboard"}
-          />
-          <Sidebar minbar={minbar} showbar={showbar}>
-            <DoctorSidebar />
-          </Sidebar>
-          <main>
-            <Outlet />
-          </main>
-        </>
-      )}
+      <Header
+        setShowbar={setShowbar}
+        setMinbar={setMinbar}
+        url={"/doctor/dashboard"}
+      />
+      <Sidebar minbar={minbar} showbar={showbar}>
+        <DoctorSidebar />
+      </Sidebar>
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
