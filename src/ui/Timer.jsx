150c5233b8abb0017e4052fee084936be4c1bd5e
bishoy labib <bishoylabib78@gmail.com>
2025-02-03 00:29:10 +0200

    Timer

---
+++ src/ui/Timer.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function Timer({ setStartCounter }) {
+  const [time, setTime] = useState(5);
+  useEffect(
+    function () {
+      const OTPTimer = setInterval(() => {
+        if (time > 0) {
+          setTime((time) => time - 1);
+        } else {
+          setStartCounter(false);
+          setTime(0);
+          return;
+        }
+      }, 1000);
+      return () => clearInterval(OTPTimer);
+    },
+    [time, setTime, setStartCounter]
+  );
+  return time;
+}
+
+export default Timer;
