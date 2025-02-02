7a4c1a69094e2e2c92e7569735ad80be35b5a8f2
bishoy labib <bishoylabib78@gmail.com>
2025-02-02 17:22:09 +0200

    SinnerMini

---
+++ src/ui/SpinnerMini.jsx
@@ -0,0 +1,11 @@
+import { BiLoaderAlt } from "react-icons/bi";
+
+function SpinnerMini() {
+  return (
+    <div className="spinnerMiniStyle">
+      <BiLoaderAlt />
+    </div>
+  );
+}
+
+export default SpinnerMini;
