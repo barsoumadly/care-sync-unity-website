087e5b5f0c05cf0d16deb5ba92bbe1bc42b361a8
bishoy labib <bishoylabib78@gmail.com>
2025-02-05 12:15:45 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
-import { UserContextProvider } from "./context/UserContextProvider";
+import { UserContextProvider } from "./context/AuthContext";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import ProtectedAuth from "./ui/ProtectedAuth";
 
