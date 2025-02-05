c1fd196f92a16150270b79515162076b15afe35e
bishoy labib <bishoylabib78@gmail.com>
2025-02-05 13:28:51 +0200

    ProtectedRoute

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,23 +1,14 @@
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Loader from "./Loader";
+import { LocalStorageState } from "../hooks/LocalStorageState";
 
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
 
   setTimeout(() => setIsLoading(false), 1000);
-
-  useEffect(
-    function () {
-      if (isLoading && !isAuthenticated) {
-        navigate("/login");
-      }
-    },
-    [isLoading, isAuthenticated, navigate]
-  );
+  LocalStorageState();
 
   if (isLoading) return <Loader />;
   if (isAuthenticated) return children;
