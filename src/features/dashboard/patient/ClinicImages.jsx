e74e71935cc8963e71ebca0eb34538c63ef135e3
Barsoum Adly <barsoumadly@gmail.com>
2025-02-07 18:19:14 +0200

    Clinic Images

---
+++ src/features/dashboard/patient/ClinicImages.jsx
@@ -0,0 +1,56 @@
+import ClinicHeader from "./ClinicHeader";
+
+function ClinicImages() {
+  return (
+    <div className="main-wrapper">
+      <div className="page-wrapper" style={{ minHeight: 270 }}>
+        <div className="content">
+          <ClinicHeader activeTab="images" headerText="Images" />
+          {/* Page Header */}
+          <div className="page-header"></div>
+          {/* /Page Header */}
+          <div id="lightgallery" className="row">
+            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
+              <a href="assets/img/blog/blog-01.jpg">
+                <img
+                  className="img-thumbnail"
+                  src="/images/clinic/image1.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
+              <a href="assets/img/blog/blog-02.jpg">
+                <img
+                  className="img-thumbnail"
+                  src="/images/clinic/image2.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
+              <a href="assets/img/blog/blog-03.jpg">
+                <img
+                  className="img-thumbnail"
+                  src="/images/clinic/image3.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
+              <a href="assets/img/blog/blog-04.jpg">
+                <img
+                  className="img-thumbnail"
+                  src="/images/clinic/image4.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClinicImages;
