3ca107399445cf31c339b3d05dbee4e1e18de099
Kerolos <kerolos.saaid@gmail.com>
2025-02-10 01:36:38 +0200

    feat: configure HTTPS for Vite development server with SSL certificates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:  {
+    https:{
+      key:'./cert.key',
+      cert:'cert.crt',
+    }
+  },
   plugins: [react()],
 })
