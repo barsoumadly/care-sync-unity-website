d1e7aefa5824eec0e065a7b1a329a658a7b8e859
Kerolos <kerolos.saaid@gmail.com>
2025-02-10 01:53:49 +0200

    feat: configure vite server for both HTTP and HTTPS with conditional SSL certificate usage

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:  {
-    https:{
-      key:'./cert.key',
-      cert:'cert.crt',
-    }
+  preview: {
+    port: 443,
+    host: true, // Listen on all addresses
+    https:
+      fs.existsSync("./cert.key") && fs.existsSync("./cert.crt")
+        ? {
+            key: "./cert.key",
+            cert: "./cert.crt",
+          }
+        : false,
   },
   plugins: [react()],
-})
+});
