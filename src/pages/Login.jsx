86593f89bc40aba42f6faf2e2e867d69fbd17dac
bishoy labib <bishoylabib78@gmail.com>
2025-02-12 20:18:42 +0200

    Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,10 +5,11 @@ import { login, requestEmailVerification } from "../services/auth";
 import { useAuth } from "../context/AuthContext";
 import AuthButton from "../ui/AuthButton";
 import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
 
 function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit, reset } = useForm();
+
   const [isEyeOpen, setIsEyeOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const { userLogin } = useAuth();
@@ -44,24 +45,20 @@ function Login() {
     setIsLogin(false);
   };
 
-  const handleSubmit = async function (event) {
-    event.preventDefault();
-    setEmail("");
-    setPassword("");
+  const onSubmit = async function (data) {
     setIsLoading(true);
 
-    const userCredentials = { email, password };
     try {
       if (isLogin) {
-        const response = await login(userCredentials);
+        const response = await login(data);
         userLogin(response?.data.user);
         navigateUser(response?.data.user.role);
         toast(`Welcome ${response.data.user.name}`, {
           icon: "👋",
         });
       } else {
-        const user = { email };
-        userLogin(user);
+        const email = data?.email;
+        userLogin({ email });
         await requestEmailVerification({ email });
         navigate("/verify-email");
         toast.success("OTP Code is sent");
@@ -75,6 +72,7 @@ function Login() {
       toast.error(error.response.data.message);
     } finally {
       setIsLoading(false);
+      reset();
     }
   };
 
@@ -87,7 +85,7 @@ function Login() {
       <h2>{isLogin ? "Login" : "Verify Email"}</h2>
 
       {/* <!-- Form --> */}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="input-block">
           <label style={{ background: `${isLoading ? "none" : "#ffffff"}` }}>
             Email <span className="login-danger">*</span>
@@ -95,10 +93,9 @@ function Login() {
           <input
             className="form-control"
             type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
             required
             disabled={isLoading}
+            {...register("email")}
           />
         </div>
         {isLogin && (
@@ -112,10 +109,9 @@ function Login() {
               <input
                 className="form-control pass-input"
                 type={`${isEyeOpen ? "text" : "password"}`}
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
                 required
                 disabled={isLoading}
+                {...register("password")}
               />
               <PasswordEye
                 isEyeOpen={isEyeOpen}
