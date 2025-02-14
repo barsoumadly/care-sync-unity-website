9305d6b75c62a9ef5a440a7569896e2b6d391a59
Barsoum Adly <barsoumadly@gmail.com>
2025-02-14 22:42:39 +0200

    local storage

--- src/hooks/LocalStorageState.js
+++ src/hooks/LocalStorageState.js
@@ -1,16 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { getUserById } from "../services/auth";
 
 export function LocalStorageState() {
   const navigate = useNavigate();
   const { userLogin } = useAuth();
   useEffect(() => {
+    const getUser = async function (isAuth) {
+      try {
+        const response = await getUserById(isAuth);
+        userLogin(response.data.user, isAuth);
+      } catch (error) {
+        navigate("/internal-server-error");
+      }
+    };
+
     let isAuth = JSON.parse(localStorage.getItem("key"));
     if (isAuth === null) {
       navigate("/login");
     } else {
-      userLogin(isAuth);
+      getUser(isAuth);
     }
   }, []);
 }
