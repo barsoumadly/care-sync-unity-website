07b192e437ae45336ca9cc1e98366d24b0107f76
Barsoum Adly <barsoumadly@gmail.com>
2025-02-21 20:44:29 +0200

    inactive start

---
+++ src/ui/InactiveStar.jsx
@@ -0,0 +1,14 @@
+function InactiveStar() {
+  return (
+    <>
+      <img
+        alt={4}
+        src="https://preclinic.dreamstechnologies.com/html/template/assets/plugins/raty/images/star-off.png"
+        title="good"
+      />
+      &nbsp;
+    </>
+  );
+}
+
+export default InactiveStar;
