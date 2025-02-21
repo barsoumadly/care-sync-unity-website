4e032a3f96d6dce8ea68208a9aa4756ee3603890
Barsoum Adly <barsoumadly@gmail.com>
2025-02-21 13:59:16 +0200

    laboratory list

---
+++ src/features/dashboard/patient/laboratories/LaboratoryList.jsx
@@ -0,0 +1,85 @@
+import LaboratoryCard from "./LaboratoryCard";
+
+function LaboratoryList() {
+  return (
+    <div className="page-wrapper" style={{ minHeight: 270 }}>
+      <div className="content container-fluid">
+        {/* Page Header */}
+        <div className="page-header">
+          <div className="row">
+            <div className="col-sm-12">
+              <ul className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <a>Laboratories </a>
+                </li>
+                <li className="breadcrumb-item">
+                  <i className="feather-chevron-right" />
+                </li>
+                <li className="breadcrumb-item active">Laboratory List</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* /Page Header */}
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="card card-table show-entire">
+              <div className="card-body">
+                {/* /Table Header */}
+                <div className="staff-search-table">
+                  <form>
+                    <div className="row">
+                      <div className="col-12 col-md-6 col-xl-3">
+                        <div className="input-block local-forms">
+                          <label>Laboratory Name </label>
+                          <input
+                            className="form-control"
+                            type="text"
+                            placeholder="ex. Alfa"
+                          />
+                        </div>
+                      </div>
+                      <div className="col-12 col-md-6 col-xl-3">
+                        <div className="input-block local-forms">
+                          <label>City </label>
+                          <select className="form-control pass-input">
+                            <option value="" disabled selected>
+                              Select your city
+                            </option>
+                            <option value="cairo">Cairo</option>
+                            <option value="giza">Giza</option>
+                            <option value="alex">Alexandria</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-12 col-md-6 col-xl-3">
+                        <div className="doctor-submit">
+                          <button
+                            type="submit"
+                            className="btn btn-primary submit-list-form me-2"
+                          >
+                            Search
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="row">
+                  {/* Cards */}
+                  <LaboratoryCard
+                    laboratoryName="Alfa Laboratory"
+                    to="laboratory-profile"
+                  />
+                </div>
+                {/* /Cards */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LaboratoryList;
