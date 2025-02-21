27bf26dc8124c7dc6f3feb2bf3104386c43320f2
Barsoum Adly <barsoumadly@gmail.com>
2025-02-21 20:44:36 +0200

    semi active star

---
+++ src/ui/SemiactiveStar.jsx
@@ -0,0 +1,14 @@
+function SemiactiveStar() {
+  return (
+    <>
+      <img
+        alt={4}
+        src="https://preclinic.dreamstechnologies.com/html/template/assets/plugins/raty/images/star-half.png"
+        title="good"
+      />
+      &nbsp;
+    </>
+  );
+}
+
+export default SemiactiveStar;
