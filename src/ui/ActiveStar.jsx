216d07f76cf5119f4d876096b248bb05f2449cb1
Barsoum Adly <barsoumadly@gmail.com>
2025-02-21 20:44:22 +0200

    active star

---
+++ src/ui/ActiveStar.jsx
@@ -0,0 +1,14 @@
+function ActiveStar() {
+  return (
+    <>
+      <img
+        alt={1}
+        src="https://preclinic.dreamstechnologies.com/html/template/assets/plugins/raty/images/star-on.png"
+        title="bad"
+      />
+      &nbsp;
+    </>
+  );
+}
+
+export default ActiveStar;
