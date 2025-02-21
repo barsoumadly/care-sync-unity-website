ed068979a5c5836fca220eb0371e43db7c05ccd0
Barsoum Adly <barsoumadly@gmail.com>
2025-02-21 17:09:51 +0200

    clinic card

---
+++ src/features/dashboard/patient/clinics/ClinicCard.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../../context/AuthContext";
+
+function ClinicCard({ clinic }) {
+  const saveActiveClinic = function (clinic) {
+    localStorage.setItem("clinic", JSON.stringify(clinic));
+  };
+
+  const { user } = useAuth();
+  return (
+    <div className="col-12 col-md-4 col-lg-3 d-flex">
+      <div
+        className="card flex-fill bg-white"
+        style={{
+          boxShadow:
+            " rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+          margin: "5%",
+        }}
+      >
+        <img
+          alt="Card Image"
+          src={clinic.profilePhoto}
+          className="card-img-top"
+          style={{ width: "260px", height: "174px" }}
+        />
+        <div className="card-header" style={{ padding: "5%" }}>
+          <h5 className="card-title mb-0" style={{ color: "#2e37a4" }}>
+            <strong>{clinic.name}</strong>
+          </h5>
+        </div>
+        <div className="card-body card-buttons" style={{ padding: "5%" }}>
+          <p className="card-text">
+            <span style={{ fontWeight: "600" }}>Our Specialization: </span>
+            {clinic.specialization.join(", ")}
+          </p>
+          <Link
+            to={`/patient/clinics/${clinic.slug}`}
+            className="btn btn-primary"
+            onClick={() => saveActiveClinic(clinic)}
+          >
+            View {user.role === "DOCTOR" ? "Patients" : "Profile"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ClinicCard;
