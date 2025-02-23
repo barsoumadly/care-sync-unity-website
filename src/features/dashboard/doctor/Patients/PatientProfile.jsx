461771512aa615753e91acccc5e98175686f2ae1
bishoy labib <bishoylabib78@gmail.com>
2025-02-23 18:53:06 +0200

    PatientProfile

--- src/features/dashboard/doctor/Patients/PatientProfile.jsx
+++ src/features/dashboard/doctor/Patients/PatientProfile.jsx
@@ -15,77 +15,12 @@ function PatientProfile() {
             ]}
             currentPage="Patient Profile"
           />
+
           {/* /Page Header */}
           <div className="row">
             <div className="col-sm-12">
-              <div className="card">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="about-info">
-                        <h4>
-                          Patients Profile{" "}
-                          <span>
-                            <a>
-                              <i className="feather-more-vertical" />
-                            </a>
-                          </span>
-                        </h4>
-                      </div>
-                      <div className="doctor-profile-head">
-                        <div className="row">
-                          <div className="col-lg-6 col-xl-4 col-md-4">
-                            <div className="profile-user-box">
-                              <div className="profile-user-img">
-                                <img
-                                  src="/images/dashborad/profiles/avatar-01.jpg"
-                                  alt="Profile"
-                                />
-                              </div>
-                              <div className="names-profiles">
-                                <h4>Smith Bruklin</h4>
-                                <h5>Engineer</h5>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-lg-4 col-md-4 d-flex align-items-center">
-                            <div className="follow-group">
-                              <div className="doctor-follows">
-                                <h5>Followers</h5>
-                                <h4>850</h4>
-                              </div>
-                              <div className="doctor-follows">
-                                <h5>Following</h5>
-                                <h4>18K</h4>
-                              </div>
-                              <div className="doctor-follows">
-                                <h5>Posts</h5>
-                                <h4>250</h4>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="col-lg-6 col-xl-4 d-flex align-items-center">
-                            <div className="follow-btn-group py-3">
-                              <button
-                                type="submit"
-                                className="btn btn-info follow-btns"
-                              >
-                                Follow
-                              </button>
-                              <button
-                                type="submit"
-                                className="btn btn-info message-btns"
-                              >
-                                Message
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <ProfileHeader />
+
               <div className="row">
                 <div className="col-lg-4">
                   <div className="doctor-personals-grp">
@@ -699,3 +634,64 @@ function PatientProfile() {
 }
 
 export default PatientProfile;
+
+function ProfileHeader() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="about-info">
+              <h4>
+                Patients Profile{" "}
+                <span>
+                  <a>
+                    <i className="feather-more-vertical" />
+                  </a>
+                </span>
+              </h4>
+            </div>
+            <div className="doctor-profile-head">
+              <div className="row">
+                <div className="col-lg-6 col-xl-4 col-md-4">
+                  <div className="profile-user-box">
+                    <div className="names-profiles">
+                      <h4>Smith Bruklin</h4>
+                      <h5>Engineer</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-4 d-flex align-items-center">
+                  <div className="follow-group">
+                    <div className="doctor-follows">
+                      <h5>Gender</h5>
+                      <h4>Female</h4>
+                    </div>
+                    <div className="doctor-follows">
+                      <h5>Age</h5>
+                      <h4>18</h4>
+                    </div>
+                    <div className="doctor-follows">
+                      <h5>Posts</h5>
+                      <h4>250</h4>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-xl-4 d-flex align-items-center">
+                  <div className="follow-btn-group py-3">
+                    <button type="submit" className="btn btn-info follow-btns">
+                      Follow
+                    </button>
+                    <button type="submit" className="btn btn-info message-btns">
+                      Message
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
