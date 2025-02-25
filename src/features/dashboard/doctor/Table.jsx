774e3ede1d7878a9c5b2ca9e93a335f02b054045
bishoy labib <bishoylabib78@gmail.com>
2025-02-25 14:51:13 +0200

    table

--- src/features/dashboard/doctor/Table.jsx
+++ src/features/dashboard/doctor/Table.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
-function Table({ headElements, bodyElements, button }) {
+function Table({ headElements, bodyElements, button, status }) {
   return (
     <div className="table-responsive">
       <table className="table border-0 custom-table comman-table datatable mb-0">
         <TableHeader headElements={headElements} />
-        <TableBody bodyElements={bodyElements} button={button} />
+        <TableBody
+          bodyElements={bodyElements}
+          button={button}
+          status={status}
+        />
       </table>
     </div>
   );
@@ -25,33 +29,57 @@ function TableHeader({ headElements }) {
   );
 }
 
-function TableBody({ bodyElements, button }) {
+function TableBody({ bodyElements, button, status }) {
   return (
     <tbody>
       {bodyElements.map((element, index) => (
         <tr key={index}>
-          <td>{index + 1}</td>
-
-          {element.map((row, i) => (
-            <td key={i}>{row}</td>
-          ))}
-
-          {button ? (
-            <td>
-              <div className="dropdown action-label">
-                <Link
-                  to={button.link}
-                  style={{ marginTop: "3%", color: "#fff" }}
-                >
-                  <button className="custom-badge book-btn">
-                    {button.name}
-                  </button>
-                </Link>
-              </div>
-            </td>
-          ) : null}
+          <TableRow
+            num={index}
+            element={element}
+            button={button}
+            status={status}
+          />
         </tr>
       ))}
     </tbody>
+  );
+}
+
+function TableRow({ num, element, button = 0, status = 0 }) {
+  return (
+    <>
+      {/* Counter */}
+      <td style={{ padding: "0 30px" }}>{num + 1}</td>
+
+      {/* Data */}
+      {element.map((row, i) => (
+        <>
+          <td key={i}>{row}</td>
+        </>
+      ))}
+
+      {/* Status */}
+      {status[num] ? (
+        <td>
+          <button className={`custom-badge status-${status[num].color} `}>
+            {status[num].name}
+          </button>
+        </td>
+      ) : (
+        <td></td>
+      )}
+
+      {/* Button */}
+      {button ? (
+        <td>
+          <div className="dropdown action-label">
+            <Link to={button.link} style={{ marginTop: "3%", color: "#fff" }}>
+              <button className="custom-badge book-btn">{button.name}</button>
+            </Link>
+          </div>
+        </td>
+      ) : null}
+    </>
   );
 }
