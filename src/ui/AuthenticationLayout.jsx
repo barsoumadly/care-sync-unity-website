f18518c3f228e9ecb0495d0bed10f3d6e5ec00c7
Barsoum Adly <barsoumadly@gmail.com>
2025-02-26 13:28:05 +0200

    authentication layout

--- src/ui/AuthenticationLayout.jsx
+++ src/ui/AuthenticationLayout.jsx
@@ -1,7 +1,28 @@
 import { Outlet } from "react-router-dom";
 import AuthenticationFrame from "../features/authentication/AuthenticationFrame";
 import SkipLogin from "../features/authentication/SkipLogin";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { getUserById } from "../services/auth";
+
 function AuthenticationLayout() {
+  const { userLogin } = useAuth();
+  useEffect(() => {
+    const getUser = async function (isAuth) {
+      try {
+        const response = await getUserById(isAuth);
+        userLogin(response.data.user, isAuth);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    let isAuth = JSON.parse(localStorage.getItem("key"));
+    if (isAuth !== null) {
+      getUser(isAuth);
+    }
+  }, []);
+
   return (
     <SkipLogin>
       <AuthenticationFrame>
