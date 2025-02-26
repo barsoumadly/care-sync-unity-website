607a0a78ff4a842c3ff166f026c4eff99187648f
bishoy labib <bishoylabib78@gmail.com>
2025-02-26 12:12:16 +0200

    Home

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,27 @@
 import { Link } from "react-router-dom";
+import { LocalStorageState } from "../hooks/LocalStorageState";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { getUserById } from "../services/auth";
 
 function Home() {
+  const { userLogin } = useAuth();
+  useEffect(() => {
+    const getUser = async function (isAuth) {
+      try {
+        const response = await getUserById(isAuth);
+        userLogin(response.data.user, isAuth);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    let isAuth = JSON.parse(localStorage.getItem("key"));
+    if (isAuth !== null) {
+      getUser(isAuth);
+    }
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
