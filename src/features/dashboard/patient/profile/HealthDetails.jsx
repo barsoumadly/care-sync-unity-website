ef615cbf71761c6edc8ee65136d8732f31a11b6a
Barsoum Adly <barsoumadly@gmail.com>
2025-02-27 16:50:04 +0200

    health details

---
+++ src/features/dashboard/patient/profile/HealthDetails.jsx
@@ -0,0 +1,144 @@
+function HealthDetails({
+  temperature,
+  weigh,
+  height,
+  heartRate,
+  bloodType,
+  bloodSugar,
+  bloodPressure,
+  handleTemperature,
+  handleWeigh,
+  handleHeight,
+  handleHeartRate,
+  handleBloodType,
+  handleBloodSugar,
+  handleBloodPressure,
+}) {
+  return (
+    <>
+      <div className="row">
+        <div className="col-12">
+          <div className="form-heading">
+            <h4>Health Details (Optional)</h4>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Temperature <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="ex: 37 °C"
+              value={temperature}
+              onChange={(event) =>
+                handleTemperature(Number(event.target.value))
+              }
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Weigh <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="ex: 80 KG"
+              value={weigh}
+              onChange={(event) => handleWeigh(Number(event.target.value))}
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Height <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="ex: 170 M"
+              value={height}
+              onChange={(event) => handleHeight(Number(event.target.value))}
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Heart Rate <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="ex: 60 beats/minute"
+              value={heartRate}
+              onChange={(event) => handleHeartRate(Number(event.target.value))}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Blood Type <span className="login-danger"></span>
+            </label>
+            <select
+              className="form-control"
+              value={bloodType}
+              onChange={(event) => handleBloodType(event.target.value)}
+            >
+              <option value="select option" disabled>
+                Select Option
+              </option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Blood Sugar <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="number"
+              placeholder="ex: 70 mg/dL"
+              value={bloodSugar}
+              onChange={(event) => handleBloodSugar(Number(event.target.value))}
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
+          <div className="input-block local-forms">
+            <label>
+              Blood Pressure <span className="login-danger"></span>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="ex: 85 mmHg"
+              value={bloodPressure}
+              onChange={(event) =>
+                handleBloodPressure(Number(event.target.value))
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HealthDetails;
