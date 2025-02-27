2dd69891d5d75aacd67870919444e11b992092dc
Barsoum Adly <barsoumadly@gmail.com>
2025-02-27 19:03:54 +0200

    patient layout

--- src/features/dashboard/patient/PatientLayout.jsx
+++ src/features/dashboard/patient/PatientLayout.jsx
@@ -19,7 +19,7 @@ function DashboardLayout() {
         setMinbar={setMinbar}
         url={"/patient/dashboard"}
       />
-      {isProfileCompleted ? (
+      {true ? (
         <>
           <Sidebar minbar={minbar} showbar={showbar}>
             <PatientSidebar />
