2e606b104bd29c2b45ccd39737ee3e6ed0c704a6
Barsoum Adly <barsoumadly@gmail.com>
2025-02-27 16:49:46 +0200

    address details

---
+++ src/features/dashboard/patient/profile/AddressDetails.jsx
@@ -0,0 +1,69 @@
+function AddressDetails({
+  city,
+  area,
+  address,
+  handleCity,
+  handleArea,
+  handleAddress,
+}) {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <div className="form-heading">
+          <h4>Address Details</h4>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-xl-3">
+        <div className="input-block local-forms">
+          <label>
+            City <span className="login-danger">*</span>
+          </label>
+          <select
+            className="form-control"
+            required
+            value={city}
+            onChange={(event) => handleCity(event.target.value)}
+          >
+            <option value="select-option" dir="">
+              Select Option
+            </option>
+          </select>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-xl-3">
+        <div className="input-block local-forms">
+          <label>
+            Area <span className="login-danger">*</span>
+          </label>
+          <select
+            className="form-control"
+            required
+            value={area}
+            onChange={(event) => handleArea(event.target.value)}
+          >
+            <option value="select-option" dir="">
+              Select Option
+            </option>
+          </select>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-xl-4">
+        <div className="input-block local-forms">
+          <label>
+            Address <span className="login-danger">*</span>
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            required
+            placeholder="ex: El Hegaz St, Al Matar, El Nozha"
+            value={address}
+            onChange={(event) => handleAddress(event.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddressDetails;
