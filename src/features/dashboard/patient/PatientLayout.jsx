172f5a7a260660509e800cca2623612cc941ba61
Barsoum Adly <barsoumadly@gmail.com>
2025-02-27 16:49:34 +0200

    patient layout

--- src/features/dashboard/patient/PatientLayout.jsx
+++ src/features/dashboard/patient/PatientLayout.jsx
@@ -4,10 +4,14 @@ import Header from "../../../ui/Header";
 import Sidebar from "../../../ui/Sidebar";
 import { Outlet } from "react-router-dom";
 import PatientSidebar from "./PatientSidebar";
+import { useAuth } from "../../../context/AuthContext";
+import CompletePatientProfile from "./profile/CompletePatientProfile";
 
 function DashboardLayout() {
   const [showbar, setShowbar] = useState(true);
   const [minbar, setMinbar] = useState(false);
+  const { isProfileCompleted } = useAuth();
+
   return (
     <>
       <Header
@@ -15,12 +19,18 @@ function DashboardLayout() {
         setMinbar={setMinbar}
         url={"/patient/dashboard"}
       />
-      <Sidebar minbar={minbar} showbar={showbar}>
-        <PatientSidebar />
-      </Sidebar>
-      <main className={minbar ? "mini-sidebar" : ""}>
-        <Outlet />
-      </main>
+      {isProfileCompleted ? (
+        <>
+          <Sidebar minbar={minbar} showbar={showbar}>
+            <PatientSidebar />
+          </Sidebar>
+          <main className={minbar ? "mini-sidebar" : ""}>
+            <Outlet />
+          </main>
+        </>
+      ) : (
+        <CompletePatientProfile />
+      )}
     </>
   );
 }
