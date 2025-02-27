8589c5328646038bd4eac92c941b58aacb9eb8c9
Barsoum Adly <barsoumadly@gmail.com>
2025-02-27 19:03:40 +0200

    dashboard content

--- src/features/dashboard/patient/DashboardContents.jsx
+++ src/features/dashboard/patient/DashboardContents.jsx
@@ -20,15 +20,8 @@ function DashboardContents() {
               <div id="heart-rate" />
               <div className="dash-content">
                 <h5>
-                  110 <span>bpm</span>
+                  __ <span>bpm</span>
                 </h5>
-                <p>
-                  <span className="passive-view">
-                    <i className="feather-arrow-up-right me-1" />
-                    40%
-                  </span>{" "}
-                  vs last month
-                </p>
               </div>
             </div>
           </div>
@@ -49,15 +42,8 @@ function DashboardContents() {
               <div id="temperature-chart" />
               <div className="dash-content">
                 <h5>
-                  38.6 <span>c</span>
+                  __ <span>°C</span>
                 </h5>
-                <p>
-                  <span className="negative-view">
-                    <i className="feather-arrow-down-right me-1" />
-                    -20%
-                  </span>{" "}
-                  vs last month
-                </p>
               </div>
             </div>
           </div>
@@ -78,109 +64,183 @@ function DashboardContents() {
               <div id="pressure-chart" />
               <div className="dash-content">
                 <h5>
-                  120 <span>mm/Hg</span>
+                  __ <span>mm/Hg</span>
                 </h5>
-                <p>
-                  <span className="negative-view">
-                    <i className="feather-arrow-down-right me-1" />
-                    -40%
-                  </span>{" "}
-                  vs last month
-                </p>
+              </div>
+              <div className="dash-content">
+                Blood Type:{" "}
+                <span
+                  style={{
+                    fontSize: "24px",
+                    color: "#2E37A4",
+                    fontWeight: "600",
+                  }}
+                >
+                  __
+                </span>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="row">
-        <div className="col-12 col-md-12 col-xl-7">
-          <div className="row">
-            <div className="col-12 col-md-12  col-xl-12">
-              <div className="card">
-                <div className="card-header">
-                  <h4 className="card-title d-inline-block">Medical History</h4>{" "}
-                  <Link
-                    to="/patient/appointments"
-                    className="patient-views float-end"
+        <div className="col-12 col-md-12  col-xl-6">
+          <div className="card">
+            <div className="card-body">
+              <div className="chart-title patient-visit">
+                <h4>
+                  Body Mass index
+                  <br />
+                  <br />
+                  <span
+                    style={{
+                      fontSize: "24px",
+                      color: "#2E37A4",
+                      fontWeight: "600",
+                    }}
                   >
-                    Show all
-                  </Link>
-                </div>
-                <div className="card-body p-0 table-dash">
-                  <div className="table-responsive">
-                    <table className="table mb-0 border-0 datatable custom-table patient-table">
-                      <thead>
-                        <tr>
-                          <th>Doctor name</th>
-                          <th>Diagnosis</th>
-                          <th>Date</th>
-                          <th>Status</th>
-                          <th />
-                          <th />
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td className="table-image">
-                            <img
-                              width={28}
-                              height={28}
-                              className="rounded-circle"
-                              src="assets/img/profiles/avatar-02.jpg"
-                              alt=""
-                            />
-                            <h2>Dr.Jenny Smith</h2>
-                          </td>
-                          <td>Dermotology</td>
-                          <td>12.05.2022 </td>
-                          <td>
-                            <div class="dropdown action-label">
-                              <a class="custom-badge status-orange">Pending</a>
-                            </div>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td className="table-image">
-                            <img
-                              width={28}
-                              height={28}
-                              className="rounded-circle"
-                              src="assets/img/profiles/avatar-04.jpg"
-                              alt=""
-                            />
-                            <h2>Andrea Lalema</h2>
-                          </td>
-                          <td>Dermotology</td>
-                          <td>10.05.2022 </td>
-                          <td>
-                            <div class="dropdown action-label">
-                              <a class="custom-badge status-green">Approved</a>
-                            </div>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td className="table-image">
-                            <img
-                              width={28}
-                              height={28}
-                              className="rounded-circle"
-                              src="assets/img/profiles/avatar-05.jpg"
-                              alt=""
-                            />
-                            <h2>Dr.William Stephin</h2>
-                          </td>
-                          <td>Dermotology</td>
-                          <td>12.05.2022 </td>
-                          <td>
-                            <div class="dropdown action-label">
-                              <a class="custom-badge status-red">Cancelled</a>
-                            </div>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
+                    __
+                  </span>{" "}
+                  KG/CM<sup>2</sup>
+                </h4>
+              </div>
+              <div className="body-mass-blk">
+                <div className="row">
+                  <div className="col-md-6">
+                    <center>
+                      <div className="weight-blk">
+                        <span
+                          style={{
+                            fontSize: "24px",
+                            color: "#2E37A4",
+                            fontWeight: "600",
+                          }}
+                        >
+                          __
+                        </span>{" "}
+                        KG
+                      </div>
+                    </center>
+                  </div>
+                  <div className="col-md-6">
+                    <center>
+                      <div className="weight-blk">
+                        <span
+                          style={{
+                            fontSize: "24px",
+                            color: "#2E37A4",
+                            fontWeight: "600",
+                          }}
+                        >
+                          __
+                        </span>{" "}
+                        CM
+                      </div>
+                    </center>
                   </div>
                 </div>
+                <div className="progress weight-bar">
+                  <div
+                    className="progress-bar progress-bar-success"
+                    role="progressbar"
+                  />
+                </div>
+                <ul className="weight-checkit">
+                  <li>Underweight</li>
+                  <li>Normal (45.5)</li>
+                  <li>Overweight</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-12  col-xl-15">
+          <div className="card">
+            <div className="card-header">
+              <h4 className="card-title d-inline-block">
+                Appointments History
+              </h4>
+              {/* <Link
+                to="/patient/appointments"
+                className="patient-views float-end"
+              >
+                Show all
+              </Link> */}
+            </div>
+            <div className="card-body p-0 table-dash">
+              <div className="table-responsive">
+                <table className="table mb-0 border-0 datatable custom-table patient-table">
+                  <thead>
+                    <tr>
+                      <th>Doctor name</th>
+                      <th>Diagnosis</th>
+                      <th>Date</th>
+                      <th>Status</th>
+                      <th />
+                      <th />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="table-image">
+                        <img
+                          width={28}
+                          height={28}
+                          className="rounded-circle"
+                          src="assets/img/profiles/avatar-02.jpg"
+                          alt=""
+                        />
+                        <h2>Dr.Jenny Smith</h2>
+                      </td>
+                      <td>Dermotology</td>
+                      <td>12.05.2022 </td>
+                      <td>
+                        <div class="dropdown action-label">
+                          <a class="custom-badge status-orange">Pending</a>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="table-image">
+                        <img
+                          width={28}
+                          height={28}
+                          className="rounded-circle"
+                          src="assets/img/profiles/avatar-04.jpg"
+                          alt=""
+                        />
+                        <h2>Andrea Lalema</h2>
+                      </td>
+                      <td>Dermotology</td>
+                      <td>10.05.2022 </td>
+                      <td>
+                        <div class="dropdown action-label">
+                          <a class="custom-badge status-green">Approved</a>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="table-image">
+                        <img
+                          width={28}
+                          height={28}
+                          className="rounded-circle"
+                          src="assets/img/profiles/avatar-05.jpg"
+                          alt=""
+                        />
+                        <h2>Dr.William Stephin</h2>
+                      </td>
+                      <td>Dermotology</td>
+                      <td>12.05.2022 </td>
+                      <td>
+                        <div class="dropdown action-label">
+                          <a class="custom-badge status-red">Cancelled</a>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
