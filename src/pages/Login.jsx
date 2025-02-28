d32ddd0d64c9d6beb3ab45cc5d57b9685dd61b22
Barsoum Adly <barsoumadly@gmail.com>
2025-02-28 14:26:34 +0200

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,17 +17,14 @@ function Login() {
 
   const navigate = useNavigate();
 
-  const showCompleteProfile = function (userRole, isProfileCompleted) {
-    if (userRole === "CLINIC_ADMIN" && !isProfileCompleted) {
-      navigate("/clinic/complete-profile");
-    } else if (userRole === "PHARMACY_ADMIN" && !isProfileCompleted) {
-    }
-  };
+  // const showCompleteProfile = function (userRole, isProfileCompleted) {
+  //   if (userRole === "CLINIC_ADMIN" && !isProfileCompleted) {
+  //     navigate("/clinic/complete-profile");
+  //   } else if (userRole === "PHARMACY_ADMIN" && !isProfileCompleted) {
+  //   }
+  // };
 
-  const navigateUser = function (userRole, isProfileCompleted = false) {
-    // if (!isProfileCompleted)
-    //   return showCompleteProfile(userRole, isProfileCompleted);
-
+  const navigateUser = function (userRole) {
     if (userRole === "CLINIC_ADMIN") {
       navigate("/clinic/dashboard", { replace: true });
     } else if (userRole === "PHARMACY_ADMIN") {
