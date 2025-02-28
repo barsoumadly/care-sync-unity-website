14e3f0c4b95eb7502af4fece271d24f6216eeef3
Barsoum Adly <barsoumadly@gmail.com>
2025-02-28 14:44:15 +0200

    laboratory complete profile

---
+++ src/features/dashboard/laboratory/profile/LaboratoryCompleteProfile.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { useAuth } from "../../../../context/AuthContext";
+import { Link } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import LaboratoryDetails from "./LaboratoryDetails";
+import AddressDetails from "./AddressDetails";
+import ProfilePhoto from "./ProfilePhoto";
+
+function LaboratoryCompleteProfile() {
+  const [laboratoryName, setLaboratoryName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [foundedYear, setFoundedYear] = useState("");
+  const [city, setCity] = useState("select-option");
+  const [area, setArea] = useState("select-option");
+  const [address, setAddress] = useState("");
+  const [avatar, setAvatar] = useState(
+    "https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg"
+  );
+  const { userLogout } = useAuth();
+
+  const handleSubmit = function (event) {
+    event.preventDefault();
+
+    if (phoneNumber.length > 12) {
+      return toast.error("Enter a valid phone number.");
+    }
+  };
+
+  return (
+    <div className="page-wrapper" style={{ marginLeft: "0px" }}>
+      <div className="content">
+        {/* Page Header */}
+        <div className="page-header">
+          <div className="row">
+            <div className="col-sm-7 col-6">
+              <ul className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <a>Profile </a>
+                </li>
+                <li className="breadcrumb-item">
+                  <i className="feather-chevron-right" />
+                </li>
+                <li className="breadcrumb-item active">Complete Profile</li>
+              </ul>
+            </div>
+            <div class="col-sm-5 col-6 text-end">
+              <Link
+                className="btn btn-primary btn-rounded"
+                onClick={() => {
+                  userLogout();
+                  logout();
+                }}
+              >
+                Logout <FiLogOut />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="card">
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <LaboratoryDetails
+                    laboratoryName={laboratoryName}
+                    phoneNumber={phoneNumber}
+                    foundedYear={foundedYear}
+                    handleLaboratoryName={setLaboratoryName}
+                    handlePhoneNumber={setPhoneNumber}
+                    handleFoundedYear={setFoundedYear}
+                  />
+                  <AddressDetails
+                    city={city}
+                    area={area}
+                    address={address}
+                    handleCity={setCity}
+                    handleArea={setArea}
+                    handleAddress={setAddress}
+                  />
+                  <ProfilePhoto avatar={avatar} handleAvatar={setAvatar} />
+                  <div className="col-12">
+                    <div className="doctor-submit text-end">
+                      <button
+                        type="submit"
+                        className="btn btn-primary submit-form me-2"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LaboratoryCompleteProfile;
