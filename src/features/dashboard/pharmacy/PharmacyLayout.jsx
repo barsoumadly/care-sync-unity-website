2fedd1462d8fc1850aaafa0e5e8c9ff091c8056c
Barsoum Adly <barsoumadly@gmail.com>
2025-02-28 14:27:27 +0200

    pharmacy layout

---
+++ src/features/dashboard/pharmacy/PharmacyLayout.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useAuth } from "../../../context/AuthContext";
+import Header from "../../../ui/Header";
+import Sidebar from "../../../ui/Sidebar";
+import PharmacySidbar from "./PharmacySidebar";
+import { Outlet } from "react-router-dom";
+import PharmacyCompleteProfile from "./profile/PharmacyCompleteProfile";
+
+function PharmacyLayout() {
+  const [showbar, setShowbar] = useState(true);
+  const [minbar, setMinbar] = useState(false);
+  const { isProfileCompleted } = useAuth();
+
+  return (
+    <>
+      <Header
+        setShowbar={setShowbar}
+        setMinbar={setMinbar}
+        url={"/pharmacy/dashboard"}
+      />
+      {isProfileCompleted ? (
+        <>
+          <Sidebar minbar={minbar} showbar={showbar}>
+            <PharmacySidbar />
+          </Sidebar>
+          <main className={minbar ? "mini-sidebar" : ""}>
+            <Outlet />
+          </main>
+        </>
+      ) : (
+        <PharmacyCompleteProfile />
+      )}
+    </>
+  );
+}
+
+export default PharmacyLayout;
