ac2f0711da884761b8247dfcaeb0ced656f55db0
Barsoum Adly <barsoumadly@gmail.com>
2025-03-01 16:07:54 +0200

    skip login

--- src/features/authentication/SkipLogin.jsx
+++ src/features/authentication/SkipLogin.jsx
@@ -4,21 +4,22 @@ import { useNavigate } from "react-router-dom";
 import Loader from "../../ui/Loader";
 
 function SkipLogin({ children }) {
-  const { user, isAuthenticated } = useAuth();
-  let { isProfileCompleted } = useAuth();
+  const { user, isAuthenticated, isProfileCompleted, isRegistered } = useAuth();
   const navigate = useNavigate();
-  if (isProfileCompleted === undefined) isProfileCompleted = true;
 
   useEffect(
     function () {
-      if (isAuthenticated) {
-        navigate(
-          `/${user.role.toLowerCase().split("_")[0]}/${
-            isProfileCompleted ? "dashboard" : "complete-profile"
-          }`
-        );
+      if (!isRegistered) {
+        if (isAuthenticated) {
+          navigate(
+            `/${user?.role?.toLowerCase().split("_")[0]}/${
+              isProfileCompleted ? "dashboard" : "complete-profile"
+            }`
+          );
+        }
       }
     },
+
     [isAuthenticated, navigate]
   );
 
