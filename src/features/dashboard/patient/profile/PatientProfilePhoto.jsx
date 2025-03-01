0ddf2405d1778c5e242b262d7c1f5d24f1d14335
Barsoum Adly <barsoumadly@gmail.com>
2025-03-01 21:23:59 +0200

    patient profile photo

--- src/features/dashboard/patient/profile/PatientProfilePhoto.jsx
+++ src/features/dashboard/patient/profile/PatientProfilePhoto.jsx
@@ -1,10 +1,41 @@
-function PatientProfilePhoto({ avatar, handleAvatar }) {
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+function PatientProfilePhoto({
+  patientData,
+  onChangePatientData,
+  onChangePageNumber,
+}) {
+  const [profilePhoto, setProfilePhoto] = useState(
+    "https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg"
+  );
+  const navigate = useNavigate();
+
+  const handleDecPageNumber = function () {
+    onChangePageNumber((pageNumber) => pageNumber - 1);
+  };
+
+  const performSubmit = function (event) {
+    event.preventDefault();
+
+    onChangePatientData({ ...patientData, profilePhoto });
+    // toast.promise(saveSettings(settings), {
+    //   loading: "Saving...",
+    //   success: <b>Data saved 👍</b>,
+    //   error: <b>Could not save 🥲</b>,
+    // });
+    navigate("/patient/dashboard");
+  };
+
   return (
-    <>
-      <div className="col-12">
+    <form>
+      <div className="col-12" style={{ marginTop: "50px" }}>
         <div className="form-heading">
           <h4>
-            Profile Photo <span style={{ fontSize: "13px" }}>(Optional)</span>
+            <span style={{ fontSize: "15px" }}>
+              Upload Your Profile Photo (optional)
+            </span>
           </h4>
         </div>
       </div>
@@ -19,9 +50,9 @@ function PatientProfilePhoto({ avatar, handleAvatar }) {
               accept="image/*"
               name="image"
               id="file"
-              onChange={(event) => {
-                handleAvatar(URL.createObjectURL(event.target.files[0]));
-              }}
+              onChange={(event) =>
+                setProfilePhoto(URL.createObjectURL(event.target.files[0]))
+              }
               className="hide-input"
               data-cf-modified-f4b406440a9d28b1c089eaf4-=""
             />
@@ -30,11 +61,34 @@ function PatientProfilePhoto({ avatar, handleAvatar }) {
             </label>
           </div>
           <div className="upload-images upload-size">
-            <img src={avatar} alt="Image" />
+            <img src={profilePhoto} alt="Image" />
           </div>
         </div>
       </div>
-    </>
+      <div className="row">
+        <div className="col-12 col-md-6 col-xl-1">
+          <div className="doctor-submit text-end">
+            <button
+              type="submit"
+              className="btn btn-primary submit-form me-2"
+              onClick={handleDecPageNumber}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-11">
+          <div className="doctor-submit text-end">
+            <button
+              className="btn btn-primary submit-form me-2"
+              onClick={performSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 }
 
