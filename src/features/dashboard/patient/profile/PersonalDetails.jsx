542f4cf3e0034ed59247541b42a7f7f360a13787
Barsoum Adly <barsoumadly@gmail.com>
2025-03-01 16:08:59 +0200

    personal details

--- src/features/dashboard/patient/profile/PersonalDetails.jsx
+++ src/features/dashboard/patient/profile/PersonalDetails.jsx
@@ -1,76 +1,81 @@
-function PersonalDetails({
-  phoneNumber,
-  age,
-  gender,
-  handlePhoneNumber,
-  handleAge,
-  handleGender,
-}) {
+import { useForm } from "react-hook-form";
+
+function PatientPersonalDetails({ patientData, onChangePatientData }) {
+  const { register, handleSubmit } = useForm();
+
+  const performSubmit = function (data) {
+    onChangePatientData({ ...data });
+  };
+
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="form-heading">
-          <h4>Personal Details</h4>
+    <>
+      <form onSubmit={handleSubmit(performSubmit)}>
+        <div className="row">
+          <div className="col-12">
+            <div className="form-heading">
+              <h4>Personal Details</h4>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-3">
+            <div className="input-block local-forms">
+              <label>
+                Phone Number <span className="login-danger">*</span>
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                value={patientData?.phoneNumber}
+                placeholder="ex: 0123456789"
+                required
+                {...register("phoneNumber")}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-3">
+            <div className="input-block local-forms">
+              <label>
+                Age <span className="login-danger">*</span>
+              </label>
+              <input
+                className="form-control"
+                type="number"
+                placeholder="ex: 30"
+                required
+                value={patientData?.age}
+                {...register("age")}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-3">
+            <div className="input-block local-forms">
+              <label>
+                Gender <span className="login-danger">*</span>
+              </label>
+              <select
+                className="form-control"
+                value={patientData?.gender}
+                required
+                {...register("gender")}
+              >
+                <option value="select-option" disabled>
+                  Select Option
+                </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="col-12 col-md-6 col-xl-3">
-        <div className="input-block local-forms">
-          <label>
-            Phone Number <span className="login-danger">*</span>
-          </label>
-          <input
-            className="form-control"
-            type="text"
-            value={phoneNumber}
-            placeholder="ex: 0123456789"
-            required
-            onChange={(event) =>
-              event.target.value.length <= 11 &&
-              handlePhoneNumber(event.target.value)
-            }
-          />
+        <div className="col-12">
+          <div className="doctor-submit text-end">
+            <button type="submit" className="btn btn-primary submit-form me-2">
+              Next
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="col-12 col-md-6 col-xl-3">
-        <div className="input-block local-forms">
-          <label>
-            Age <span className="login-danger">*</span>
-          </label>
-          <input
-            className="form-control"
-            type="number"
-            placeholder="ex: 30"
-            required
-            value={age}
-            onChange={(event) =>
-              Number(event.target.value) > 0 &&
-              Number(event.target.value) < 100 &&
-              handleAge(Number(event.target.age))
-            }
-          />
-        </div>
-      </div>
-      <div className="col-12 col-md-6 col-xl-3">
-        <div className="input-block local-forms">
-          <label>
-            Gender <span className="login-danger">*</span>
-          </label>
-          <select
-            className="form-control"
-            value={gender}
-            required
-            onChange={(event) => handleGender(event.target.value)}
-          >
-            <option value="select-option" disabled>
-              Select Option
-            </option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </div>
-      </div>
-    </div>
+      </form>
+    </>
   );
 }
 
-export default PersonalDetails;
+export default PatientPersonalDetails;
