482bed8023899b8b237ab47385e7d4e4b60ed11b
Barsoum Adly <barsoumadly@gmail.com>
2025-03-02 20:20:35 +0200

    laboratory view profile

---
+++ src/features/dashboard/laboratory/profile/LaboratoryViewProfile.jsx
@@ -0,0 +1,96 @@
+import Map from "../../../../ui/Map";
+import LaboratoryHeader from "./LaboratoryHeader";
+
+const laboratory = {
+  id: 1,
+  name: "Alfa Laboratory",
+  slug: "alfa-laboratory",
+  email: "alfa-laboratory@gmail.com",
+  city: "Cairo",
+  address:
+    "El-Sayed El-Merghany, Manshîyet el Bakri, Heliopolis, Cairo Governorate",
+  location:
+    "Alfa Laboratory El-Sayed El-Merghany, Manshîyet el Bakri, Heliopolis, Cairo Governorate",
+  phone: "012 81001504",
+  foundedDate: 1975,
+  profilePhoto:
+    "https://lh3.googleusercontent.com/p/AF1QipNCM2ymcmttFz5-YmrUyMwNQwfN70YkJ0jFsWUZ=s1360-w1360-h1020",
+  rating: 3.5,
+};
+
+function LaboratoryViewProfile() {
+  return (
+    <div className="main-wrapper">
+      <div className="page-wrapper">
+        <div className="content">
+          <LaboratoryHeader ClinicHeader activeTab="about" />
+          <div className="profile-tabs">
+            <div className="tab-content">
+              <div className="tab-pane show active" id="about-cont">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="card-box">
+                      <h3 className="card-title"> Informations</h3>
+                      <div className="experience-box">
+                        <div className="hello-park">
+                          <p>
+                            At {laboratory.name}, our experienced doctors and
+                            staff are dedicated to providing compassionate,
+                            cutting-edge care.
+                            <br /> We prioritize your well-being and strive for
+                            the best possible outcomes.
+                          </p>
+                        </div>
+                        <ul className="experience-list">
+                          <li>
+                            <div className="experience-user">
+                              <div className="before-circle" />
+                            </div>
+                            <div className="experience-content">
+                              <div className="timeline-content">
+                                <a href="#/" className="name">
+                                  Pharmacy Foundation
+                                </a>
+                                <div>team of healthcare professionals</div>
+                                <span className="time">1974 - 1975</span>
+                              </div>
+                            </div>
+                          </li>
+                          <li>
+                            <div className="experience-user">
+                              <div className="before-circle" />
+                            </div>
+                            <div className="experience-content">
+                              <div className="timeline-content">
+                                <a href="#/" className="name">
+                                  Recent Developments
+                                </a>
+                                <div>
+                                  It began to attract medical tourists from
+                                  other countries.
+                                </div>
+                                <span className="time">1990 - 2000</span>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="card-box">
+                      <Map
+                        name={laboratory.name}
+                        markerPosition={laboratory.location}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LaboratoryViewProfile;
