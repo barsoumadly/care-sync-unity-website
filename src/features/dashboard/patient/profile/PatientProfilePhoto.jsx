fa1f17d64a9b711121818dff9efa3a07d9e0bd5e
Barsoum Adly <barsoumadly@gmail.com>
2025-03-02 19:25:38 +0200

    patient profile photo

--- src/features/dashboard/patient/profile/PatientProfilePhoto.jsx
+++ src/features/dashboard/patient/profile/PatientProfilePhoto.jsx
@@ -30,41 +30,44 @@ function PatientProfilePhoto({
 
   return (
     <form>
-      <div className="col-12" style={{ marginTop: "50px" }}>
-        <div className="form-heading">
-          <h4>
-            <span style={{ fontSize: "15px" }}>
-              Upload Your Profile Photo (optional)
-            </span>
-          </h4>
+      <div className="row">
+        <div className="col-12" style={{ marginTop: "50px" }}>
+          <div className="form-heading">
+            <h4>
+              <span style={{ fontSize: "15px" }}>
+                Upload Your Profile Photo (optional)
+              </span>
+            </h4>
+          </div>
         </div>
-      </div>
-      <div className="col-12 col-md-6 col-xl-5">
-        <div className="input-block local-top-form">
-          <label className="local-top">
-            Avatar <span className="login-danger"></span>
-          </label>
-          <div className="settings-btn upload-files-avator">
-            <input
-              type="file"
-              accept="image/*"
-              name="image"
-              id="file"
-              onChange={(event) =>
-                setProfilePhoto(URL.createObjectURL(event.target.files[0]))
-              }
-              className="hide-input"
-              data-cf-modified-f4b406440a9d28b1c089eaf4-=""
-            />
-            <label htmlFor="file" className="upload">
-              Choose File
+        <div className="col-12 col-md-6 col-xl-5">
+          <div className="input-block local-top-form">
+            <label className="local-top">
+              Avatar <span className="login-danger"></span>
             </label>
-          </div>
-          <div className="upload-images upload-size">
-            <img src={profilePhoto} alt="Image" />
+            <div className="settings-btn upload-files-avator">
+              <input
+                type="file"
+                accept="image/*"
+                name="image"
+                id="file"
+                onChange={(event) =>
+                  setProfilePhoto(URL.createObjectURL(event.target.files[0]))
+                }
+                className="hide-input"
+                data-cf-modified-f4b406440a9d28b1c089eaf4-=""
+              />
+              <label htmlFor="file" className="upload">
+                Choose File
+              </label>
+            </div>
+            <div className="upload-images upload-size">
+              <img src={profilePhoto} alt="Image" />
+            </div>
           </div>
         </div>
       </div>
+
       <div className="row">
         <div className="col-12 col-md-6 col-xl-1">
           <div className="doctor-submit text-end">
