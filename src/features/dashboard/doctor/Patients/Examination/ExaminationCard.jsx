dcf541cd623426f9ec29154a3fb596593e26fd04
bishoy labib <bishoylabib78@gmail.com>
2025-03-04 16:51:06 +0200

    ExaminationCard

---
+++ src/features/dashboard/doctor/Patients/Examination/ExaminationCard.jsx
@@ -0,0 +1,32 @@
+function ExaminationCard({ type, setOpenCard, isAdding }) {
+  return (
+    <div
+      className="text-center mt-5 bg-light py-5"
+      style={{ padding: "0 10%" }}
+    >
+      <div className="reminder-icon">
+        <img
+          src={`${
+            !isAdding
+              ? `/images/dashborad/${type}.webp`
+              : "/images/dashborad/check-mark.png"
+          }`}
+          alt={type}
+        />
+      </div>
+      <h4>{`${
+        !isAdding
+          ? `You haven't added any ${type} prescription`
+          : `You have added a ${type} prescription.`
+      }`}</h4>
+      <button
+        className="btn btn-primary mt-4 btn-block"
+        onClick={() => setOpenCard(type)}
+      >
+        {!isAdding ? "Add" : "Edit"} {type}
+      </button>
+    </div>
+  );
+}
+
+export default ExaminationCard;
