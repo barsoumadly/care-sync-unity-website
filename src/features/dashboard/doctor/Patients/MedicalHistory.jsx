ed459080997a488903b0a97beda896c78c16dffe
bishoy labib <bishoylabib78@gmail.com>
2025-03-04 16:51:17 +0200

    MedicalHistory

---
+++ src/features/dashboard/doctor/Patients/MedicalHistory.jsx
@@ -0,0 +1,18 @@
+function MedicalHistory() {
+  return (
+    <div className="hello-park">
+      <p>
+        Completed my graduation in Gynaecologist Medicine from the well known
+        and renowned institution of India – SARDAR PATEL MEDICAL COLLEGE, BARODA
+        in 2000-01, which was affiliated to M.S. University. I ranker in
+        University exams from the same university from 1996-01.
+      </p>
+      <p>
+        Worked as Professor and Head of the department ; Community medicine
+        Department at Sterline Hospital, Rajkot, Gujarat from 2003-2015
+      </p>
+    </div>
+  );
+}
+
+export default MedicalHistory;
