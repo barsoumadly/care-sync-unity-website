6a5b9b019d500c889fd678c99a71d0275221f3de
bishoy labib <bishoylabib78@gmail.com>
2025-03-04 16:50:53 +0200

    AboutOfPatient

---
+++ src/features/dashboard/doctor/Patients/AboutOfPatient.jsx
@@ -0,0 +1,36 @@
+function AboutOfPatient() {
+  return (
+    <div className="doctor-personals-grp">
+      <div className="card">
+        <div className="card-body">
+          <div className="heading-detail ">
+            <h4 className="mb-3">About me</h4>
+            <p>
+              Hello I am Smith Bruklin a Gynaecologist in Sanjivni Hospital
+              Surat. I love to work with all my hospital staff and seniour
+              doctors.
+            </p>
+          </div>
+          <div className="about-me-list">
+            <ul className="list-space">
+              <li>
+                <h4>Gender</h4>
+                <span>Male</span>
+              </li>
+              <li>
+                <h4>Operation Done</h4>
+                <span>30+</span>
+              </li>
+              <li>
+                <h4>Designation</h4>
+                <span>Engineer</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutOfPatient;
