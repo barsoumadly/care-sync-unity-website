a3c508b9ac98ea0f3fad7a09975a9b79422006cc
bishoy labib <bishoylabib78@gmail.com>
2025-03-06 19:43:01 +0200

    ProtectedCompleteProfile

--- src/ui/ProtectedCompleteProfile.jsx
+++ src/ui/ProtectedCompleteProfile.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import Loader from "./Loader";
 import { LocalStorageState } from "../hooks/LocalStorageState";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function ProtectedCompleteProfile({ children }) {
+  const { isProfileCompleted } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
   setTimeout(() => setIsLoading(false), 1000);
   LocalStorageState();
 
   if (isLoading) return <Loader />;
-  return children;
+  if (!isProfileCompleted) return children;
+  else navigate(-1);
 }
 
 export default ProtectedCompleteProfile;
