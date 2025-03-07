1ee5e65742a016950ba65e88e8554336061ce496
Kerolos <kerolos.saaid@gmail.com>
2025-03-07 19:37:40 +0200

    fix: update profile photo upload to use FormData with photo key

--- src/services/user.js
+++ src/services/user.js
@@ -4,15 +4,14 @@ const API_URL = "http://localhost:8000/api/v1/users/profile-photo";
 
 const updateProfilePhoto = async function (profilePhoto, token) {
   try {
-    await axios.put(
-      `${API_URL}`,
-      { file: { path: profilePhoto.name } },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const formData = new FormData();
+    formData.append("photo", profilePhoto);
+
+    await axios.put(`${API_URL}`, formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   } catch (error) {
     console.log(error);
   }
