50cae6e5142380cb6dfd1eab133f1d680a1659cd
Barsoum Adly <barsoumadly@gmail.com>
2025-03-07 14:28:32 +0200

    patient functions

---
+++ src/services/patient.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/v1/patients";
+
+const updatePatientProfile = async function (patientData, token) {
+  const data = {
+    phone: patientData.phoneNumber,
+    dateOfBirth: patientData.birthDate,
+    gender: patientData.gender,
+    address: {
+      city: patientData.selectedCity,
+      area: patientData.selectedArea,
+      address: patientData.address,
+    },
+    temperature: patientData.temperature,
+    weight: patientData.weight,
+    height: patientData.height,
+    heartRate: patientData.heartRate,
+    bloodType: patientData.bloodType,
+    bloodSugar: patientData.bloodSugar,
+    bloodPressure: patientData.bloodPressure,
+  };
+
+  try {
+    await axios.put(`${API_URL}/profile`, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { updatePatientProfile };
