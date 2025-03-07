d1d79998bed6636201f4b4633123b228109240c8
bishoy labib <bishoylabib78@gmail.com>
2025-03-07 20:57:00 +0200

    Modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,31 @@
+import { cloneElement, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ children }) {
-  return createPortal(<div className="overlay">{children}</div>, document.body);
+function Modal({ children, onClose }) {
+  const ref = useRef();
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          onClose();
+        }
+      }
+
+      document.addEventListener("click", handleClick, true);
+
+      return () => document.removeEventListener("click", handleClick, true);
+    },
+    [close]
+  );
+
+  return createPortal(
+    <div className="overlay">
+      <div className="modal styled-modal fade show">
+        {cloneElement(children, { ref: ref })}
+      </div>
+    </div>,
+    document.body
+  );
 }
 
 export default Modal;
