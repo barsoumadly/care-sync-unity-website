1cf2e8f2055d5bdcdc447f1530a272e25f639276
bishoy labib <bishoylabib78@gmail.com>
2025-03-08 16:41:58 +0200

    PatientEditProfile

--- src/features/dashboard/patient/profile/PatientEditProfile.jsx
+++ src/features/dashboard/patient/profile/PatientEditProfile.jsx
@@ -9,35 +9,41 @@ import { FaBriefcaseMedical } from "react-icons/fa6";
 import { BsFillImageFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { getPatientProfile } from "../../../../services/patient";
+import useProfile from "../useProfile";
+import { useQuery } from "@tanstack/react-query";
+import SpinnerMini from "../../../../ui/SpinnerMini";
 
 function PatientEditProfile() {
   const [pageNumber, setPageNumber] = useState(1);
   const [patientData, setPatientData] = useState({});
 
-  useEffect(function () {
-    const getProfile = async function () {
-      const response = await getPatientProfile();
-      const data = response.data.data;
-      setPatientData({
-        phoneNumber: data.phone,
-        // birthDate: data.dateOfBirth,
-        gender: data.gender,
-        selectedCity: data.address.city,
-        selectedArea: data.address.area,
-        address: data.address.address,
-        temperature: data.temperature || "",
-        weight: data.weight || "",
-        height: data.height || "",
-        heartRate: data.heartRate || "",
-        bloodType: data.bloodType || "select-option",
-        bloodType: data.bloodType || "",
-        bloodSugar: data.bloodSugar || "",
-        bloodPressure: data.bloodPressure || "",
-      });
-    };
+  const { data, isLoading, error } = useProfile();
 
-    getProfile();
-  }, []);
+  if (error) toast.error(error.message);
+
+  useEffect(
+    function () {
+      if (!isLoading) {
+        setPatientData({
+          phoneNumber: data?.phone,
+          birthDate: data.dateOfBirth,
+          gender: data?.gender,
+          selectedCity: data?.address.city,
+          selectedArea: data?.address.area,
+          address: data?.address.address,
+          temperature: data?.temperature || "",
+          weight: data?.weight || "",
+          height: data?.height || "",
+          heartRate: data?.heartRate || "",
+          bloodType: data?.bloodType || "select-option",
+          bloodType: data?.bloodType || "",
+          bloodSugar: data?.bloodSugar || "",
+          bloodPressure: data?.bloodPressure || "",
+        });
+      }
+    },
+    [data]
+  );
 
   const handleSubmit = function (event) {
     event.preventDefault();
@@ -64,6 +70,7 @@ function PatientEditProfile() {
     <div className="page-wrapper">
       <div className="content">
         {/* Page Header */}
+
         <div className="page-header">
           <div className="row">
             <div className="col-sm-7 col-6">
@@ -88,7 +95,6 @@ function PatientEditProfile() {
             </div>
           </div>
         </div>
-
         <div className="row">
           <div className="col-sm-12">
             <div className="card">
@@ -100,136 +106,140 @@ function PatientEditProfile() {
                   {pageNumber === 4 && "Profile Photo"}
                 </h4>
               </div>
-              <div className="card-body">
-                <div className="wizard" style={{ marginLeft: "-50px" }}>
-                  <ul
-                    class="nav nav-tabs justify-content-center"
-                    id="myTab"
-                    role="tablist"
-                  >
-                    <li
-                      className="nav-item flex-fill"
-                      role="presentation"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      aria-label="Seller Details"
-                      data-bs-original-title="Seller Details"
+              {isLoading ? (
+                <SpinnerMini />
+              ) : (
+                <div className="card-body">
+                  <div className="wizard" style={{ marginLeft: "-50px" }}>
+                    <ul
+                      class="nav nav-tabs justify-content-center"
+                      id="myTab"
+                      role="tablist"
                     >
-                      <a
-                        class={`nav-link ${
-                          pageNumber === 1 && "active"
-                        } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                        id="step1-tab"
-                        data-bs-toggle="tab"
-                        role="tab"
-                        aria-controls="step1"
-                        aria-selected="true"
+                      <li
+                        className="nav-item flex-fill"
+                        role="presentation"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        aria-label="Seller Details"
+                        data-bs-original-title="Seller Details"
                       >
-                        <IoPersonSharp />
-                      </a>
-                    </li>
-                    <li
-                      class="nav-item flex-fill"
-                      role="presentation"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      aria-label="Company Document"
-                      data-bs-original-title="Company Document"
-                    >
-                      <a
-                        className={`nav-link ${
-                          pageNumber === 2 && "active"
-                        } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                        id="step2-tab"
-                        data-bs-toggle="tab"
-                        role="tab"
-                        aria-controls="step2"
-                        aria-selected="false"
-                        tabindex="-1"
+                        <a
+                          class={`nav-link ${
+                            pageNumber === 1 && "active"
+                          } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                          id="step1-tab"
+                          data-bs-toggle="tab"
+                          role="tab"
+                          aria-controls="step1"
+                          aria-selected="true"
+                        >
+                          <IoPersonSharp />
+                        </a>
+                      </li>
+                      <li
+                        class="nav-item flex-fill"
+                        role="presentation"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        aria-label="Company Document"
+                        data-bs-original-title="Company Document"
                       >
-                        <IoHome />
-                      </a>
-                    </li>
-                    <li
-                      class="nav-item flex-fill"
-                      role="presentation"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      aria-label="Bank Details"
-                      data-bs-original-title="Bank Details"
-                    >
-                      <a
-                        className={`nav-link ${
-                          pageNumber === 3 && "active"
-                        } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                        href="#step3"
-                        id="step3-tab"
-                        data-bs-toggle="tab"
-                        role="tab"
-                        aria-controls="step3"
-                        aria-selected="false"
-                        tabindex="-1"
+                        <a
+                          className={`nav-link ${
+                            pageNumber === 2 && "active"
+                          } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                          id="step2-tab"
+                          data-bs-toggle="tab"
+                          role="tab"
+                          aria-controls="step2"
+                          aria-selected="false"
+                          tabindex="-1"
+                        >
+                          <IoHome />
+                        </a>
+                      </li>
+                      <li
+                        class="nav-item flex-fill"
+                        role="presentation"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        aria-label="Bank Details"
+                        data-bs-original-title="Bank Details"
                       >
-                        <FaBriefcaseMedical />
-                      </a>
-                    </li>
-                    <li
-                      class="nav-item flex-fill"
-                      role="presentation"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      aria-label="Bank Details"
-                      data-bs-original-title="Bank Details"
-                    >
-                      <a
-                        className={`nav-link ${
-                          pageNumber === 4 && "active"
-                        } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                        href="#step3"
-                        id="step3-tab"
-                        data-bs-toggle="tab"
-                        role="tab"
-                        aria-controls="step3"
-                        aria-selected="false"
-                        tabindex="-1"
+                        <a
+                          className={`nav-link ${
+                            pageNumber === 3 && "active"
+                          } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                          href="#step3"
+                          id="step3-tab"
+                          data-bs-toggle="tab"
+                          role="tab"
+                          aria-controls="step3"
+                          aria-selected="false"
+                          tabindex="-1"
+                        >
+                          <FaBriefcaseMedical />
+                        </a>
+                      </li>
+                      <li
+                        class="nav-item flex-fill"
+                        role="presentation"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        aria-label="Bank Details"
+                        data-bs-original-title="Bank Details"
                       >
-                        <BsFillImageFill />
-                      </a>
-                    </li>
-                  </ul>
+                        <a
+                          className={`nav-link ${
+                            pageNumber === 4 && "active"
+                          } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                          href="#step3"
+                          id="step3-tab"
+                          data-bs-toggle="tab"
+                          role="tab"
+                          aria-controls="step3"
+                          aria-selected="false"
+                          tabindex="-1"
+                        >
+                          <BsFillImageFill />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  {pageNumber === 1 && (
+                    <PatientPersonalDetails
+                      patientData={patientData}
+                      onChangePatientData={setPatientData}
+                      onChangePageNumber={setPageNumber}
+                    />
+                  )}
+
+                  {pageNumber === 2 && (
+                    <PatientAddressDetails
+                      patientData={patientData}
+                      onChangePatientData={setPatientData}
+                      onChangePageNumber={setPageNumber}
+                    />
+                  )}
+
+                  {pageNumber === 3 && (
+                    <PatientHealthDetails
+                      patientData={patientData}
+                      onChangePatientData={setPatientData}
+                      onChangePageNumber={setPageNumber}
+                    />
+                  )}
+
+                  {pageNumber === 4 && (
+                    <PatientProfilePhoto
+                      patientData={patientData}
+                      onChangePatientData={setPatientData}
+                      onChangePageNumber={setPageNumber}
+                    />
+                  )}
                 </div>
-                {pageNumber === 1 && (
-                  <PatientPersonalDetails
-                    patientData={patientData}
-                    onChangePatientData={setPatientData}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-
-                {pageNumber === 2 && (
-                  <PatientAddressDetails
-                    patientData={patientData}
-                    onChangePatientData={setPatientData}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-
-                {pageNumber === 3 && (
-                  <PatientHealthDetails
-                    patientData={patientData}
-                    onChangePatientData={setPatientData}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-
-                {pageNumber === 4 && (
-                  <PatientProfilePhoto
-                    patientData={patientData}
-                    onChangePatientData={setPatientData}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-              </div>
+              )}
             </div>
           </div>
         </div>
