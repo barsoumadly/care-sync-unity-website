4a1d04f19ac537e8b344203f2c550094febfda7b
bishoy labib <bishoylabib78@gmail.com>
2025-03-08 20:55:15 +0200

    ProtectedRoute

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -2,20 +2,29 @@ import { useAuth } from "../context/AuthContext";
 import { useState } from "react";
 import Loader from "./Loader";
 import { LocalStorageState } from "../hooks/LocalStorageState";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
   const { user, isAuthenticated, isProfileCompleted } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+  const location = useLocation();
+  const type = location.pathname.split("/");
 
   setTimeout(() => setIsLoading(false), 1000);
   LocalStorageState();
 
   if (isLoading) return <Loader />;
+
   if (!isProfileCompleted)
     navigate(`/${user?.role?.toLowerCase().split("_")[0]}/complete-profile`);
-  else if (isAuthenticated) return children;
+  else if (type[1] === user?.role?.toLowerCase().split("_")[0]) {
+    if (isAuthenticated) return children;
+  } else {
+    navigate(-1);
+    return true;
+  }
+
   return true;
 }
 
