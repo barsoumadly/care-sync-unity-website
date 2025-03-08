6e372652804472bfafd845cb19c9bdb52b9e4eaa
bishoy labib <bishoylabib78@gmail.com>
2025-03-08 14:46:29 +0200

    user.js

--- src/services/user.js
+++ src/services/user.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8000/api/v1/users/profile-photo";
 
-const updateProfilePhoto = async function (profilePhoto, token) {
+const updateProfilePhoto = async function (profilePhoto) {
+  const token = JSON.parse(localStorage.getItem("key"));
   try {
     const formData = new FormData();
     formData.append("photo", profilePhoto);
