e977de05a561219fb97daa329870e2f50bcbff73
Barsoum Adly <barsoumadly@gmail.com>
2025-03-09 13:29:01 +0200

    pharmacy service

---
+++ src/services/pharmacy.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/v1/pharmacies";
+
+const updatePharmacyProfile = async function (pharmacyData) {
+  const token = JSON.parse(localStorage.getItem("key"));
+  const data = {
+    name: pharmacyData.pharmacyName,
+    phone: pharmacyData.phoneNumber,
+    foundedYear: pharmacyData.foundedYear,
+    address: {
+      city: pharmacyData.selectedCity,
+      area: pharmacyData.selectedArea,
+      address: pharmacyData.address,
+    },
+  };
+  try {
+    await axios.put(`${API_URL}/profile`, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPharmacyProfile = async function () {
+  const token = JSON.parse(localStorage.getItem("key"));
+  try {
+    const response = await axios.get(`${API_URL}/profile`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return await response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { updatePharmacyProfile, getPharmacyProfile };
