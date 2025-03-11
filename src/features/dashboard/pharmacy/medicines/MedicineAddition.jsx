8a7c7e651b6637103ee75881b05586a545a87c21
Barsoum Adly <barsoumadly@gmail.com>
2025-03-11 18:27:34 +0200

    medicine addition

--- src/features/dashboard/pharmacy/medicines/MedicineAddition.jsx
+++ src/features/dashboard/pharmacy/medicines/MedicineAddition.jsx
@@ -1,148 +1,151 @@
 import { IoArrowBackOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import DynamicField from "./DynamicField";
+import { useState } from "react";
 
 function MedicineAddition() {
-  return (
-    <div className="page-wrapper" style={{ minHeight: 270 }}>
-      <div className="content container-fluid">
-        {/* Page Header */}
-        <div className="page-header">
-          <div className="row">
-            <div className="col-sm-7 col-6">
-              <ul className="breadcrumb">
-                <li className="breadcrumb-item">
-                  <a>Medicines </a>
-                </li>
-                <li className="breadcrumb-item">
-                  <i className="feather-chevron-right" />
-                </li>
-                <li className="breadcrumb-item active">Add Medicine</li>
-              </ul>
-            </div>
-            <div class="col-sm-5 col-6 text-end">
-              <Link
-                to="/pharmacy/dashboard"
-                class="btn btn-primary btn-rounded"
-              >
-                <IoArrowBackOutline /> Return Back
-              </Link>
-            </div>
-          </div>
-        </div>
+  const [medicineList, setMedicineList] = useState([{}]);
+  const [openCard, setOpenCard] = useState(false);
 
-        <div className="row">
-          <div className="col-md-12">
-            <div className="card invoices-add-card">
-              <div className="card-body">
-                <form action="#" className="invoices-form">
-                  <div className="invoice-item"></div>
-                  <div className="invoice-add-table">
-                    <h4>Medicine Details</h4>
-                    <div className="table-responsive">
-                      <table className="table table-striped table-nowrap  mb-0 no-footer add-table-items">
-                        <thead>
-                          <tr>
-                            <th>Name</th>
-                            <th>Quantity</th>
-                            <th>Price</th>
-                            <th>Expiration Date</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr className="add-row">
-                            <td>
-                              <input type="text" className="form-control" />
-                            </td>
-                            <td>
-                              <input type="number" className="form-control" />
-                            </td>
-                            <td>
-                              <input type="number" className="form-control" />
-                            </td>
-                            <td>
-                              <input type="date" className="form-control" />
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-lg-7 col-md-6">
-                      <div className="invoice-faq">
-                        <div
-                          className="panel-group"
-                          id="accordion"
-                          role="tablist"
-                          aria-multiselectable="true"
-                        >
-                          <div className="faq-tab">
-                            <div className="panel panel-default">
-                              <div
-                                className="panel-heading"
-                                role="tab"
-                                id="headingTwo"
-                              ></div>
-                              <div
-                                id="collapseTwo"
-                                className="panel-collapse collapse"
-                                role="tabpanel"
-                                aria-labelledby="headingTwo"
-                                data-bs-parent="#accordion"
-                              >
-                                <div className="panel-body">
-                                  <textarea
-                                    className="form-control"
-                                    defaultValue={""}
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="faq-tab">
-                            <div className="panel panel-default">
-                              <div
-                                className="panel-heading"
-                                role="tab"
-                                id="headingThree"
-                              ></div>
-                              <div
-                                id="collapseThree"
-                                className="panel-collapse collapse"
-                                role="tabpanel"
-                                aria-labelledby="headingThree"
-                                data-bs-parent="#accordion"
-                              >
-                                <div className="panel-body">
-                                  <textarea
-                                    className="form-control"
-                                    defaultValue={""}
-                                  />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-5 col-md-6">
-                      <div className="upload-sign">
-                        <div className="input-block"></div>
-                        <div className="input-block float-end mb-0">
-                          <button className="btn btn-primary" type="submit">
-                            Add Medicine
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </form>
+  function handleCancel() {
+    setMedicineList([{}]);
+    setOpenCard(false);
+  }
+
+  function handleSubmit() {
+    console.log(medicineList);
+    if (medicineList[0].medicine_name) {
+      setMedicineList([{}]);
+      setOpenCard(false);
+    }
+  }
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <div className="page-wrapper">
+          <div className="content">
+            {/* Page Header */}
+            <div className="page-header">
+              <div className="row">
+                <div className="col-sm-7 col-6">
+                  <ul className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <a>Medicines</a>
+                    </li>
+                    <li className="breadcrumb-item">
+                      <i className="feather-chevron-right" />
+                    </li>
+                    <li className="breadcrumb-item active">Add Medicine</li>
+                  </ul>
+                </div>
+                <div class="col-sm-5 col-6 text-end">
+                  <Link
+                    to="/pharmacy/dashboard"
+                    class="btn btn-primary btn-rounded"
+                  >
+                    <IoArrowBackOutline /> Return Back
+                  </Link>
+                </div>
               </div>
             </div>
+            {openCard ? (
+              <div className=" container-fluid ">
+                <div className="col-xl-12 ">
+                  <div className="card invoice-info-card">
+                    <div className="card-body">
+                      <div className="invoice-item invoice-item-one">
+                        <div className="row">
+                          <div className="col-md-6">
+                            <div className="invoice-logo">
+                              <img
+                                src="/public/images/logo/care-sync-unity-logo.png"
+                                alt="logo"
+                              />
+                            </div>
+                            <div className="invoice-head">
+                              <h2>Add Medicine</h2>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="invoice-item invoice-item-two">
+                        <div className="row">
+                          <div className="col-md-6">
+                            <div className="invoice-info">
+                              <strong className="customer-text-one"></strong>
+                              <h6 className="invoice-name"></h6>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <DynamicField
+                        fields={{
+                          f1: "Medicine Name",
+                          f2: "Quantity",
+                          f3: "Price",
+                          f4: "Expiration Date",
+                        }}
+                        serviceList={medicineList}
+                        setServiceList={setMedicineList}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="doctor-submit text-end">
+                    <button
+                      type="submit"
+                      className="btn btn-primary submit-form me-2"
+                      onClick={handleSubmit}
+                    >
+                      Save Prescription
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary cancel-form"
+                      onClick={handleCancel}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className=" container-fluid ">
+                <div className="col-xl-12 ">
+                  <div className="card invoice-info-card">
+                    <div className="card-boyd">
+                      <div
+                        className="text-center mt-5 py-5"
+                        style={{ padding: "0px 10%", backgroundColor: "#fff" }}
+                      >
+                        <div className="reminder-icon">
+                          <img
+                            alt="medicine"
+                            src="/images/dashborad/medicine.webp"
+                            style={{ width: "100%" }}
+                          />
+                        </div>
+                        <h4>Start adding medicines to your list</h4>
+                        <button
+                          className="btn btn-primary mt-4 btn-block"
+                          style={{ width: "450px" }}
+                          onClick={() => setOpenCard(true)}
+                        >
+                          Add medicine
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
