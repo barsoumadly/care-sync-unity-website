8f0fdb416a8bc8b290d4b0d4b3f132aa69804ae4
Barsoum Adly <barsoumadly@gmail.com>
2025-03-11 18:27:41 +0200

    dynamic field

---
+++ src/features/dashboard/pharmacy/medicines/DynamicField.jsx
@@ -0,0 +1,169 @@
+import { MdLibraryAdd } from "react-icons/md";
+
+function DynamicField({ fields, serviceList = [{}], setServiceList }) {
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
+
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, {}]);
+  };
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
+
+  return (
+    <>
+      {serviceList.map((singleService, index) => (
+        <div key={index} className="row">
+          <div className="col-12 col-md-12 col-xl-3">
+            <div className="input-block local-forms">
+              <label>
+                {fields.f1}
+                {index > 0 ? `_${index + 1}` : ""}
+                <span className="login-danger">*</span>
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="ex: Panadol"
+                required
+                name={fields.f1.toLowerCase().split(" ").join("_")}
+                onChange={(e) => handleServiceChange(e, index)}
+                value={singleService?.[fields.f1.toLowerCase()]}
+              />
+            </div>
+          </div>
+          {fields.f2 && (
+            <div className="col-12 col-md-12 col-xl-2">
+              <div className="input-block local-forms">
+                <label>
+                  {fields.f2} {index > 0 ? `_${index + 1}` : ""}
+                  <span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder={`${
+                    fields.f1 === "Medicine Name" ? "ex: 15" : ""
+                  }`}
+                  name={fields.f2.toLowerCase().split(" ").join("_")}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  value={
+                    singleService?.[
+                      fields.f2.toLowerCase().split(" ").join("_")
+                    ]
+                  }
+                />
+              </div>
+            </div>
+          )}
+          {fields.f3 && (
+            <div className="col-12 col-md-12 col-xl-2">
+              <div className="input-block local-forms">
+                <label>
+                  {fields.f4} {index > 0 ? `_${index + 1}` : ""}
+                  <span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control"
+                  type="date"
+                  name={fields.f4.toLowerCase().split(" ").join("_")}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  value={
+                    singleService?.[
+                      fields.f4.toLowerCase().split(" ").join("_")
+                    ]
+                  }
+                />
+              </div>
+            </div>
+          )}
+
+          {fields.f4 && (
+            <div className="col-12 col-md-12 col-xl-2">
+              <div className="input-block local-forms">
+                <label>
+                  {fields.f3} {index > 0 ? `_${index + 1}` : ""}
+                  <span className="login-danger">*</span>
+                </label>
+
+                <input
+                  type="number"
+                  placeholder="ex: 150"
+                  className="form-control select"
+                  fdprocessedid="m7xaqs"
+                  name={fields.f3.toLowerCase().split(" ").join("_")}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  value={
+                    singleService?.[
+                      fields.f3.toLowerCase().split(" ").join("_")
+                    ]
+                  }
+                />
+              </div>
+            </div>
+          )}
+          {fields.f5 && (
+            <div className="col-12 col-md-12 col-xl-3">
+              <div className="input-block local-forms">
+                <label>
+                  {fields.f5} {index > 0 ? `_${index + 1}` : ""}
+                </label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name={fields.f5.toLowerCase().split(" ").join("_")}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  value={
+                    singleService?.[
+                      fields.f5.toLowerCase().split(" ").join("_")
+                    ]
+                  }
+                />
+              </div>
+            </div>
+          )}
+
+          {serviceList.length !== 1 && (
+            <div
+              className="col-12 col-md-6 col-xl-2"
+              style={{ marginBottom: "30px" }}
+            >
+              <div className="doctor-submit">
+                <button
+                  type="button"
+                  onClick={() => handleServiceRemove(index)}
+                  className="btn btn-primary submit-list-form me-2"
+                >
+                  &times;
+                </button>
+              </div>
+            </div>
+          )}
+          {serviceList.length - 1 === index && (
+            <div className="col-12 col-md-6 col-xl-2">
+              <div className="doctor-submit">
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="btn btn-primary submit-list-form me-2"
+                >
+                  <MdLibraryAdd />
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default DynamicField;
