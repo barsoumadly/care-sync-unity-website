4c38110d3db09879c555ce2630d2440503c6021e
bishoy labib <bishoylabib78@gmail.com>
2025-03-11 11:25:07 +0200

    ClinicImages

---
+++ src/features/dashboard/clinic/Complete-Profile/ClinicImages.jsx
@@ -0,0 +1,70 @@
+function ClinicImages({ images, setImage }) {
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    setImage((images) => [...images, files[0]]);
+  };
+  function deleteHandler(image) {
+    setImage((images) => images.filter((e) => e !== image));
+    URL.revokeObjectURL(image);
+  }
+
+  return (
+    <>
+      <div className="col-12">
+        <div className="form-heading">
+          <h4>
+            Clinic images <span style={{ fontSize: "12px" }}>(Optional)</span>
+          </h4>
+        </div>
+      </div>
+      <div className="col-12 col-md-6 col-xl-5">
+        <div className="input-block local-top-form">
+          <label className="local-top">
+            Clinic images <span className="login-danger">*</span>
+          </label>
+          <div className="settings-btn upload-files-avator">
+            <input
+              type="file"
+              accept="image/*"
+              name="image"
+              id="file"
+              onChange={uploadImage}
+              className="hide-input"
+              data-cf-modified-f4b406440a9d28b1c089eaf4-=""
+            />
+            <label htmlFor="file" className="upload">
+              Choose File
+            </label>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+            }}
+          >
+            {images.map((image, i) => (
+              <ShowImages image={image} key={i} deleteImage={deleteHandler} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ClinicImages;
+function ShowImages({ image, deleteImage }) {
+  return (
+    <div className="upload-images upload-size" style={{ marginRight: "20px" }}>
+      <img src={`${URL.createObjectURL(image)}`} alt="Image" />
+      <button
+        onClick={() => deleteImage(image)}
+        type="button"
+        className="btn-icon logo-hide-btn"
+        style={{ border: "0", background: "#fff" }}
+      >
+        <i className="feather-x-circle" />
+      </button>
+    </div>
+  );
+}
