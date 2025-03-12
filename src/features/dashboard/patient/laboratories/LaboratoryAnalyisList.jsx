5c771df2f46584b73cbeecbc7a83fdc04f3530d5
Barsoum Adly <barsoumadly@gmail.com>
2025-03-12 20:10:29 +0200

    laboratory analysis list

--- src/features/dashboard/patient/laboratories/LaboratoryAnalyisList.jsx
+++ src/features/dashboard/patient/laboratories/LaboratoryAnalyisList.jsx
@@ -1,6 +1,22 @@
 import LaboratoryHeader from "./LaboratoryHeader";
+import useAnalysis from "./useAnalysis";
+import AnalysisTuple from "./AnalysisTuple";
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
+import DetailsModal from "./DetailsModal";
+import { useState } from "react";
 
 function LaboratoryAnalysisList() {
+  const laboratory = JSON.parse(localStorage.getItem("laboratory"));
+
+  const { data, isLoading } = useAnalysis(laboratory.userId);
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeAnalysis, setActiveAnalysis] = useState(null);
+  const analysis = data?.analysisTests;
+
+  const handleOpenModal = function () {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
   return (
     <div className="main-wrapper">
       <div className="page-wrapper" style={{ minHeight: 270 }}>
@@ -8,48 +24,83 @@ function LaboratoryAnalysisList() {
           <LaboratoryHeader activeTab="analysis" />
           {/* Page Header */}
           <div className="page-header"></div>
-          {/* /Page Header */}
-          <div id="lightgallery" className="row">
-            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
-              <a href="assets/img/blog/blog-01.jpg">
-                <img
-                  className="img-thumbnail"
-                  src="/images/clinic/image1.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
-              <a
-                href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MOvvaxkrLPSehZzvLBfMJM2Xk6UL1AVL8w&s"
-                target="blank"
-              >
-                <img
-                  className="img-thumbnail"
-                  src="/images/clinic/image2.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
-              <a href="assets/img/blog/blog-03.jpg">
-                <img
-                  className="img-thumbnail"
-                  src="/images/clinic/image3.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 m-b-20">
-              <a href="assets/img/blog/blog-04.jpg">
-                <img
-                  className="img-thumbnail"
-                  src="/images/clinic/image4.jpg"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
+
+          <>
+            {/* /Page Header */}
+            {isLoading ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                {data ? (
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <div className="card card-table show-entire">
+                        <div className="card-body">
+                          {isLoading ? (
+                            <LoadingSpinner />
+                          ) : (
+                            <div className="table-responsive">
+                              <table className="table border-0 custom-table comman-table datatable mb-0">
+                                <thead>
+                                  <tr>
+                                    <th>Analysis Name</th>
+                                    <th>Price</th>
+                                    <th />
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {analysis?.map((analysis) => (
+                                    <AnalysisTuple
+                                      analysis={analysis}
+                                      onOpenModal={handleOpenModal}
+                                      onChangeActiveAnalysis={setActiveAnalysis}
+                                      key={analysis._id}
+                                    />
+                                  ))}
+                                </tbody>
+                              </table>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className=" container-fluid ">
+                    <div className="col-xl-12 ">
+                      <div className="card invoice-info-card">
+                        <div className="card-boyd">
+                          <div
+                            className="text-center mt-5 py-5"
+                            style={{
+                              padding: "0px 10%",
+                              backgroundColor: "#fff",
+                            }}
+                          >
+                            <div className="reminder-icon">
+                              <img
+                                alt="medicine"
+                                src="/images/dashborad/analysis.webp"
+                                style={{ width: "100%" }}
+                              />
+                            </div>
+                            <h4>No Analysis are Found</h4>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+
+            {isOpen && (
+              <DetailsModal
+                analysis={activeAnalysis}
+                onOpenModal={handleOpenModal}
+              />
+            )}
+          </>
         </div>
       </div>
     </div>
