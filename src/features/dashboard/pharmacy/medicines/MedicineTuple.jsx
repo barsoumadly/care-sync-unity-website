3b25d6afa17121ec868bb3b3ee4fb4551cc17f7d
Barsoum Adly <barsoumadly@gmail.com>
2025-03-12 16:20:20 +0200

    medicine tuple

--- src/features/dashboard/pharmacy/medicines/MedicineTuple.jsx
+++ src/features/dashboard/pharmacy/medicines/MedicineTuple.jsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { deleteMedicine } from "../../../../services/medicine";
 
 function MedicineTuple({ medicine }) {
   const navigate = useNavigate();
+
+  const handleDelete = async function (id) {
+    await deleteMedicine(id);
+  };
 
   return (
     <tr>
@@ -10,12 +15,22 @@ function MedicineTuple({ medicine }) {
       <td>{medicine.price} EGP</td>
       <td>{medicine.expirationDate.split("-").reverse().join("-")}</td>
       <td>
-        <div class="dropdown action-label">
+        <div className="dropdown action-label">
           <button
-            class="custom-badge book-btn"
+            className="custom-badge book-btn"
+            style={{ marginRight: "20px" }}
             onClick={() => navigate(`/pharmacy/edit-medicine/${medicine._id}`)}
           >
             Edit
+          </button>
+        </div>
+        <div className="dropdown action-label">
+          <button
+            className="custom-badge book-btn"
+            style={{ backgroundColor: "#f03d32" }}
+            onClick={() => handleDelete(medicine._id)}
+          >
+            Delete
           </button>
         </div>
       </td>
