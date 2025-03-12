c25b1142ab1f1412587f57b7be38c6ce0094b4e4
Barsoum Adly <barsoumadly@gmail.com>
2025-03-12 16:09:44 +0200

    analysis service

---
+++ src/services/analysis.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/v1/analysis";
+
+const addAnalysisList = async function (analysisList) {
+  const token = JSON.parse(localStorage.getItem("key"));
+
+  const analysis = analysisList.map((analysis) => {
+    return {
+      name: analysis.analysis_name,
+      price: Number(analysis.price),
+      details: analysis.details,
+    };
+  });
+
+  try {
+    await axios.post(`${API_URL}/`, analysis, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAnalysisList = async function () {
+  const token = JSON.parse(localStorage.getItem("key"));
+  try {
+    const response = await axios.get(`${API_URL}/`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const editAnalysisDetails = async function (analysisId, analysisData) {
+  const token = JSON.parse(localStorage.getItem("key"));
+
+  try {
+    const response = await axios.put(
+      `${API_URL}/`,
+      { analysisId, analysisData },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteAnalysis = async function (analysisId) {
+  const token = JSON.parse(localStorage.getItem("key"));
+
+  try {
+    const response = await axios.delete(`${API_URL}/${analysisId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export {
+  addAnalysisList,
+  getAnalysisList,
+  editAnalysisDetails,
+  deleteAnalysis,
+};
