1355e606365a675af9ed04a922340a1366f2b26a
Barsoum Adly <barsoumadly@gmail.com>
2025-03-13 17:31:55 +0200

    medicine tuple

--- src/features/dashboard/patient/medicines/MedicineTuple.jsx
+++ src/features/dashboard/patient/medicines/MedicineTuple.jsx
@@ -2,27 +2,9 @@ function MedicineTuple({ medicine }) {
   return (
     <tr>
       <td>{medicine.name}</td>
-      <td>{medicine.pharmacyName}</td>
       <td>{medicine.expirationDate}</td>
-      <td>{medicine.date}</td>
-      <td>{medicine.cost}</td>
-      <td>
-        <div className="dropdown action-label">
-          <a
-            className={`custom-badge ${
-              medicine.status === "on way"
-                ? "status-orange"
-                : medicine.status === "delivered"
-                ? "status-green"
-                : "status-blue"
-            } `}
-          >
-            {medicine.status === "on way" && "On The Way"}
-            {medicine.status === "delivered" && "Delivered"}
-            {medicine.status === "obtained" && "Obtained"}
-          </a>
-        </div>
-      </td>
+      <td>{medicine.quantity} Pieces</td>
+      <td>{medicine.price} EGP</td>
     </tr>
   );
 }
