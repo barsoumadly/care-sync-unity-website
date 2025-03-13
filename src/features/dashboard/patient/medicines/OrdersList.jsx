d283b69d25a5e4f29219f1e6647d81a2d3fe3307
Barsoum Adly <barsoumadly@gmail.com>
2025-03-13 17:32:16 +0200

    order list

---
+++ src/features/dashboard/patient/medicines/OrdersList.jsx
@@ -0,0 +1,82 @@
+import { IoArrowBackOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import useMedicineOrders from "./useMedicineOrders";
+import OrderTuple from "./OrderTuple";
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
+
+function OrdersList() {
+  const { isLoading, data } = useMedicineOrders();
+
+  console.log(data);
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <div className="page-wrapper">
+          <div className="content">
+            {/* Page Header */}
+            <div className="page-header">
+              <div className="row">
+                <div className="col-sm-7 col-6">
+                  <ul className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <a>Medicine Orders</a>
+                    </li>
+                    <li className="breadcrumb-item">
+                      <i className="feather-chevron-right" />
+                    </li>
+                    <li className="breadcrumb-item active">Orders List</li>
+                  </ul>
+                </div>
+                <div class="col-sm-5 col-6 text-end">
+                  <Link
+                    to="/patient/dashboard"
+                    class="btn btn-primary btn-rounded"
+                  >
+                    <IoArrowBackOutline /> Return Back
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* /Page Header */}
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="card card-table show-entire">
+                  <div className="card-body">
+                    <div className="table-responsive">
+                      {isLoading ? (
+                        <LoadingSpinner />
+                      ) : (
+                        <table className="table border-0 custom-table comman-table datatable mb-0">
+                          <thead>
+                            <tr>
+                              <th>Order Number</th>
+                              <th>Pharmacy Name</th>
+                              <th>Status</th>
+                              <th />
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {data?.map((order, index) => (
+                              <OrderTuple
+                                order={order}
+                                key={order._id}
+                                orderNumber={index + 1}
+                              />
+                            ))}
+                          </tbody>
+                        </table>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default OrdersList;
