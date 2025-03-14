c185cb84dcce24ef21e5a583fa3c8b747f0699b3
bishoy labib <bishoylabib78@gmail.com>
2025-03-14 17:40:12 +0200

    Patients

---
+++ src/features/dashboard/doctor/Patients/PatientList/Patients.jsx
@@ -0,0 +1,85 @@
+import DashboardPageHeader from "../../DashboardPageHeader";
+import SearchBar from "../../SearchBar";
+import Table from "./Table";
+import PatientTuples from "./PatientTuples";
+
+const patientList = [
+  {
+    id: 1,
+    name: "Andrea Lalema",
+    specialization: "Cardiology",
+  },
+  {
+    id: 2,
+    name: "Smith Bruklin",
+    specialization: "Neurology",
+  },
+  {
+    id: 3,
+    name: "Bernardo James	",
+    specialization: "Oncology",
+  },
+  {
+    id: 4,
+    name: "Bishoy Labib",
+    specialization: "Orthopedics",
+  },
+];
+
+function Patients() {
+  return (
+    <div className="main-wrapper">
+      <div className="page-wrapper">
+        <div className="content">
+          {/* Page Header */}
+          <DashboardPageHeader
+            pageList={[
+              { name: "Patients", link: "/doctor/dashboard" },
+              { name: "Clinics List", link: "/doctor/patients" },
+            ]}
+            currentPage="Patient List"
+          />
+          {/* /Page Header */}
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="card card-table show-entire">
+                <div className="card-body">
+                  {/* Table Header */}
+                  <SearchBar name="Patients List" />
+                  {/* /Table Header */}
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>Num</th>
+                        <th>Name</th>
+                        <th>Specialization</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {patientList.map((element, index) => (
+                        <tr key={element.id}>
+                          <PatientTuples
+                            num={index}
+                            element={element}
+                            button={{
+                              name: "Examination",
+                              link: "/doctor/patient-profile",
+                            }}
+                          />
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Patients;
