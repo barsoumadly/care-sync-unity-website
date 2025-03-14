12cfbbece59cc61658fd40a72fab0b124a5d914d
bishoy labib <bishoylabib78@gmail.com>
2025-03-14 17:45:47 +0200

    useEditDoctorProfile

--- src/features/dashboard/doctor/complete-profile/useEditDoctorProfile.js
+++ src/features/dashboard/doctor/complete-profile/useEditDoctorProfile.js
@@ -1,15 +1,10 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { updatePatientProfile } from "../../../../services/patient";
-import { updateProfilePhoto } from "../../../../services/user";
+import { useMutation } from "@tanstack/react-query";
 
 function useEditDoctorProfile() {
-  const queryClient = useQueryClient();
-
   const { mutate: updateDoctorProfile, isDataLoading } = useMutation({
     // mutationFn: ({ doctorData }) => updateDoctorProfile(doctorData),
     onSuccess: () => {
       toast.success("Your profile successfully saved");
-      queryClient.invalidateQueries({ queryKey: ["doctorProfile"] });
     },
     onError: (err) => toast.error(err.message),
   });
