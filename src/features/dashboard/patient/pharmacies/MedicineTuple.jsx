168d444864f3f1b010fa82ffa5f627131e697aa4
Barsoum Adly <barsoumadly@gmail.com>
2025-03-14 16:05:58 +0200

    medicine tuple

---
+++ src/features/dashboard/patient/pharmacies/MedicineTuple.jsx
@@ -0,0 +1,11 @@
+function MedicineTuple({ medicine }) {
+  return (
+    <tr>
+      <td>{medicine.name}</td>
+      <td>{medicine.quantity} Pieces</td>
+      <td>{medicine.price} EGP</td>
+    </tr>
+  );
+}
+
+export default MedicineTuple;
