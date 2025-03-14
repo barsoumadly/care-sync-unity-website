abed113cbadcc01fa7ef175fae918439cc693519
Barsoum Adly <barsoumadly@gmail.com>
2025-03-14 16:04:07 +0200

    medicine list

--- src/features/dashboard/patient/pharmacies/MedicinesList.jsx
+++ src/features/dashboard/patient/pharmacies/MedicinesList.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 import PharmacyHeader from "./pharmacyHeader";
 import MedicineField from "./MedicineField";
 import OrderModal from "./OrderModal";
+import useMedicines from "./useMedicines";
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
+import { useNavigate, useParams } from "react-router-dom";
+import usePharmacyProfile from "./usePharmacyProfile";
 
 const medicinesList = [
-  { name: "Panadol", photo: "", expirationDate: "08-05-2030", cost: 100 },
+  { name: "Panadol", expirationDate: "08-05-2030", cost: 100 },
   { name: "Bronchicum", expirationDate: "12-09-2030", cost: 69 },
 ];
 
 function MedicinesList() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedMedicine, setSelectedMedicine] = useState({});
+  const [medicineOrders, setMedicineOrders] = useState([]);
+  const { id } = useParams();
+  const { data: pharmacy } = usePharmacyProfile(id);
+  const { isLoading, data } = useMedicines(pharmacy?.userId);
+
+  const medicinesNames = medicinesList.map((medicine) => medicine?.name);
+  const medicines = data?.medicines.filter((medicine) =>
+    medicinesNames.includes(medicine.name)
+  );
+
+  const navigate = useNavigate();
 
   const handleOpenModal = function () {
     setIsOpen((isOpen) => !isOpen);
@@ -20,12 +35,29 @@ function MedicinesList() {
     setSelectedMedicine(medicine);
   };
 
+  const handleCancel = function () {
+    setMedicineOrders([]);
+  };
+
+  const handleCheckOrders = function () {
+    localStorage.setItem(
+      "order",
+      JSON.stringify({
+        pharmacyId: pharmacy._id,
+        medicines: medicineOrders,
+        pharmacyName: pharmacy.name,
+      })
+    );
+
+    navigate("/patient/check-order");
+  };
+
   return (
     <>
       <div className="main-wrapper">
         <div className="page-wrapper">
           <div className="content">
-            <PharmacyHeader activeTab="medicines" />
+            <PharmacyHeader activeTab="medicines" pharmacy={pharmacy} />
             {/* Page Header */}
             <div className="page-header"></div>
             {/* /Page Header */}
@@ -34,30 +66,54 @@ function MedicinesList() {
                 <div className="card card-table show-entire">
                   <div className="card-body">
                     <div className="table-responsive">
-                      <table className="table border-0 custom-table comman-table datatable mb-0">
-                        <thead>
-                          <tr>
-                            <th>Medicine Name</th>
-                            <th>Expiration Date</th>
-                            <th>Price</th>
-                            <th />
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {medicinesList.map((medicine) => (
-                            <MedicineField
-                              medicine={medicine}
-                              onOpenModal={handleOpenModal}
-                              key={medicine.id}
-                              onSelectMedicine={handleSelectMedicine}
-                            />
-                          ))}
-                        </tbody>
-                      </table>
+                      {isLoading ? (
+                        <LoadingSpinner />
+                      ) : (
+                        <table className="table border-0 custom-table comman-table datatable mb-0">
+                          <thead>
+                            <tr>
+                              <th>Medicine Name</th>
+                              <th>Expiration Date</th>
+                              <th>Price</th>
+                              <th />
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {medicines?.map((medicine) => (
+                              <MedicineField
+                                medicine={medicine}
+                                onOpenModal={handleOpenModal}
+                                key={medicine.id}
+                                onSelectMedicine={handleSelectMedicine}
+                              />
+                            ))}
+                          </tbody>
+                        </table>
+                      )}
                     </div>
                   </div>
                 </div>
               </div>
+              {medicineOrders.length > 0 && (
+                <div className="col-12">
+                  <div class="doctor-submit text-end">
+                    <button
+                      type="submit"
+                      className="btn btn-primary submit-form me-2"
+                      onClick={handleCheckOrders}
+                    >
+                      Check Orders
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-primary cancel-form"
+                      onClick={handleCancel}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -67,6 +123,7 @@ function MedicinesList() {
           <OrderModal
             medicine={selectedMedicine}
             onOpenModal={handleOpenModal}
+            onSetOrders={setMedicineOrders}
           />
         )}
       </div>
