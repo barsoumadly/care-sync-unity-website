bd9032a9c4ed8e1509d873284c6c30de8e7e3714
bishoy labib <bishoylabib78@gmail.com>
2025-03-15 15:43:46 +0200

    DoctorPersonalDetails

--- src/features/dashboard/doctor/complete-profile/PersonalDetails.jsx
+++ src/features/dashboard/doctor/complete-profile/PersonalDetails.jsx
@@ -9,17 +9,21 @@ function telephoneCheck(p) {
 }
 
 function DoctorPersonalDetails({
-  // doctorData,
-  // onChangeDoctorData,
+  profilePhoto = "https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg",
+  setProfilePhoto,
   onChangePageNumber,
   type,
   register,
   errors,
 }) {
+  const performSubmit = function (event) {
+    const photo = URL.createObjectURL(event.target.files[0]);
+    setProfilePhoto(photo);
+  };
   return (
     <>
       <div className="row">
-        <div className="col-12" style={{ marginTop: "50px" }}>
+        <div className="col-12">
           <div className="form-heading">
             <h4>
               <span style={{ fontSize: "15px" }}>
@@ -28,78 +32,109 @@ function DoctorPersonalDetails({
             </h4>
           </div>
         </div>
-        <div className="col-12 col-md-6 col-xl-3">
-          <div className="input-block local-forms">
-            <label>
-              Phone Number <span className="login-danger">*</span>
-            </label>
-            <input
-              className="form-control"
-              type="text"
-              placeholder="ex: 0123456789"
-              {...register("phoneNumber", {
-                required: "This field is required",
-                validate: (value) =>
-                  telephoneCheck(value) || "Your Phone number isn't Vaild",
-              })}
-            />
-            <span className="error-message ">
-              {errors?.phoneNumber?.message}
-            </span>
+        {type === "edit" && (
+          <div className="col-md-12">
+            <div className="profile-img-wrap">
+              <img
+                className="inline-block"
+                src={profilePhoto}
+                // src="\images\dashborad\user.jpg"
+                alt="user"
+              />
+              <div className="fileupload btn">
+                <span className="btn-text">edit</span>
+                <input
+                  className="upload"
+                  type="file"
+                  accept="image/*"
+                  name="image"
+                  id="file"
+                  onChange={performSubmit}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col-12 col-md-6 col-xl-3">
-          <div className="input-block local-forms">
-            <label>
-              Age <span className="login-danger">*</span>
-            </label>
-            <input
-              className="form-control"
-              type="number"
-              placeholder="ex: 30"
-              {...register("age", { required: "This field is required" })}
-            />
-            <span className="error-message ">{errors?.age?.message}</span>
+        )}
+        <div className={`${type === "edit" ? "profile-basic" : ""}`}>
+          <div className="row">
+            <div className="col-12 col-md-6 col-xl-5">
+              <div className="input-block local-forms">
+                <label>
+                  Phone Number <span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="ex: 0123456789"
+                  {...register("phoneNumber", {
+                    required: "This field is required",
+                    validate: (value) =>
+                      telephoneCheck(value) || "Your Phone number isn't Vaild",
+                  })}
+                />
+                <span className="error-message ">
+                  {errors?.phoneNumber?.message}
+                </span>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-xl-5">
+              <div className="input-block local-forms">
+                <label className="focus-label">
+                  Birth Date<span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control floating datetimepicker"
+                  type="date"
+                  {...register("birthDate", {
+                    required: "This field is required",
+                  })}
+                />
+
+                <span className="error-message ">{errors?.age?.message}</span>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-xl-5">
+              <div className="input-block local-forms">
+                <label>
+                  Gender <span className="login-danger">*</span>
+                </label>
+                <select
+                  className="form-control"
+                  {...register("gender", {
+                    required: "This field is required",
+                  })}
+                >
+                  <option value="select-option" disabled>
+                    Select Option
+                  </option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+              <span className="error-message ">{errors?.gender?.message}</span>
+            </div>
+            <div className="col-12 col-md-6 col-xl-5">
+              <div className="input-block local-forms">
+                <label>
+                  Specialization <span className="login-danger">*</span>
+                </label>
+                <select
+                  className="form-control select"
+                  {...register("specialization", {
+                    required: "This field is required",
+                  })}
+                >
+                  <option value="Specialization" disabled>
+                    Specialization
+                  </option>
+                  <option value="orthopedics">Orthopedics</option>
+                  <option value="radiology">Radiology</option>
+                  <option>Dentist</option>
+                </select>
+              </div>{" "}
+              <span className="error-message ">{errors?.gender?.message}</span>
+            </div>{" "}
           </div>
-        </div>
-        <div className="col-12 col-md-6 col-xl-3">
-          <div className="input-block local-forms">
-            <label>
-              Gender <span className="login-danger">*</span>
-            </label>
-            <select
-              className="form-control"
-              {...register("gender", { required: "This field is required" })}
-            >
-              <option value="select-option" disabled>
-                Select Option
-              </option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
-          <span className="error-message ">{errors?.gender?.message}</span>
-        </div>
-        <div className="col-12 col-md-6 col-xl-3">
-          <div className="input-block local-forms">
-            <label>
-              Specialization <span className="login-danger">*</span>
-            </label>
-            <select
-              className="form-control select"
-              {...register("specialization", {
-                required: "This field is required",
-              })}
-            >
-              <option value="Specialization" disabled>
-                Specialization
-              </option>
-              <option value="orthopedics">Orthopedics</option>
-              <option value="radiology">Radiology</option>
-              <option>Dentist</option>
-            </select>
-          </div>{" "}
-          <span className="error-message ">{errors?.gender?.message}</span>
         </div>
       </div>
       <div className="col-12 col-sm-12">
