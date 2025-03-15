e845fe97293ec413500e80d4d60fddaf62d8c5a4
Barsoum Adly <barsoumadly@gmail.com>
2025-03-15 19:42:08 +0200

    patient dashboard contents

--- src/features/dashboard/patient/DashboardContents.jsx
+++ src/features/dashboard/patient/DashboardContents.jsx
@@ -1,9 +1,21 @@
 import useProfile from "./useProfile";
 import SpinnerMini from "../../../ui/SpinnerMini";
 import toast from "react-hot-toast";
+import useMedicineOrders from "./medicines/useMedicineOrders";
+import OrderTuple from "./medicines/OrderTuple";
+import AnalysisTuple from "./analysis/analysisTuple";
+import LoadingSpinner from "../../../ui/LoadingSpinner";
+import { Link } from "react-router-dom";
+import useAnalysisOrders from "./analysis/useAnalysisOrders";
 
 function DashboardContents() {
   const { data: patientData, isLoading, error } = useProfile();
+  const { data: medicines, isLoading: isLoad1 } = useMedicineOrders();
+  const { isLoading: isLoad2, data: analysis } = useAnalysisOrders();
+  const medicineOrders = isLoad1 ? [] : [...medicines];
+  const analysisOrders = isLoad2 ? [] : [...analysis];
+
+  console.log(analysisOrders);
 
   if (error) toast.error(error?.message);
 
@@ -110,12 +122,13 @@ function DashboardContents() {
       </div>
 
       <div className="row">
-        <div className="col-12 col-md-12  col-xl-6">
+        <div className="col-12 col-md-12  col-xl-4">
           <div className="card">
             <div className="card-body">
               <div className="chart-title patient-visit">
-                <h4>
+                <h4 style={{ marginBottom: "3px" }}>
                   Body Mass index
+                  <br />
                   <br />
                   <br />
                   <span
@@ -191,11 +204,15 @@ function DashboardContents() {
             </div>
           </div>
         </div>
-        <div className="col-12 col-md-12  col-xl-15">
+
+        <div className="col-12 col-md-12  col-xl-8">
           <div className="card">
             <div className="card-header">
               <h4 className="card-title d-inline-block">
                 Appointments History
+                <br />
+                <br />
+                <br />
               </h4>
               {/* <Link
                 to="/patient/appointments"
@@ -277,6 +294,165 @@ function DashboardContents() {
                     </tr>
                   </tbody>
                 </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-12 col-md-12  col-xl-8">
+          <div className="card">
+            <div className="card-body">
+              <div className="chart-title patient-visit">
+                <h4>Medicine Orders</h4>
+                <Link
+                  to="/patient/medicine-orders"
+                  style={{ color: "rgb(46, 55, 164)" }}
+                >
+                  Show all
+                </Link>
+              </div>
+              <div className="body-mass-blk">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="table-responsive">
+                      {isLoad1 ? (
+                        <LoadingSpinner />
+                      ) : medicineOrders?.length !== 0 ? (
+                        <table className="table border-0 custom-table comman-table datatable mb-0">
+                          <thead>
+                            <tr>
+                              <th>Order Number</th>
+                              <th>Pharmacy Name</th>
+                              <th>Date</th>
+                              <th>Status</th>
+                              <th />
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {medicineOrders?.reverse()?.map((order, index) => (
+                              <OrderTuple
+                                order={order}
+                                key={order._id}
+                                orderNumber={index + 1}
+                              />
+                            ))}
+                          </tbody>
+                        </table>
+                      ) : (
+                        <div className=" container-fluid ">
+                          <div className="col-xl-12 ">
+                            <div className="card invoice-info-card">
+                              <div className="card-boyd">
+                                <div
+                                  className="text-center mt-5 py-5"
+                                  style={{
+                                    padding: "0px 10%",
+                                    backgroundColor: "#fff",
+                                  }}
+                                >
+                                  <div className="reminder-icon">
+                                    <img
+                                      alt="medicine"
+                                      src="/images/dashborad/order.png"
+                                      style={{
+                                        width: "180%",
+                                        marginTop: "-90px",
+                                        marginLeft: "-40px",
+                                      }}
+                                    />
+                                  </div>
+                                  <h4>No orders are Found</h4>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-12  col-xl-8">
+          <div className="card">
+            <div className="card-body">
+              <div className="chart-title patient-visit">
+                <h4>Analysis Results</h4>
+                <Link
+                  to="/patient/analysis-results"
+                  style={{ color: "rgb(46, 55, 164)" }}
+                >
+                  Show all
+                </Link>
+              </div>
+              <div className="body-mass-blk">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="table-responsive">
+                      {isLoading ? (
+                        <LoadingSpinner />
+                      ) : analysisOrders?.length !== 0 ? (
+                        <table className="table border-0 custom-table comman-table datatable mb-0">
+                          <thead>
+                            <tr>
+                              <th>Analysis Number</th>
+                              <th>Laboratory Name</th>
+                              <th>Date</th>
+                              <th>Status</th>
+                              <th />
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {analysisOrders
+                              ?.reverse()
+                              ?.reverse()
+                              ?.map((analysis, index) => (
+                                <AnalysisTuple
+                                  analysis={analysis}
+                                  key={analysis._id}
+                                  analysisNumber={index + 1}
+                                />
+                              ))}
+                          </tbody>
+                        </table>
+                      ) : (
+                        <div className=" container-fluid ">
+                          <div className="col-xl-12 ">
+                            <div className="card invoice-info-card">
+                              <div className="card-boyd">
+                                <div
+                                  className="text-center mt-5 py-5"
+                                  style={{
+                                    padding: "0px 10%",
+                                    backgroundColor: "#fff",
+                                  }}
+                                >
+                                  <div className="reminder-icon">
+                                    <img
+                                      alt="medicine"
+                                      src="/images/dashborad/analysis.png"
+                                      style={{
+                                        width: "150%",
+                                        marginTop: "-80px",
+                                        marginLeft: "-30px",
+                                      }}
+                                    />
+                                  </div>
+                                  <h4>No Results are Found</h4>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
