eb46a2a2a0690a58ce3105f94444e6ecd4f85028
Barsoum Adly <barsoumadly@gmail.com>
2025-03-15 16:29:24 +0200

    order list

--- src/features/dashboard/patient/medicines/OrdersList.jsx
+++ src/features/dashboard/patient/medicines/OrdersList.jsx
@@ -5,7 +5,8 @@ import OrderTuple from "./OrderTuple";
 import LoadingSpinner from "../../../../ui/LoadingSpinner";
 
 function OrdersList() {
-  const { isLoading, data } = useMedicineOrders();
+  const { isLoading, data: orders } = useMedicineOrders();
+  const data = orders ? [...orders] : [];
 
   return (
     <>
@@ -41,21 +42,48 @@ function OrdersList() {
               <div className="col-sm-12">
                 <div className="card card-table show-entire">
                   <div className="card-body">
+                    <div className="staff-search-table">
+                      <form>
+                        <div className="row">
+                          <div className="col-12 col-md-6 col-xl-3">
+                            <div className="input-block local-forms">
+                              <label>Pharmacy Name </label>
+                              <input
+                                className="form-control"
+                                type="text"
+                                placeholder="ex. Royal"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-12 col-md-6 col-xl-3">
+                            <div className="doctor-submit">
+                              <button
+                                type="submit"
+                                className="btn btn-primary submit-list-form me-2"
+                              >
+                                Search
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                     <div className="table-responsive">
                       {isLoading ? (
                         <LoadingSpinner />
-                      ) : (
+                      ) : data.length !== 0 ? (
                         <table className="table border-0 custom-table comman-table datatable mb-0">
                           <thead>
                             <tr>
                               <th>Order Number</th>
                               <th>Pharmacy Name</th>
+                              <th>Date</th>
                               <th>Status</th>
                               <th />
                             </tr>
                           </thead>
                           <tbody>
-                            {data?.map((order, index) => (
+                            {data?.reverse()?.map((order, index) => (
                               <OrderTuple
                                 order={order}
                                 key={order._id}
@@ -64,6 +92,35 @@ function OrdersList() {
                             ))}
                           </tbody>
                         </table>
+                      ) : (
+                        <div className=" container-fluid ">
+                          <div className="col-xl-12 ">
+                            <div className="card invoice-info-card">
+                              <div className="card-boyd">
+                                <div
+                                  className="text-center mt-5 py-5"
+                                  style={{
+                                    padding: "0px 10%",
+                                    backgroundColor: "#fff",
+                                  }}
+                                >
+                                  <div className="reminder-icon">
+                                    <img
+                                      alt="medicine"
+                                      src="/images/dashborad/order.png"
+                                      style={{
+                                        width: "180%",
+                                        marginTop: "-90px",
+                                        marginLeft: "-40px",
+                                      }}
+                                    />
+                                  </div>
+                                  <h4>No orders are Found</h4>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       )}
                     </div>
                   </div>
