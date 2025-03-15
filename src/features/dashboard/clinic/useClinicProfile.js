5bf260b53cc0ac7af038d8aec20351242759b21c
bishoy labib <bishoylabib78@gmail.com>
2025-03-15 21:54:06 +0200

    useClinicProfile

--- src/features/dashboard/clinic/useClinicProfile.js
+++ src/features/dashboard/clinic/useClinicProfile.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+import { getClinicProfile } from "../.././../services/clinic";
 
 function useClinicProfile() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["clinicProfile"],
-    // queryFn:
+    queryFn: getClinicProfile,
   });
   return { data, error, isLoading };
 }
