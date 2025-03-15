eaec7133121111dbf112ffbfbf5583419c90dbc8
bishoy labib <bishoylabib78@gmail.com>
2025-03-15 13:00:32 +0200

    new Sidebar with hover

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,16 +1,28 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { logout } from "../services/auth";
+import { useState } from "react";
 
 function Sidebar({ minbar, showbar, children }) {
   const { userLogout } = useAuth();
 
+  const [ishover, sethover] = useState(true);
+
+  function MouseOver() {
+    sethover(false);
+  }
+  function MouseOut() {
+    sethover(true);
+  }
+
   return (
-    <div className={minbar ? "mini-sidebar" : ""}>
+    <div className={minbar && ishover ? "mini-sidebar" : ""}>
       <div
         className="sidebar"
         id="sidebar"
         style={{ marginLeft: `${showbar ? "0px" : "-225px"}` }}
+        onMouseOver={MouseOver}
+        onMouseOut={MouseOut}
       >
         <div className="sidebar-inner slimscroll scroll">
           <div id="sidebar-menu" className="sidebar-menu">
