795e349c35ca0dceca44b9424854b0bd95bb2332
bishoy labib <bishoylabib78@gmail.com>
2025-03-15 15:43:36 +0200

    complete profile

--- src/features/dashboard/doctor/complete-profile/CompleteDoctorProfile.jsx
+++ src/features/dashboard/doctor/complete-profile/CompleteDoctorProfile.jsx
@@ -68,134 +68,136 @@ function CompleteDoctorProfile() {
                 {pageNumber === 3 && "Profile Photo"}
               </h4>
             </div>
-            <div className="card-body">
-              <div className="wizard" style={{ marginLeft: "-50px" }}>
-                <ul
-                  className="nav nav-tabs justify-content-center"
-                  id="myTab"
-                  role="tablist"
+
+            <div className="wizard" style={{ marginLeft: "-50px" }}>
+              <ul
+                className="nav nav-tabs justify-content-center"
+                id="myTab"
+                role="tablist"
+              >
+                <li
+                  className="nav-item flex-fill"
+                  role="presentation"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  aria-label="Seller Details"
+                  data-bs-original-title="Seller Details"
                 >
-                  <li
-                    className="nav-item flex-fill"
-                    role="presentation"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    aria-label="Seller Details"
-                    data-bs-original-title="Seller Details"
+                  <a
+                    className={`nav-link ${
+                      pageNumber === 1 && "active"
+                    } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                    id="step1-tab"
+                    data-bs-toggle="tab"
+                    role="tab"
+                    aria-controls="step1"
+                    aria-selected="true"
                   >
-                    <a
-                      className={`nav-link ${
-                        pageNumber === 1 && "active"
-                      } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                      id="step1-tab"
-                      data-bs-toggle="tab"
-                      role="tab"
-                      aria-controls="step1"
-                      aria-selected="true"
-                    >
-                      <IoPersonSharp />
-                    </a>
-                  </li>
-                  <li
-                    className="nav-item flex-fill"
-                    role="presentation"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    aria-label="Company Document"
-                    data-bs-original-title="Company Document"
+                    <IoPersonSharp />
+                  </a>
+                </li>
+                <li
+                  className="nav-item flex-fill"
+                  role="presentation"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  aria-label="Company Document"
+                  data-bs-original-title="Company Document"
+                >
+                  <a
+                    className={`nav-link ${
+                      pageNumber === 2 && "active"
+                    } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                    id="step2-tab"
+                    data-bs-toggle="tab"
+                    role="tab"
+                    aria-controls="step2"
+                    aria-selected="false"
                   >
-                    <a
-                      className={`nav-link ${
-                        pageNumber === 2 && "active"
-                      } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                      id="step2-tab"
-                      data-bs-toggle="tab"
-                      role="tab"
-                      aria-controls="step2"
-                      aria-selected="false"
-                    >
-                      <FaKitMedical />
-                    </a>
-                  </li>
-                  <li
-                    className="nav-item flex-fill"
-                    role="presentation"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    aria-label="Bank Details"
-                    data-bs-original-title="Bank Details"
+                    <FaKitMedical />
+                  </a>
+                </li>
+                <li
+                  className="nav-item flex-fill"
+                  role="presentation"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  aria-label="Bank Details"
+                  data-bs-original-title="Bank Details"
+                >
+                  <a
+                    className={`nav-link ${
+                      pageNumber === 3 && "active"
+                    } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
+                    href="#step3"
+                    id="step3-tab"
+                    data-bs-toggle="tab"
+                    role="tab"
+                    aria-controls="step3"
+                    aria-selected="false"
                   >
-                    <a
-                      className={`nav-link ${
-                        pageNumber === 3 && "active"
-                      } rounded-circle mx-auto d-flex align-items-center justify-content-center`}
-                      href="#step3"
-                      id="step3-tab"
-                      data-bs-toggle="tab"
-                      role="tab"
-                      aria-controls="step3"
-                      aria-selected="false"
-                    >
-                      <BsFillImageFill />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <form onSubmit={handleSubmit(performSubmit)}>
-                {pageNumber === 1 && (
-                  <DoctorPersonalDetails
-                    register={register}
-                    errors={errors}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-
-                {pageNumber === 2 && (
-                  <DoctorProfessionalDetails
-                    register={register}
-                    control={control}
-                    errors={errors}
-                    onChangePageNumber={setPageNumber}
-                  />
-                )}
-
-                {pageNumber === 3 && (
-                  <>
-                    <DoctorProfilePhoto
-                      profilePhoto={profilePhoto}
-                      setProfilePhoto={setProfilePhoto}
-                    />
-
-                    <div className="row">
-                      <div className="col-12 col-md-6 col-xl-1">
-                        <div className="doctor-submit text-end">
-                          <button
-                            type="submit"
-                            className="btn btn-primary submit-form me-2"
-                            onClick={() =>
-                              setPageNumber((pageNumber) => pageNumber - 1)
-                            }
-                          >
-                            Back
-                          </button>
-                        </div>
-                      </div>
-                      <div className="col-12 col-md-6 col-xl-11">
-                        <div className="doctor-submit text-end">
-                          <button
-                            type="submit"
-                            className="btn btn-primary submit-form me-2"
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                )}
-              </form>
+                    <BsFillImageFill />
+                  </a>
+                </li>
+              </ul>
             </div>
           </PageCard>
+
+          <form onSubmit={handleSubmit(performSubmit)}>
+            {pageNumber === 1 && (
+              <PageCard>
+                <DoctorPersonalDetails
+                  register={register}
+                  errors={errors}
+                  onChangePageNumber={setPageNumber}
+                />{" "}
+              </PageCard>
+            )}
+
+            {pageNumber === 2 && (
+              <DoctorProfessionalDetails
+                register={register}
+                control={control}
+                errors={errors}
+                onChangePageNumber={setPageNumber}
+              />
+            )}
+
+            {pageNumber === 3 && (
+              <PageCard>
+                <DoctorProfilePhoto
+                  profilePhoto={profilePhoto}
+                  setProfilePhoto={setProfilePhoto}
+                />
+
+                <div className="row">
+                  <div className=" col-md-6 col-xl-1" style={{ width: "50%" }}>
+                    <div className="doctor-submit ">
+                      <button
+                        type="submit"
+                        className="btn btn-primary submit-form me-2"
+                        onClick={() =>
+                          setPageNumber((pageNumber) => pageNumber - 1)
+                        }
+                      >
+                        Back
+                      </button>
+                    </div>
+                  </div>
+                  <div className=" col-md-6 col-xl-11" style={{ width: "50%" }}>
+                    <div className="doctor-submit text-end">
+                      <button
+                        type="submit"
+                        className="btn btn-primary submit-form me-2"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </PageCard>
+            )}
+          </form>
         </div>
       </PageWrapper>
     </>
