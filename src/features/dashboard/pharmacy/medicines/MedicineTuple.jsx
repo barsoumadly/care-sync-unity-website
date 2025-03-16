f8c349a0eaa40925e4b67ae61b1dc70f1b85d4cf
Barsoum Adly <barsoumadly@gmail.com>
2025-03-16 18:06:14 +0200

    medicine tuple

--- src/features/dashboard/pharmacy/medicines/MedicineTuple.jsx
+++ src/features/dashboard/pharmacy/medicines/MedicineTuple.jsx
@@ -1,39 +1,64 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { deleteMedicine } from "../../../../services/medicine";
+import { CgMoreVerticalAlt } from "react-icons/cg";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { useState } from "react";
+import useMedicineDelete from "./useMedicineDelete";
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
 
 function MedicineTuple({ medicine }) {
-  const navigate = useNavigate();
+  const { removeMedicine, isDataLoading } = useMedicineDelete();
+  const [dropdown, setDropdown] = useState(false);
 
-  const handleDelete = async function (id) {
-    await deleteMedicine(id);
+  const handleDelete = function (id) {
+    removeMedicine({ id });
   };
 
   return (
     <tr>
-      <td>{medicine.name}</td>
-      <td>{medicine.quantity} Pieces</td>
-      <td>{medicine.price} EGP</td>
-      <td>{medicine.expirationDate.split("-").reverse().join("-")}</td>
-      <td>
-        <div className="dropdown action-label">
-          <button
-            className="custom-badge book-btn"
-            style={{ marginRight: "20px" }}
-            onClick={() => navigate(`/pharmacy/edit-medicine/${medicine._id}`)}
-          >
-            Edit
-          </button>
-        </div>
-        <div className="dropdown action-label">
-          <button
-            className="custom-badge book-btn"
-            style={{ backgroundColor: "#f03d32" }}
-            onClick={() => handleDelete(medicine._id)}
-          >
-            Delete
-          </button>
-        </div>
-      </td>
+      {isDataLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <td>{medicine.name}</td>
+          <td>{medicine.quantity} Pieces</td>
+          <td>{medicine.price} EGP</td>
+          <td>{medicine.expirationDate.split("-").reverse().join("-")}</td>
+          <td className="text-end">
+            <div className="dropdown dropdown-action">
+              <a
+                className="action-icon dropdown-toggle"
+                onClick={() => setDropdown((dropdown) => !dropdown)}
+              >
+                <CgMoreVerticalAlt />
+              </a>
+              <div
+                className="dropdown-menu dropdown-menu-end"
+                style={{
+                  display: `${dropdown ? "block" : "none"}`,
+                }}
+              >
+                <Link
+                  to={`/pharmacy/edit-medicine/${medicine._id}`}
+                  className="dropdown-item"
+                >
+                  <FaEdit /> Edit
+                </Link>
+                <button
+                  className="dropdown-item"
+                  onClick={() => {
+                    handleDelete(medicine._id);
+                    setDropdown((dropdown) => !dropdown);
+                  }}
+                >
+                  <RiDeleteBin6Fill /> Delete
+                </button>
+              </div>
+            </div>
+          </td>
+        </>
+      )}
     </tr>
   );
 }
