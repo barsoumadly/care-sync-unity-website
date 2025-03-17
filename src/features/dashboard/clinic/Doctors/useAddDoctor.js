9134a3df052838226b260064cb0512c763b36caf
bishoy labib <bishoylabib78@gmail.com>
2025-03-17 19:46:08 +0200

    useAddDoctor

--- src/features/dashboard/clinic/Doctors/useAddDoctor.js
+++ src/features/dashboard/clinic/Doctors/useAddDoctor.js
@@ -1,11 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addDoctor } from "../../../../services/clinic";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 function useAddDoctor() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const { mutate: addNewDoctor, isDataLoading } = useMutation({
     mutationFn: ({ doctorData }) => addDoctor(doctorData),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["doctorList"] });
+
       toast.success("Doctor successfully added");
+      navigate("/clinic/doctor-list");
     },
     onError: (err) => toast.error(err.message),
   });
