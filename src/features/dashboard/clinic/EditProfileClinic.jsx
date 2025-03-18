a535c27198245c1bb9126b87111cd97e3ebbcbf5
bishoy labib <bishoylabib78@gmail.com>
2025-03-18 11:17:03 +0200

    EditProfileClinic

--- src/features/dashboard/clinic/EditProfileClinic.jsx
+++ src/features/dashboard/clinic/EditProfileClinic.jsx
@@ -1,25 +1,7 @@
-import { useForm } from "react-hook-form";
-import { useState } from "react";
 import CompleteClinicProfile from "./Complete-Profile/CompleteClinicProfile";
 import useClinicProfile from "./useClinicProfile";
-
-// const ClinicData = {
-//   id: 1,
-//   clinicName: "moneb clinic",
-//   mobile: "01201915154",
-//   founded: "2003",
-//   address: "moneb",
-//   biography: "testlkdfg",
-//   images: [{}],
-//   avatar: {},
-//   selectedCity: "RedSea",
-//   selectedArea: "ganub kouser",
-// };
-
 function EditProfileClinic() {
-  const { data } = useClinicProfile();
-  console.log(data);
-
+  const { data, isLoading } = useClinicProfile();
   return <CompleteClinicProfile clinicData={data} />;
 }
 
