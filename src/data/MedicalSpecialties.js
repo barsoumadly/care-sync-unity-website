d108ae12b9faa25facec3ecacdeac24f2c2f94cb
bishoy labib <bishoylabib78@gmail.com>
2025-03-18 13:53:29 +0200

    MedicalSpecialties

---
+++ src/data/MedicalSpecialties.js
@@ -0,0 +1,24 @@
+const MedicalSpecialties = [
+  "Anaesthesiology",
+  "Cardiology",
+  "Dentist",
+  "Dermatology",
+  "Emergency Medicine",
+  "Family Medicine",
+  "Internal Medicine",
+  "Medical Genetics",
+  "Neurology",
+  "Obstetrics and Gynecology",
+  "Ophthalmology",
+  "Orthopedic Surgery",
+  "Otolaryngology",
+  "Pathology",
+  "Pediatrics",
+  "Physical and Rehabilitation (PM&R)",
+  "Psychiatry",
+  "Radiology",
+  "Surgery",
+  "Urology",
+];
+
+export default MedicalSpecialties;
