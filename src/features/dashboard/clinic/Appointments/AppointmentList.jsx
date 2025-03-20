01826ef7d6c74b0afe1c0af2453186be8bdc34e7
bishoy labib <bishoylabib78@gmail.com>
2025-03-20 13:18:25 +0200

    AppointmentList

--- src/features/dashboard/clinic/Appointments/AppointmentList.jsx
+++ src/features/dashboard/clinic/Appointments/AppointmentList.jsx
@@ -97,18 +97,18 @@ function AppointmentList() {
         {/* /Page Header */}
         <div className="row">
           <div className="col-sm-12">
-            <div className="card card-table show-entire">
-              <div className="card-body">
-                {/* Table Header */}
-                <TableHeader />
-                {/* /Table Header */}
-                <Table />
-              </div>
-            </div>
+            {/* <div className="card card-table show-entire">
+              <div className="card-body"> */}
+            {/* Table Header */}
+            <TableHeader />
+            {/* /Table Header */}
+            <Table />
           </div>
         </div>
       </div>
     </div>
+    //   </div>
+    // </div>
   );
 }
 
@@ -186,42 +186,124 @@ function Table() {
     setIsOpen((isOpen) => !isOpen);
   };
   return (
-    <>
-      <div className="table-responsive">
-        <table className="table border-0 custom-table comman-table datatable mb-0">
-          <thead>
-            <tr>
-              <th>
-                <div className="form-check check-tables">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    defaultValue="something"
-                  />
-                </div>
-              </th>
-              <th>Name</th>
-              <th>Specialization</th>
-              <th>Number of Appointments</th>
-              <th>Days</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {doctorInfo?.map((doctor) => (
-              <AppointmentTableTuples
-                doctor={doctor}
-                key={doctor.id}
-                handleOpenModal={handleOpenModal}
+    <div className="row" style={{ marginTop: "30px" }}>
+      {doctorInfo?.map((doctor) => (
+        <DoctorCard doctor={doctor} key={doctor.id} />
+      ))}
+    </div>
+
+    // <>
+    //   <div className="table-responsive">
+    //     <table className="table border-0 custom-table comman-table datatable mb-0">
+    //       <thead>
+    //         <tr>
+    //           <th>
+    //             <div className="form-check check-tables">
+    //               <input
+    //                 className="form-check-input"
+    //                 type="checkbox"
+    //                 defaultValue="something"
+    //               />
+    //             </div>
+    //           </th>
+    //           <th>Name</th>
+    //           <th>Specialization</th>
+    //           <th>Number of Appointments</th>
+    //           <th>Days</th>
+    //           <th />
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         {doctorInfo?.map((doctor) => (
+    //           <AppointmentTableTuples
+    //             doctor={doctor}
+    //             key={doctor.id}
+    //             handleOpenModal={handleOpenModal}
+    //           />
+    //         ))}
+    //       </tbody>
+    //     </table>
+    //   </div>
+    //   {isOpen && (
+    //     <CheckChosicModal handleOpenModal={handleOpenModal} id={doctorId} />
+    //   )}
+    // </>
+  );
+}
+
+function DoctorCard({ doctor }) {
+  return (
+    <div className="col-sm-6 col-md-6 col-xl-3">
+      <Link to={`/clinic/${doctor.name}/${doctor.doctorId}`}>
+        <div className="blog grid-blog">
+          <div className="blog-image">
+            <a href="blog-details.html">
+              <img
+                className="img-fluid"
+                src="/images/clinic/clinic.jpg"
+                alt=""
               />
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {isOpen && (
-        <CheckChosicModal handleOpenModal={handleOpenModal} id={doctorId} />
-      )}
-    </>
+            </a>
+            <div className="blog-views">
+              <h5>{doctor.specialization}</h5>
+            </div>
+            <ul className="nav view-blog-list blog-views">
+              <li>
+                <i className="feather-message-square me-1" />
+                {doctor.appointmentCount}
+              </li>
+            </ul>
+          </div>
+          <div className="blog-content">
+            <div className="blog-grp-blk">
+              <div className="blog-img-blk">
+                <a>
+                  <img
+                    className="img-fluid"
+                    src="https://upload.wikimedia.org/wikipedia/commons/b/bc/Unknown_person.jpg"
+                    alt=""
+                  />
+                </a>
+                <div className="content-blk-blog ms-2">
+                  <h4>
+                    <a href="profile.html">{doctor.name}</a>
+                  </h4>
+                  <h5>{doctor.specialization}</h5>
+                </div>
+              </div>
+              <span>
+                <i className="feather-calendar me-1" />
+                05 Sep 2022
+              </span>
+            </div>
+            <h4 className="blog-title">
+              <a href="blog-details.html">Doctor's appointments:</a>
+            </h4>
+            {doctor.workingDays.map((schedule) => {
+              return (
+                <>
+                  {schedule?.day && (
+                    <div
+                      className={`custom-badge status-pink`}
+                      style={{ margin: "5px", minWidth: "0" }}
+                    >
+                      {schedule?.day}
+                    </div>
+                  )}
+                </>
+              );
+            })}
+            <Link to={`/clinic/${doctor.name}/${doctor.doctorId}`}>
+              <a className="read-more d-flex" style={{ marginTop: "15px" }}>
+                {" "}
+                Read more about {doctor.appointmentCount} appointments
+                <i className="fa fa-long-arrow-right ms-2" />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 }
 
