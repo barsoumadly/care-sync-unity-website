413bdbfe9c9c7b8151a64d97bdf65e92df08112c
bishoy labib <bishoylabib78@gmail.com>
2025-03-21 20:30:08 +0200

    formatTime

---
+++ src/utils/formatTime.js
@@ -0,0 +1,16 @@
+export default function formatTime(date) {
+  let hours = date.split(":")[0];
+  let minutes = date.split(":")[1];
+
+  // Check whether AM or PM
+  let newformat = hours >= 12 ? "PM" : "AM";
+
+  // Find current hour in AM-PM Format
+  hours = hours % 12;
+
+  // To display "0" as "12"
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  return hours + ":" + minutes + " " + newformat;
+}
