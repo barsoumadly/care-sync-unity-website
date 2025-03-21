0c1ec89802e2696499fb33eb1a4eca8b966aa632
bishoy labib <bishoylabib78@gmail.com>
2025-03-21 20:29:13 +0200

    EditProfileDoc

--- src/features/dashboard/clinic/Doctors/EditProfileDoc.jsx
+++ src/features/dashboard/clinic/Doctors/EditProfileDoc.jsx
@@ -1,17 +1,15 @@
-import { useFieldArray, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Button from "../reusable/Button";
 import DynamicDoctorSheduleInput from "./DynamicDoctorSheduleInput";
 import useEditDoctor from "./useEtidDoctor";
 import { useLocation } from "react-router-dom";
-import PasswordEye from "../../../authentication/PasswordEye";
+
 import { useEffect, useState } from "react";
 import useDoctorList from "./useDoctorList";
-import LoadingSpinner from "../../../../ui/LoadingSpinner";
 import MedicalSpecialties from "../../../../data/MedicalSpecialties";
 function EditProfileDoc() {
   const DATA = useLocation().pathname.split("/");
   const DOCTOR_ID = DATA[3];
-  const [isEyeOpen, setIsEyeOpen] = useState(false);
   const [doctorData, setdoctorData] = useState();
   const { updateDoctorData, isDataLoading } = useEditDoctor(DOCTOR_ID);
   const { data, isLoading } = useDoctorList();
@@ -35,12 +33,9 @@ function EditProfileDoc() {
     setValue("specialization", doctorData?.[0]?.doctor?.specialization);
     setValue("email", doctorData?.[0]?.user?.email);
     setValue("gender", doctorData?.[0]?.doctor?.gender);
+    setValue("price", doctorData?.[0]?.price);
     setValue("doctorShedule", doctorData?.[0]?.schedule);
   }, [doctorData]);
-
-  const handlePasswordEye = function (result) {
-    setIsEyeOpen(result);
-  };
 
   function onSubmit(data) {
     updateDoctorData({ data });
@@ -81,123 +76,129 @@ function EditProfileDoc() {
                         <h4>Doctor Details</h4>
                       </div>
                     </div>
-                    {isLoading ? (
-                      <LoadingSpinner />
-                    ) : (
-                      <>
-                        <div className="col-12 col-md-6 col-xl-3">
-                          <div className="input-block local-forms">
-                            <label>
-                              Doctor Name{" "}
-                              <span className="login-danger">*</span>
-                            </label>
+                    <div className="col-12 col-md-6 col-xl-5">
+                      <div className="input-block local-forms">
+                        <label>
+                          Doctor Name <span className="login-danger">*</span>
+                        </label>
+                        <input
+                          className="form-control"
+                          type="text"
+                          placeholder="ex: Daniel Bruk"
+                          {...register("doctorName", {
+                            required: "This field is required",
+                          })}
+                        />
+                        <span className="error-message ">
+                          {errors?.doctorName?.message}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-4">
+                      <div className="input-block local-forms">
+                        <label>
+                          Email <span className="login-danger">*</span>
+                        </label>
+                        <input
+                          className="form-control"
+                          type="email"
+                          placeholder="example@email.com"
+                          {...register("email", {
+                            required: "This field is required",
+                          })}
+                        />
+                        <span className="error-message ">
+                          {errors?.email?.message}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3">
+                      <div className="input-block select-gender">
+                        <label className="gen-label">
+                          Gender<span className="login-danger">*</span>
+                        </label>
+                        <div className="form-check-inline">
+                          <label className="form-check-label">
                             <input
-                              className="form-control"
-                              type="text"
-                              placeholder="ex: Daniel Bruk"
-                              {...register("name", {
+                              type="radio"
+                              name="gender"
+                              className="form-check-input mt-0"
+                              value={"male"}
+                              {...register("gender", {
                                 required: "This field is required",
                               })}
                             />
-                            <span className="error-message ">
-                              {errors?.name?.message}
-                            </span>
-                          </div>
+                            Male
+                          </label>
                         </div>
-                        <div className="col-12 col-md-6 col-xl-3">
-                          <div className="input-block local-forms">
-                            <label>
-                              Specialization{" "}
-                              <span className="login-danger">*</span>
-                            </label>
-                            <select
-                              className="form-control select"
-                              {...register("specialization", {
-                                required: "This field is required",
-                              })}
-                            >
-                              <option disabled>Select Specialization</option>
-                              {MedicalSpecialties.map((specialty) => (
-                                <option key={specialty}>{specialty}</option>
-                              ))}
-                            </select>
-                          </div>
-                          <span className="error-message ">
-                            {errors?.specialization?.message}
-                          </span>
-                        </div>{" "}
-                        <div className="col-12 col-md-6 col-xl-4">
-                          <div className="input-block local-forms">
-                            <label>
-                              Email <span className="login-danger">*</span>
-                            </label>
+                        <div className="form-check-inline">
+                          <label className="form-check-label">
                             <input
-                              className="form-control"
-                              type="email"
-                              placeholder="example@email.com"
-                              {...register("email", {
+                              type="radio"
+                              name="gender"
+                              className="form-check-input mt-0"
+                              value={"female"}
+                              {...register("gender", {
                                 required: "This field is required",
                               })}
                             />
-                            <span className="error-message ">
-                              {errors?.email?.message}
-                            </span>
-                          </div>
+                            Female
+                          </label>
                         </div>
-                        <div className="col-12 col-md-6 col-xl-2">
-                          <div className="input-block select-gender">
-                            <label className="gen-label">
-                              Gender<span className="login-danger">*</span>
-                            </label>
-                            <div className="form-check-inline">
-                              <label className="form-check-label">
-                                <input
-                                  type="radio"
-                                  name="gender"
-                                  className="form-check-input mt-0"
-                                  defaultChecked={true}
-                                  value={"male"}
-                                  {...register("gender", {
-                                    required: "This field is required",
-                                  })}
-                                />
-                                Male
-                              </label>
-                            </div>
-                            <div className="form-check-inline">
-                              <label className="form-check-label">
-                                <input
-                                  type="radio"
-                                  name="gender"
-                                  className="form-check-input mt-0"
-                                  value={"female"}
-                                  {...register("gender", {
-                                    required: "This field is required",
-                                  })}
-                                />
-                                Female
-                              </label>
-                            </div>
-                            <span className="error-message ">
-                              {errors?.gender?.message}
-                            </span>
-                          </div>
-                        </div>
-                      </>
-                    )}
+                        <span className="error-message ">
+                          {errors?.gender?.message}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-5">
+                      <div className="input-block local-forms">
+                        <label>
+                          Specialization <span className="login-danger">*</span>
+                        </label>
+                        <select
+                          className="form-control select"
+                          {...register("specialization", {
+                            required: "This field is required",
+                          })}
+                        >
+                          <option disabled>Select Specialization</option>
+                          {MedicalSpecialties.map((specialty) => (
+                            <option key={specialty}>{specialty}</option>
+                          ))}
+                        </select>
+                      </div>
+                      <span className="error-message ">
+                        {errors?.specialization?.message}
+                      </span>
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-4">
+                      <div className="input-block local-forms">
+                        <label>
+                          Examination fees{" "}
+                          <span className="login-danger">*</span>
+                        </label>
+                        <input
+                          className="form-control"
+                          type="number"
+                          placeholder="200 EGP"
+                          {...register("price", {
+                            required: "This field is required",
+                          })}
+                        />
+                        <span className="error-message ">
+                          {errors?.price?.message}
+                        </span>
+                      </div>
+                    </div>
                     {/* Doctor Shedule  */}
                     <div className="form-heading">
                       <h4>Doctor Shedule </h4>
                     </div>
-                    {isLoading ? (
-                      <LoadingSpinner />
-                    ) : (
-                      <DynamicDoctorSheduleInput
-                        control={control}
-                        register={register}
-                        errors={errors}
-                      />
-                    )}
+                    <DynamicDoctorSheduleInput
+                      control={control}
+                      register={register}
+                      errors={errors}
+                    />
                     {/* Button */}
                     <div className="col-12">
                       <div className="doctor-submit text-end">
