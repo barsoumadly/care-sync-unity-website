7bf634dd5d74d4a672fc9ba9b83d91c5e4e5a1c8
bishoy labib <bishoylabib78@gmail.com>
2025-03-21 20:27:36 +0200

    useBookAppointment

--- src/features/dashboard/clinic/Appointments/useBookAppointment.js
+++ src/features/dashboard/clinic/Appointments/useBookAppointment.js
@@ -1,23 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 
 function useBookAppointment() {
-  const { mutate: updateClinicProfile, isDataLoading } = useMutation({
-    // mutationFn: ({ appointmentData }) => updateClinicProfile(appointmentData),
+  const { mutate: BookNewAppointment, isLoading } = useMutation({
+    // mutationFn: bookAppointment,
     onSuccess: () => {
       toast.success("Appointment successfully deleted");
     },
     onError: (err) => toast.error(err.message),
   });
 
-  const { mutate: UpdateProfilePhoto, isPhotoLoading } = useMutation({
-    // mutationFn: ({ profilePhoto }) => updateProfilePhoto(profilePhoto),
-  });
-
   return {
-    updateClinicProfile,
-    UpdateProfilePhoto,
-    isDataLoading,
-    isPhotoLoading,
+    BookNewAppointment,
+    isLoading,
   };
 }
 
