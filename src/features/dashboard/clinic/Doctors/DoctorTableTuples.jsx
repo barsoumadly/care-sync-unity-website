b2ba35fae1a5d6c162e8f6e0429a928b1a1cdceb
bishoy labib <bishoylabib78@gmail.com>
2025-03-23 14:32:50 +0200

    doctorTuple

--- src/features/dashboard/clinic/Doctors/DoctorTableTuples.jsx
+++ src/features/dashboard/clinic/Doctors/DoctorTableTuples.jsx
@@ -2,22 +2,35 @@ import { useState } from "react";
 import { CgMoreVerticalAlt } from "react-icons/cg";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin6Fill } from "react-icons/ri";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { createChat } from "../../../../services/chat-app";
 function DoctorTableTuples({ doctor, handleOpenModal, dropdown, setDropdown }) {
+  const handleCreateChat = async function (id) {
+    const response = await createChat(
+      id,
+      JSON.parse(localStorage.getItem("key"))
+    );
+    localStorage.setItem("chat", JSON.stringify(response.data.data.chat));
+  };
+
   return (
     <>
       <tr>
         <td></td>
         <td className="profile-image">
-          <img
-            width={28}
-            height={28}
-            src={doctor.user.profilePhoto.url}
-            className="rounded-circle m-r-5"
-            alt=""
-          />{" "}
-          {doctor.user.name}
+          <Link
+            to="/clinic/chat"
+            onClick={() => handleCreateChat(doctor.user._id)}
+          >
+            <img
+              width={28}
+              height={28}
+              src={doctor.user.profilePhoto.url}
+              className="rounded-circle m-r-5"
+              alt=""
+            />{" "}
+            {doctor.user.name}
+          </Link>
         </td>
 
         <td>{doctor.doctor.specialization}</td>
