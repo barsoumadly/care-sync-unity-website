85657ff4d48d5a912d4f1960ff5a7bcfea1f47fd
bishoy labib <bishoylabib78@gmail.com>
2025-03-23 20:14:02 +0200

    editClinic

--- src/features/dashboard/clinic/EditProfileClinic.jsx
+++ src/features/dashboard/clinic/EditProfileClinic.jsx
@@ -1,8 +1,17 @@
+import LoadingSpinner from "../../../ui/LoadingSpinner";
 import CompleteClinicProfile from "./Complete-Profile/CompleteClinicProfile";
 import useClinicProfile from "./useClinicProfile";
 function EditProfileClinic() {
   const { data, isLoading } = useClinicProfile();
-  return <CompleteClinicProfile clinicData={data} />;
+  console.log(data);
+  if (isLoading)
+    return (
+      <div style={{ marginTop: "25%" }}>
+        <LoadingSpinner />;
+      </div>
+    );
+
+  if (data) return <CompleteClinicProfile clinicData={data} />;
 }
 
 export default EditProfileClinic;
