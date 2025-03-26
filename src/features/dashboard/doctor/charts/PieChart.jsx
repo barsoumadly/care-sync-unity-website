f75b10440d96646a15ba97c8cb384b049e97e19a
bishoy labib <bishoylabib78@gmail.com>
2025-03-26 11:35:45 +0200

    PieCharts

--- src/features/dashboard/doctor/charts/PieChart.jsx
+++ src/features/dashboard/doctor/charts/PieChart.jsx
@@ -14,58 +14,7 @@ const data = [
   { name: "Female", duration: "Female", value: 26, color: "#14b8a6" },
 ];
 
-const startDataLight = [
-  {
-    duration: "1 night",
-    value: 0,
-    color: "#ef4444",
-  },
-  {
-    duration: "2 nights",
-    value: 0,
-    color: "#f97316",
-  },
-  {
-    duration: "3 nights",
-    value: 0,
-    color: "#eab308",
-  },
-  {
-    duration: "4-5 nights",
-    value: 0,
-    color: "#84cc16",
-  },
-  {
-    duration: "6-7 nights",
-    value: 0,
-    color: "#22c55e",
-  },
-  {
-    duration: "8-14 nights",
-    value: 0,
-    color: "#14b8a6",
-  },
-  {
-    duration: "15-21 nights",
-    value: 0,
-    color: "#3b82f6",
-  },
-  {
-    duration: "21+ nights",
-    value: 0,
-    color: "#a855f7",
-  },
-];
-
 export default function PieCharts() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const onPieEnter = useCallback(
-    (_, index) => {
-      setActiveIndex(index);
-    },
-    [setActiveIndex]
-  );
-
   return (
     <di>
       <ResponsiveContainer width="100%" height={300}>
@@ -92,7 +41,7 @@ export default function PieCharts() {
           <Legend
             verticalAlign="bottom"
             align="center"
-            width="50%"
+            width="100%"
             layout="horizontal"
             iconSize={15}
             iconType="circle"
