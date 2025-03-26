521b0c6b4cc4231dd50b551a9a0c759ba7b832f6
bishoy labib <bishoylabib78@gmail.com>
2025-03-26 14:02:12 +0200

    patient List

--- src/features/dashboard/clinic/Appointments/PatientsOfDoc.jsx
+++ src/features/dashboard/clinic/Appointments/PatientsOfDoc.jsx
@@ -2,76 +2,80 @@ import { useState } from "react";
 import PatientTableTuples from "./PatientTableTuples";
 import CheckChosicModal from "../reusable/CheckChosicModal";
 import { useLocation } from "react-router-dom";
-const patients = [
-  {
-    id: 1,
-    name: "Andrea Lalema",
-    image: "../images/dashborad/profiles/avatar-01.jpg",
-    paymentType: "Credit Card",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0004",
-  },
-  {
-    id: 2,
-    name: "Smith Bruklin",
-    image: "../images/dashborad/profiles/avatar-02.jpg",
-    paymentType: "Debit Card",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0001",
-  },
-  {
-    id: 3,
-    name: "Bernardo James",
-    image: "../images/dashborad/profiles/avatar-03.jpg",
-    paymentType: "Pay Pal",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0002",
-  },
-  {
-    id: 4,
-    name: "Mark Hay Smith",
-    image: "../images/dashborad/profiles/avatar-04.jpg",
-    paymentType: "Credit Card",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0003",
-  },
-  {
-    id: 5,
-    name: "Galaviz Lalema",
-    image: "../images/dashborad/profiles/avatar-05.jpg",
-    paymentType: "Debit Card",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0004",
-  },
-  {
-    id: 6,
-    name: "Andrea Lalema",
-    image: "../images/dashborad/profiles/avatar-06.jpg",
-    paymentType: "Pay Pal",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0003",
-  },
-  {
-    id: 7,
-    name: "William Stephin",
-    image: "../images/dashborad/profiles/avatar-03.jpg",
-    paymentType: "Pay Pal",
-    trueStatus: "Waiting",
-    falseStatus: "Done",
-    turn: "#INV-0005",
-  },
-];
+import usePatientAppointmentList from "./usePatientAppointmentList";
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
+// const patients = [
+//   {
+//     id: 1,
+//     name: "Andrea Lalema",
+//     image: "../images/dashborad/profiles/avatar-01.jpg",
+//     paymentType: "Credit Card",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0004",
+//   },
+//   {
+//     id: 2,
+//     name: "Smith Bruklin",
+//     image: "../images/dashborad/profiles/avatar-02.jpg",
+//     paymentType: "Debit Card",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0001",
+//   },
+//   {
+//     id: 3,
+//     name: "Bernardo James",
+//     image: "../images/dashborad/profiles/avatar-03.jpg",
+//     paymentType: "Pay Pal",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0002",
+//   },
+//   {
+//     id: 4,
+//     name: "Mark Hay Smith",
+//     image: "../images/dashborad/profiles/avatar-04.jpg",
+//     paymentType: "Credit Card",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0003",
+//   },
+//   {
+//     id: 5,
+//     name: "Galaviz Lalema",
+//     image: "../images/dashborad/profiles/avatar-05.jpg",
+//     paymentType: "Debit Card",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0004",
+//   },
+//   {
+//     id: 6,
+//     name: "Andrea Lalema",
+//     image: "../images/dashborad/profiles/avatar-06.jpg",
+//     paymentType: "Pay Pal",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0003",
+//   },
+//   {
+//     id: 7,
+//     name: "William Stephin",
+//     image: "../images/dashborad/profiles/avatar-03.jpg",
+//     paymentType: "Pay Pal",
+//     trueStatus: "Waiting",
+//     falseStatus: "Done",
+//     turn: "#INV-0005",
+//   },
+// ];
 function PatientsOfDoc() {
   const [isOpen, setIsOpen] = useState(false);
   const [patientId, setPatientId] = useState(false);
   const path = useLocation();
   const doctorId = path.pathname.split("/")[3];
+
+  const { data: patients, isLoading } = usePatientAppointmentList(doctorId);
 
   const handleOpenModal = function (id) {
     setPatientId(id);
@@ -86,12 +90,12 @@ function PatientsOfDoc() {
             <div className="col-sm-12">
               <ul className="breadcrumb">
                 <li className="breadcrumb-item">
-                  <a href="payments.html">Accounts </a>
+                  <a href="payments.html">Appointment List </a>
                 </li>
                 <li className="breadcrumb-item">
                   <i className="feather-chevron-right" />
                 </li>
-                <li className="breadcrumb-item active">Payments </li>
+                <li className="breadcrumb-item active">Patient List </li>
               </ul>
             </div>
           </div>
@@ -189,37 +193,35 @@ function PatientsOfDoc() {
                   </form>
                 </div>
                 <div className="table-responsive">
-                  <table className="table border-0 custom-table comman-table datatable mb-0">
-                    <thead>
-                      <tr>
-                        <th>
-                          <div className="form-check check-tables ">
-                            <input
-                              className="form-check-input"
-                              type="checkbox"
-                              defaultValue="something"
-                            />
-                          </div>
-                        </th>
-                        <th>Turn</th>
-                        <th>Patient</th>
-                        <th>Payment Type</th>
-                        {/*   <th>Paid Date</th> */}
-                        {/*   <th>Paid Amount</th> */}
-                        <th>Status</th>
-                        <th />
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {patients.map((patient) => (
-                        <PatientTableTuples
-                          key={patient.id}
-                          patient={patient}
-                          handleOpenModal={handleOpenModal}
-                        />
-                      ))}
-                    </tbody>
-                  </table>
+                  {isLoading ? (
+                    <LoadingSpinner />
+                  ) : (
+                    <table className="table border-0 custom-table comman-table datatable mb-0">
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>Turn</th>
+                          <th>Patient</th>
+                          <th>Phone</th>
+                          <th>Payment Type</th>
+                          <th>Paid Amount</th>
+                          <th>Specialization</th>
+                          <th>Paid Date</th>
+                          <th>Status</th>
+                          <th />
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {patients?.map((patient) => (
+                          <PatientTableTuples
+                            key={patient.patient.id}
+                            patient={patient}
+                            handleOpenModal={handleOpenModal}
+                          />
+                        ))}
+                      </tbody>
+                    </table>
+                  )}
                 </div>
               </div>
             </div>
