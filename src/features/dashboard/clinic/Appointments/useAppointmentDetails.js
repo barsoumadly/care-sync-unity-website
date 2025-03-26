7e4838410626893ec75d4cd64291551e3704405e
bishoy labib <bishoylabib78@gmail.com>
2025-03-26 19:00:22 +0200

    useAppointmentDetails

--- src/features/dashboard/clinic/Appointments/useAppointmentDetails.js
+++ src/features/dashboard/clinic/Appointments/useAppointmentDetails.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+import { getAppointmentDetails } from "../../../../services/clinic";
 
 function useAppointmentDetails(appointmentId) {
   const { data, error, isLoading } = useQuery({
     queryKey: ["appointmentDetails", appointmentId],
-    // queryFn:
+    queryFn: () => getAppointmentDetails(appointmentId),
   });
   return { data, error, isLoading };
 }
