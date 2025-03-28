480c8d21bffb1eecdfe219d047970bbcf1f4f185
bishoy labib <bishoylabib78@gmail.com>
2025-03-28 21:20:49 +0200

    useWritingPrescription

--- src/features/dashboard/doctor/Patients/Examination/useWritingPrescription.js
+++ src/features/dashboard/doctor/Patients/Examination/useWritingPrescription.js
@@ -1,13 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createPrescription } from "../../../../../services/prescription";
+import toast from "react-hot-toast";
+import { use, useState } from "react";
 
 function useWritingPrescription() {
+  const [prescriptionData, setPrescriptionData] = useState(" ");
+
   const { mutate, isLoading, error } = useMutation({
-    // mutationFn: ({ prescriptionData }) => writingPrescription(prescriptionData),
-    onSuccess: () => {
+    mutationFn: createPrescription,
+    onSuccess: (data) => {
+      console.log(data);
+      setPrescriptionData(data);
       toast.success("Prescription successfully saved");
     },
     onError: (err) => toast.error(err.message),
   });
-  return { mutate, isLoading, error };
+
+  return { prescriptionData, mutate, isLoading, error };
 }
+
 export default useWritingPrescription;
