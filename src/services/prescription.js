b5b03a39359b7ad7605a7bf8c542411a1636ce48
Barsoum Adly <barsoumadly@gmail.com>
2025-03-28 14:29:39 +0200

    prescription service

---
+++ src/services/prescription.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/v1/prescriptions";
+
+const getPrescriptionsByPatientId = async function () {
+  const token = JSON.parse(localStorage.getItem("key"));
+
+  try {
+    const response = await axios.get(`${API_URL}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return await response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getPrescriptionsByPatientId };
