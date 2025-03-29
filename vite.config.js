55944be9415be4c529e37eddb11c92b6b6c735be
bishoy labib <bishoylabib78@gmail.com>
2025-03-29 13:35:59 +0200

    eslint removed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 import fs from "fs";
 
 // https://vite.dev/config/
@@ -44,7 +43,7 @@ export default defineConfig(({ mode }) => ({
       },
     ],
   },
-  plugins: [react(), eslint()],
+  plugins: [react()],
   define: {
     "import.meta.env.VITE_API_URL": JSON.stringify(
       mode === "production"
