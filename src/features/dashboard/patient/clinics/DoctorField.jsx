b5e808b4ba346c318d2c6f4919c3cbb8cda7322c
Barsoum Adly <barsoumadly@gmail.com>
2025-03-30 13:44:55 +0200

    doctor field

--- src/features/dashboard/patient/clinics/DoctorField.jsx
+++ src/features/dashboard/patient/clinics/DoctorField.jsx
@@ -1,35 +1,89 @@
+import LoadingSpinner from "../../../../ui/LoadingSpinner";
+import useDoctor from "./useDoctor";
+
 function DoctorField({ doctor, onSelectDoctor, onOpenModal }) {
+  const { data, isLoading } = useDoctor(doctor.id);
+  const days = doctor.schedule.map((schedule) => schedule.day);
+  const startTimeHour = doctor.schedule.map(
+    (schedule) => schedule.startTime.split(":")[0]
+  );
+  const startTimeMinutes = doctor.schedule.map(
+    (schedule) => schedule.startTime.split(":")[1]
+  );
+  const startTime = startTimeHour.map((time, i) =>
+    Number(time) > 12
+      ? `${time - 12 < 10 ? `0${time - 12}` : time - 12}: ${
+          startTimeMinutes[i]
+        } PM`
+      : Number(time) === 12
+      ? `${time}:00 PM`
+      : `${time < 10 ? `0${time}` : time}: ${startTimeMinutes[i]} AM`
+  );
+
+  const endTimeHour = doctor.schedule.map(
+    (schedule) => schedule.endTime.split(":")[0]
+  );
+  const endTimeMinutes = doctor.schedule.map(
+    (schedule) => schedule.endTime.split(":")[1]
+  );
+  const endTime = endTimeHour.map((time, i) =>
+    Number(time) > 12
+      ? `${time - 12 < 10 ? `0${time - 12}` : time - 12}: ${
+          endTimeMinutes[i]
+        } PM`
+      : `${time < 10 ? `0${time}` : time}: ${endTimeMinutes[i]} AM`
+  );
+
   return (
     <tr>
-      <td className="profile-image">
-        <a href="profile.html">
-          <img
-            width={28}
-            height={28}
-            src={doctor.profilePhoto}
-            className="rounded-circle m-r-5"
-            alt=""
-          />
-          {doctor.name}
-        </a>
-      </td>
-      <td> {doctor.specialization}</td>
-      <td> {doctor.days}</td>
-      <td> {doctor.time}</td>
-      <td> {doctor.cost} EGP</td>
-      <td>
-        <div className="dropdown action-label">
-          <button
-            className="custom-badge book-btn"
-            onClick={() => {
-              onOpenModal();
-              onSelectDoctor(doctor);
-            }}
-          >
-            Book appointment
-          </button>
-        </div>
-      </td>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <td className="profile-image">
+            <a href="profile.html">
+              <img
+                width={28}
+                height={28}
+                src={data?.userId?.profilePhoto?.url}
+                className="rounded-circle m-r-5"
+                alt=""
+              />
+              {data?.userId?.name}
+            </a>
+          </td>
+          <td> {data?.specialization}</td>
+          <td>
+            {days.map((day) => (
+              <span>
+                {day}
+                <br />
+              </span>
+            ))}
+          </td>
+          <td>
+            {startTime.map((time, i) => (
+              <span>
+                {time} - {endTime[i]} <br />
+              </span>
+            ))}
+          </td>
+          <td> {doctor.price} EGP</td>
+          <td>
+            <div className="dropdown action-label">
+              <button
+                className="custom-badge book-btn"
+                onClick={() => {
+                  onOpenModal();
+                  onSelectDoctor({ ...doctor, ...data });
+                }}
+              >
+                Book appointment
+              </button>
+            </div>
+          </td>
+        </>
+      )}
     </tr>
   );
 }
