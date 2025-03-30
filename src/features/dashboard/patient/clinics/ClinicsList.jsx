7638b8e3beb4a04912af5a8bbb2b1aa32dbf5f71
Barsoum Adly <barsoumadly@gmail.com>
2025-03-30 20:47:36 +0200

    clinic list

--- src/features/dashboard/patient/clinics/ClinicsList.jsx
+++ src/features/dashboard/patient/clinics/ClinicsList.jsx
@@ -5,60 +5,16 @@ import useClinics from "./useClinics";
 import LoadingSpinner from "../../../../ui/LoadingSpinner";
 import useProfile from "../useProfile";
 
-let clinicsList = [
-  {
-    id: 1,
-    name: "Heliopolis Hospital",
-    slug: "heliopolis-hospital",
-    email: "heliopolis-hospital@gmail.com",
-    city: "Cairo",
-    address: "El Hegaz St, Al Matar, El Nozha, Cairo Governorate",
-    phone: "02 26339870",
-    location:
-      "Heliopolis Hospital El Hegaz St, Al Matar, El Nozha, Cairo Governorate",
-    specialization: ["Cardiology", "Neurology", "Oncology", "Orthopedics"],
-    foundedDate: 1937,
-    profilePhoto:
-      "https://lh5.googleusercontent.com/p/AF1QipPopa6nUNMevGnUYIrhcu2weu-SP-C661XfNIFT=w243-h174-n-k-no-nu",
-    images: [
-      "https://lh3.googleusercontent.com/p/AF1QipNa7dcPiWAkZCVHhwyQUvBrH0WLV8zIXMX9rMle=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipNhcWsTLwQ87CYlzwZsobYz8MTy9Z3ngcdKc_eW=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipN6wIiLf0SGaXNK4flDJ-NJ4RVlThLGj316lc47=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipO9v7ZgcJ_GIwW0eOgzCEqXOJNjHar1v7V_40dS=s1360-w1360-h1020",
-    ],
-    rating: 4,
-  },
-  {
-    id: 2,
-    name: "Cleopatra Hospital",
-    slug: "cleopatra-hospital",
-    email: "cleopatra-hospital@gmail.com",
-    city: "Cairo",
-    address: "39 Cleopatra، Maidan Salahuddin Square, Heliopolis",
-    phone: "02 24143931",
-    location: "39 Cleopatra، Maidan Salahuddin Square, Almazah, Heliopolis",
-    specialization: ["Gynocolgy", "Neurology", "Dentist", "Otolaryngology"],
-    foundedDate: 1979,
-    profilePhoto:
-      "https://lh3.googleusercontent.com/p/AF1QipO_3Ws6r-6SA5RMKVYq0SVKhjQxKVC5wH1nG7HW=s1360-w1360-h1020",
-    images: [
-      "https://lh3.googleusercontent.com/p/AF1QipOU43wsL8ZDWCnC4UUpoFDhDW6K5CN7_xW4pMkS=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipOtKOli7fk3qE4WNj7XV7MLtBYcIMRZqgPVi2k=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipOZU5YcObC2X2JXmzRbmmZ7CaaRWKFstojAb9bc=s1360-w1360-h1020",
-      "https://lh3.googleusercontent.com/p/AF1QipN58PI00Yg4ncIy9Rju8zZtT-1MFTZmQPWoHzWe=s1360-w1360-h1020",
-    ],
-    rating: 4.5,
-  },
-];
-
 function ClinicsList() {
   const { data: clinicsList, isLoading } = useClinics();
 
   const { data: user, isLoading: isLoad } = useProfile();
 
-  const nearestClinics = clinicsList?.filter(
-    (clinic) => clinic.address.area === user?.address?.area
-  );
+  const nearestClinics = clinicsList
+    ?.filter((clinic) => clinic.address.area === user?.address?.area)
+    .filter((clinic) => clinic.name.includes("Clinic"));
+
+  console.log(clinicsList);
 
   return (
     <div className="page-wrapper" style={{ minHeight: 270 }}>
@@ -103,7 +59,7 @@ function ClinicsList() {
                           />
                         </div>
                       </div>
-                      <div className="col-12 col-md-6 col-xl-3">
+                      <div className="col-12 col-md-6 col-xl-2">
                         <div className="input-block local-forms">
                           <label>Specialization </label>
                           <select className="form-control pass-input">
@@ -117,7 +73,7 @@ function ClinicsList() {
                           </select>
                         </div>
                       </div>
-                      <div className="col-12 col-md-6 col-xl-3">
+                      <div className="col-12 col-md-6 col-xl-2">
                         <div className="input-block local-forms">
                           <label>City </label>
                           <select className="form-control pass-input">
@@ -127,6 +83,20 @@ function ClinicsList() {
                             <option value="cairo">Cairo</option>
                             <option value="giza">Giza</option>
                             <option value="alex">Alexandria</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-12 col-md-6 col-xl-2">
+                        <div className="input-block local-forms">
+                          <label>Area </label>
+                          <select className="form-control pass-input">
+                            <option value="" disabled selected>
+                              Select your area
+                            </option>
+                            <option value="cairo">Maadi</option>
+                            <option value="giza">Heliopolis</option>
+                            <option value="alex">Ain Shams</option>
+                            <option value="alex">Bab al-Louq</option>
                           </select>
                         </div>
                       </div>
