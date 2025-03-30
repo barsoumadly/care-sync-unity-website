994afa990682df7e2a7e9152ced25407a07737cd
bishoy labib <bishoylabib78@gmail.com>
2025-03-30 12:48:39 +0200

    Fix some problem in Dr.dashboard

--- src/features/dashboard/doctor/DynamicProfileInput.jsx
+++ src/features/dashboard/doctor/DynamicProfileInput.jsx
@@ -14,69 +14,67 @@ function DynamicProfileInput({
   });
   return (
     <>
-      {fields.map((field, index) => {
-        return (
-          <div className="row" key={index}>
-            {fieldNamesText.map((fieldName) => (
-              <div className="col-12 col-md-6 col-xl-5">
-                <div className="input-block local-forms">
-                  <label>
-                    {fieldName} <span className="login-danger">*</span>
-                  </label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder={`${fieldName}:`}
-                    {...register(`${arrayName}.${index}.${fieldName}`, {
-                      required: "This field is required",
-                    })}
-                  />
-                  <span className="error-message ">
-                    {errors?.[arrayName]?.[index][fieldName]?.message}
-                  </span>
-                </div>
+      {fields.map((field, index) => (
+        <div className="row" key={index}>
+          {fieldNamesText.map((fieldName, index) => (
+            <div className="col-12 col-md-6 col-xl-5" key={index}>
+              <div className="input-block local-forms">
+                <label>
+                  {fieldName} <span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder={`${fieldName}:`}
+                  {...register(`${arrayName}.${index}.${fieldName}`, {
+                    required: "This field is required",
+                  })}
+                />
+                <span className="error-message ">
+                  {errors?.[arrayName]?.[index][fieldName]?.message}
+                </span>
               </div>
-            ))}
-            {fieldNamesDate?.map((fieldName) => (
-              <div className="col-12 col-md-6 col-xl-5">
-                <div className="input-block local-forms">
-                  <label>
-                    {fieldName} <span className="login-danger">*</span>
-                  </label>
-                  <input
-                    className="form-control"
-                    type="date"
-                    {...register(
-                      `${arrayName}.${index}.${fieldName
-                        .split(" ")[0]
-                        .toLowerCase()}Date`,
-                      {
-                        required: "This field is required",
-                      }
-                    )}
-                  />
-                  <span className="error-message ">
-                    {errors?.[arrayName]?.[index][fieldName]?.message}
-                  </span>
-                </div>
-              </div>
-            ))}
-
-            <div className="col-12 col-md-6 col-xl-2 mb-4">
-              {" "}
-              <div className="doctor-submit">
-                <button
-                  className="btn btn-primary cancel-form"
-                  type="button"
-                  onClick={() => remove(index)}
-                >
-                  Remove{" "}
-                </button>
-              </div>{" "}
             </div>
+          ))}
+          {fieldNamesDate?.map((fieldName, index) => (
+            <div className="col-12 col-md-6 col-xl-5" key={index}>
+              <div className="input-block local-forms">
+                <label>
+                  {fieldName} <span className="login-danger">*</span>
+                </label>
+                <input
+                  className="form-control"
+                  type="date"
+                  {...register(
+                    `${arrayName}.${index}.${fieldName
+                      .split(" ")[0]
+                      .toLowerCase()}Date`,
+                    {
+                      required: "This field is required",
+                    }
+                  )}
+                />
+                <span className="error-message ">
+                  {errors?.[arrayName]?.[index][fieldName]?.message}
+                </span>
+              </div>
+            </div>
+          ))}
+
+          <div className="col-12 col-md-6 col-xl-2 mb-4">
+            {" "}
+            <div className="doctor-submit">
+              <button
+                className="btn btn-primary cancel-form"
+                type="button"
+                onClick={() => remove(index)}
+              >
+                Remove{" "}
+              </button>
+            </div>{" "}
           </div>
-        );
-      })}
+        </div>
+      ))}
       <div className="col-12 col-md-6 col-xl-2">
         <div className="doctor-submit">
           <button
