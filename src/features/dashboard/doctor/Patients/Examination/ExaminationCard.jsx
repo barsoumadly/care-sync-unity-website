14162b8ef73fbe9b3e5a3a10f0f4ad11c3c759bf
bishoy labib <bishoylabib78@gmail.com>
2025-03-31 20:43:47 +0200

    ExaminationCard

--- src/features/dashboard/doctor/Patients/Examination/ExaminationCard.jsx
+++ src/features/dashboard/doctor/Patients/Examination/ExaminationCard.jsx
@@ -1,31 +1,67 @@
-function ExaminationCard({ type, setOpenCard, isAdding }) {
+function ExaminationCard({
+  setOpenCard,
+  isAddingPrescription,
+  isAddingAnalysis,
+}) {
   return (
-    <div
-      className="text-center mt-5 bg-light py-5"
-      style={{ padding: "0 10%" }}
-    >
-      <div className="reminder-icon">
-        <img
-          src={`${
-            !isAdding
-              ? `/images/dashborad/${type}.webp`
-              : "/images/dashborad/check-mark.png"
-          }`}
-          alt={type}
-          style={{ width: "100%" }}
-        />
-      </div>
-      <h4>{`${
-        !isAdding
-          ? `You haven't added any ${type} prescription`
-          : `You have added a ${type} prescription.`
-      }`}</h4>
-      <button
-        className="btn btn-primary mt-4 btn-block"
-        onClick={() => setOpenCard(type)}
+    <div className="row">
+      {/* medicine */}
+      <div
+        className="col-xl-6 col-md-6 text-center mt-5 bg-light py-5"
+        style={{ padding: "0 10%" }}
       >
-        {!isAdding ? "Add" : "Edit"} {type}
-      </button>
+        <div className="reminder-icon">
+          <img
+            src={`${
+              isAddingPrescription !== "medicine"
+                ? `/images/dashborad/medicine.webp`
+                : "/images/dashborad/check-mark.png"
+            }`}
+            alt="medicine"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <h4>{`${
+          isAddingPrescription !== "medicine"
+            ? `You haven't added any medicine prescription`
+            : `You have added a medicine prescription.`
+        }`}</h4>
+        <button
+          className="btn btn-secondary mt-4 btn-block"
+          onClick={() => setOpenCard("medicine")}
+        >
+          {isAddingPrescription !== "medicine" ? "Add" : "Edit"} {"medicine"}
+        </button>
+      </div>
+
+      {/* analysis */}
+      <div
+        className="col-xl-6 col-md-6 text-center mt-5 bg-light py-5"
+        style={{ padding: "0 10%" }}
+      >
+        <div className="reminder-icon">
+          <img
+            src={`${
+              isAddingAnalysis !== "analysis"
+                ? `/images/dashborad/analysis.webp`
+                : "/images/dashborad/check-mark.png"
+            }`}
+            alt="analysis"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <h4>{`${
+          isAddingAnalysis !== "analysis"
+            ? `You haven't added any analysis prescription`
+            : `You have added a analysis prescription.`
+        }`}</h4>
+        <button
+          className="btn btn-secondary mt-4 btn-block"
+          onClick={() => setOpenCard("analysis")}
+        >
+          {isAddingAnalysis !== "analysis" ? "Add" : "Edit"} {"analysis"}
+        </button>
+      </div>
     </div>
   );
 }
