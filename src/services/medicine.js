f038ddcb560aded4f0389b9ea78894f2e0b2314a
Barsoum Adly <barsoumadly@gmail.com>
2025-03-31 22:36:46 +0200

    medicine service

--- src/services/medicine.js
+++ src/services/medicine.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/v1/medicines";
+// const API_URL = "http://localhost:8000/api/v1/medicines";
+
+const API_URL = `${import.meta.env.VITE_API_URL}/medicines`;
 
 const addMedicineList = async function (medicineList) {
   const token = JSON.parse(localStorage.getItem("key"));
