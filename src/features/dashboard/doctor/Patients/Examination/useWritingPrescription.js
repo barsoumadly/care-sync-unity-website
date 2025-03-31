0f9d4e1090c03f55f03c1ad1c486548258cf4dfe
bishoy labib <bishoylabib78@gmail.com>
2025-03-31 20:43:58 +0200

    useWritingPrescription

--- src/features/dashboard/doctor/Patients/Examination/useWritingPrescription.js
+++ src/features/dashboard/doctor/Patients/Examination/useWritingPrescription.js
@@ -4,19 +4,16 @@ import toast from "react-hot-toast";
 import { use, useState } from "react";
 
 function useWritingPrescription() {
-  const [prescriptionData, setPrescriptionData] = useState(" ");
-
   const { mutate, isLoading, error } = useMutation({
     mutationFn: createPrescription,
     onSuccess: (data) => {
       console.log(data);
-      setPrescriptionData(data);
       toast.success("Prescription successfully saved");
     },
     onError: (err) => toast.error(err.message),
   });
 
-  return { prescriptionData, mutate, isLoading, error };
+  return { mutate, isLoading, error };
 }
 
 export default useWritingPrescription;
