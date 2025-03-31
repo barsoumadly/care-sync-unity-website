88a087d3cd081cbf0189454654f3dc863a4bf23e
Barsoum Adly <barsoumadly@gmail.com>
2025-03-31 21:49:09 +0200

    medicine tuple

--- src/features/dashboard/patient/medicines/MedicineTuple.jsx
+++ src/features/dashboard/patient/medicines/MedicineTuple.jsx
@@ -2,8 +2,10 @@ function MedicineTuple({ medicine }) {
   return (
     <tr>
       <td>{medicine.name}</td>
-      <td>{medicine.expirationDate}</td>
-      <td>{medicine.quantity} Pieces</td>
+      <td>{medicine.expirationDate.split("-").reverse().join("-")}</td>
+      <td>
+        {medicine.quantity} {medicine.quantity > 1 ? "Pieces" : "Piece"}
+      </td>
       <td>{medicine.price} EGP</td>
     </tr>
   );
