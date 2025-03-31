2b8e2659dd29a3f1da70e48997975de1e62aebd0
bishoy labib <bishoylabib78@gmail.com>
2025-04-01 00:33:53 +0200

    useScheduleList

--- src/features/dashboard/doctor/doctor-schedule/useScheduleList.js
+++ src/features/dashboard/doctor/doctor-schedule/useScheduleList.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+import { getDoctorSchedule } from "../../../../services/doctor";
 
 function useScheduleList() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["ScheduleList"],
-    // queryFn: getAppointmentList,
+    queryFn: getDoctorSchedule,
   });
 
   return { data, error, isLoading };
