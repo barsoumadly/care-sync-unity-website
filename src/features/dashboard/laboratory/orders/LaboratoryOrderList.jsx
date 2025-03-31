70ef657edb51d0a1a03f69b67b5ad9203011af26
Barsoum Adly <barsoumadly@gmail.com>
2025-03-31 18:26:17 +0200

    laboratory orderList

--- src/features/dashboard/laboratory/orders/LaboratoryOrderList.jsx
+++ src/features/dashboard/laboratory/orders/LaboratoryOrderList.jsx
@@ -43,6 +43,32 @@ function LaboratoryOrderList() {
               <div className="col-sm-12">
                 <div className="card card-table show-entire">
                   <div className="card-body">
+                    <div className="staff-search-table">
+                      <form>
+                        <div className="row">
+                          <div className="col-12 col-md-6 col-xl-4">
+                            <div className="input-block local-forms">
+                              <label>Patient Name </label>
+                              <input
+                                className="form-control"
+                                type="text"
+                                placeholder="ex: Smith"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-12 col-md-6 col-xl-4">
+                            <div className="doctor-submit">
+                              <button
+                                type="submit"
+                                className="btn btn-primary submit-list-form me-2"
+                              >
+                                Search
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
                     <div className="table-responsive">
                       {isLoading ? (
                         <LoadingSpinner />
