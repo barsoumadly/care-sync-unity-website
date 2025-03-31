5b1d6105595661a9d38d98fb14c86dcb3b0e206b
Barsoum Adly <barsoumadly@gmail.com>
2025-03-31 22:36:40 +0200

    laboratory service

--- src/services/laboratory.js
+++ src/services/laboratory.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/v1/laboratories";
+const API_URL = `${import.meta.env.VITE_API_URL}/laboratories`;
 
 const updateLaboratoryProfile = async function (laboratoryData) {
   const token = JSON.parse(localStorage.getItem("key"));
