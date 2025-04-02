952e51a1b5119f9d39dc68f62f31bf80e9a29ee2
bishoy labib <bishoylabib78@gmail.com>
2025-04-02 23:12:41 +0200

    AboutOfPatient

--- src/features/dashboard/doctor/Patients/Examination/AboutOfPatient.jsx
+++ src/features/dashboard/doctor/Patients/Examination/AboutOfPatient.jsx
@@ -1,4 +1,4 @@
-function AboutOfPatient() {
+function AboutOfPatient({ data }) {
   return (
     <div className="doctor-personals-grp">
       <div className="card">
@@ -6,24 +6,24 @@ function AboutOfPatient() {
           <div className="heading-detail ">
             <h4 className="mb-3">About me</h4>
             <p>
-              Hello I am Smith Bruklin a Gynaecologist in Sanjivni Hospital
-              Surat. I love to work with all my hospital staff and seniour
-              doctors.
+              Hello, my name is {data?.userId?.name}. I'm here today because I'd
+              like to establish care with this clinic. My contact information is{" "}
+              {data?.phone}.
             </p>
           </div>
           <div className="about-me-list">
             <ul className="list-space">
               <li>
-                <h4>Gender</h4>
-                <span>Male</span>
+                <h4>Blood Type</h4>
+                <span>{data?.bloodType}</span>
               </li>
               <li>
-                <h4>Operation Done</h4>
-                <span>30+</span>
+                <h4>Blood Pressure</h4>
+                <span>{data?.bloodPressure}</span>
               </li>
               <li>
-                <h4>Designation</h4>
-                <span>Engineer</span>
+                <h4>Blood Sugar</h4>
+                <span>{data?.bloodSugar}</span>
               </li>
             </ul>
           </div>
