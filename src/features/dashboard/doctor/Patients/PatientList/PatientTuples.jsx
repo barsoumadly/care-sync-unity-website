983f9987e2aa48be37d2510d246f86e73eb2ad17
bishoy labib <bishoylabib78@gmail.com>
2025-04-02 14:36:18 +0200

    Patients

--- src/features/dashboard/doctor/Patients/PatientList/PatientTuples.jsx
+++ src/features/dashboard/doctor/Patients/PatientList/PatientTuples.jsx
@@ -6,24 +6,29 @@ export default function PatientTuples({ num, element, button }) {
       <td></td>
 
       {/* Counter */}
-      <td style={{ padding: "0 30px" }}>{num + 1}</td>
+      <td style={{ padding: "0 20px" }}>{num + 1}</td>
 
       {/* Data */}
       <td>{element.name}</td>
       <td>{element.specialization}</td>
+      <td>{element.type}</td>
 
       {/* Button */}
       <td className="col-6 col-md-2 col-xl-3">
-        <div className="dropdown action-label">
-          <Link
-            to={`${button.link}/${element.id}/patient-profile`}
-            style={{ marginTop: "3%", color: "#fff" }}
-          >
-            <button className="btn btn-primary submit-list-form me-2">
-              {button.name}
-            </button>
-          </Link>
-        </div>
+        {element?.status !== "pending" ? (
+          <div className="dropdown action-label">
+            <Link
+              to={`${button.link}/${element.id}/patient-profile`}
+              style={{ marginTop: "3%", color: "#fff" }}
+            >
+              <button className="btn btn-primary submit-list-form me-2">
+                {button.name}
+              </button>
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
       </td>
     </>
   );
