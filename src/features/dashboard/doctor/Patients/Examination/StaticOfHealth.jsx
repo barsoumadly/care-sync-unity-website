8be90561e69c7dab5d479492a46d5db42b942074
bishoy labib <bishoylabib78@gmail.com>
2025-04-02 23:13:11 +0200

    StaticOfHealth

--- src/features/dashboard/doctor/Patients/Examination/StaticOfHealth.jsx
+++ src/features/dashboard/doctor/Patients/Examination/StaticOfHealth.jsx
@@ -1,4 +1,4 @@
-function StaticOfHealth() {
+function StaticOfHealth({ data }) {
   return (
     <div className="doctor-personals-grp">
       <div className="card">
@@ -9,31 +9,15 @@ function StaticOfHealth() {
           <div className="skill-blk">
             <div className="skill-statistics">
               <div className="skills-head">
-                <h5>Heart Beat</h5>
-                <p>45%</p>
-              </div>
-              <div className="progress mb-0">
-                <div
-                  className="progress-bar bg-operations"
-                  role="progressbar"
-                  style={{ width: "45%" }}
-                  aria-valuenow={45}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                />
-              </div>
-            </div>
-            <div className="skill-statistics">
-              <div className="skills-head">
-                <h5>Haemoglobin</h5>
-                <p>85%</p>
+                <h5>Temperature </h5>
+                <p>{data?.temperature} &deg;c</p>
               </div>
               <div className="progress mb-0">
                 <div
                   className="progress-bar bg-haemoglobin"
                   role="progressbar"
-                  style={{ width: "85%" }}
-                  aria-valuenow={85}
+                  style={{ width: "80%" }}
+                  aria-valuenow={90}
                   aria-valuemin={0}
                   aria-valuemax={100}
                 />
@@ -41,31 +25,48 @@ function StaticOfHealth() {
             </div>
             <div className="skill-statistics">
               <div className="skills-head">
-                <h5>Blood Pressure </h5>
-                <p>65%</p>
-              </div>
-              <div className="progress mb-0">
-                <div
-                  className="progress-bar bg-statistics"
-                  role="progressbar"
-                  style={{ width: "65%" }}
-                  aria-valuenow={65}
-                  aria-valuemin={0}
-                  aria-valuemax={100}
-                />
-              </div>
-            </div>
-            <div className="skill-statistics">
-              <div className="skills-head">
-                <h5>Sugar </h5>
-                <p>90%</p>
+                <h5>Height </h5>
+                <p>{data?.height}cm</p>
               </div>
               <div className="progress mb-0">
                 <div
                   className="progress-bar bg-visit"
                   role="progressbar"
-                  style={{ width: "90%" }}
+                  style={{ width: "70%" }}
                   aria-valuenow={90}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                />
+              </div>
+            </div>
+            <div className="skill-statistics">
+              <div className="skills-head">
+                <h5>Weight </h5>
+                <p>{data?.weight}kg</p>
+              </div>
+              <div className="progress mb-0">
+                <div
+                  className="progress-bar bg-operations"
+                  role="progressbar"
+                  style={{ width: `${data?.weight}%` }}
+                  aria-valuenow={90}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                />
+              </div>
+            </div>
+
+            <div className="skill-statistics">
+              <div className="skills-head">
+                <h5>Heart rate </h5>
+                <p>{data?.heartRate}%</p>
+              </div>
+              <div className="progress mb-0">
+                <div
+                  className="progress-bar bg-visit"
+                  role="progressbar"
+                  style={{ width: `${data?.heartRate}%` }}
+                  aria-valuenow={data?.heartRate}
                   aria-valuemin={0}
                   aria-valuemax={100}
                 />
