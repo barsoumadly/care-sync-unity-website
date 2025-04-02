fc234dd198f8d66925d8edb31428a2cbe5cc160a
bishoy labib <bishoylabib78@gmail.com>
2025-04-02 23:13:07 +0200

    ProfileHeader

--- src/features/dashboard/doctor/Patients/Examination/ProfileHeader.jsx
+++ src/features/dashboard/doctor/Patients/Examination/ProfileHeader.jsx
@@ -1,27 +1,31 @@
 import { Link } from "react-router-dom";
 
-function ProfileHeader() {
+function ProfileHeader({ data }) {
+  const year = new Date().getFullYear();
+  console.log(year);
+
   return (
     <div className="card">
       <div className="card-body">
         <div className="row">
           <div className="col-md-12">
             <div className="about-info">
-              <h4>
-                Patients Profile{" "}
-                <span>
-                  <a>
-                    <i className="feather-more-vertical" />
-                  </a>
-                </span>
-              </h4>
+              <h4>Patients Profile</h4>
             </div>
             <div className="doctor-profile-head">
               <div className="row">
                 <div className="col-lg-6 col-xl-4 col-md-4">
                   <div className="profile-user-box">
+                    <div className="profile-user-img" style={{ top: "0" }}>
+                      <img
+                        src={data?.userId?.profilePhoto?.url}
+                        style={{ width: "90px" }}
+                      />
+                      <div className="input-block doctor-up-files profile-edit-icon mb-0"></div>
+                    </div>
+
                     <div className="names-profiles">
-                      <h4>Smith Bruklin</h4>
+                      <h4>{data?.userId?.name}</h4>
                       <h5>Engineer</h5>
                     </div>
                   </div>
@@ -30,11 +34,11 @@ function ProfileHeader() {
                   <div className="follow-group">
                     <div className="doctor-follows">
                       <h5>Gender</h5>
-                      <h4>Female</h4>
+                      <h4>{data?.gender}</h4>
                     </div>
                     <div className="doctor-follows">
                       <h5>Age</h5>
-                      <h4>18</h4>
+                      <h4>{year - +data?.dateOfBirth.split("-")[0]}</h4>
                     </div>
                     <div className="doctor-follows">
                       <h5>specialty</h5>
