ba45695fcbcbc7059ebe3c3379ddc9003d563838
bishoy labib <bishoylabib78@gmail.com>
2025-04-02 14:36:24 +0200

    Patients

--- src/features/dashboard/doctor/Patients/PatientList/Patients.jsx
+++ src/features/dashboard/doctor/Patients/PatientList/Patients.jsx
@@ -4,35 +4,36 @@ import Table from "./Table";
 import PatientTuples from "./PatientTuples";
 import { useLocation } from "react-router-dom";
 import usePatientList from "./usePatientList";
-
-const patientList = [
-  {
-    id: 1,
-    name: "Andrea Lalema",
-    specialization: "Cardiology",
-  },
-  {
-    id: 2,
-    name: "Smith Bruklin",
-    specialization: "Neurology",
-  },
-  {
-    id: 3,
-    name: "Bernardo James	",
-    specialization: "Oncology",
-  },
-  {
-    id: 4,
-    name: "Bishoy Labib",
-    specialization: "Orthopedics",
-  },
-];
+import LoadingSpinner from "../../../../../ui/LoadingSpinner";
+import NoData from "../../NoData";
+// const patientList = [
+//   {
+//     id: 1,
+//     name: "Andrea Lalema",
+//     specialization: "Cardiology",
+//   },
+//   {
+//     id: 2,
+//     name: "Smith Bruklin",
+//     specialization: "Neurology",
+//   },
+//   {
+//     id: 3,
+//     name: "Bernardo James	",
+//     specialization: "Oncology",
+//   },
+//   {
+//     id: 4,
+//     name: "Bishoy Labib",
+//     specialization: "Orthopedics",
+//   },
+// ];
 
 function Patients() {
   const CLINIC_NAME = useLocation().pathname.split("/")[2];
   const CLINIC_ID = useLocation().pathname.split("/")[3];
-  const { data } = usePatientList(CLINIC_ID);
-  console.log(data);
+  const { data: patientList, isLoading } = usePatientList(CLINIC_ID);
+  console.log(patientList);
 
   return (
     <div className="main-wrapper">
@@ -54,31 +55,38 @@ function Patients() {
                   {/* Table Header */}
                   <SearchBar name="Patients List" />
                   {/* /Table Header */}
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th></th>
-                        <th>Num</th>
-                        <th>Name</th>
-                        <th>Specialization</th>
-                        <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {patientList.map((element, index) => (
-                        <tr key={element.id}>
-                          <PatientTuples
-                            num={index}
-                            element={element}
-                            button={{
-                              name: "Examination",
-                              link: `/doctor/${CLINIC_NAME}`,
-                            }}
-                          />
+                  {isLoading ? (
+                    <LoadingSpinner />
+                  ) : patientList && patientList?.[0]?.name ? (
+                    <Table>
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>No</th>
+                          <th>Name</th>
+                          <th>Specialization</th>
+                          <th>Type</th>
+                          <th></th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </Table>
+                      </thead>
+                      <tbody>
+                        {patientList?.map((element, index) => (
+                          <tr key={element.id}>
+                            <PatientTuples
+                              num={index}
+                              element={element}
+                              button={{
+                                name: "Examination",
+                                link: `/doctor/${CLINIC_NAME}`,
+                              }}
+                            />
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  ) : (
+                    <NoData name="appointments" />
+                  )}
                 </div>
               </div>
             </div>
