b4f77dd551561f277af6d8006eae0fc59fa39951
bishoy labib <bishoylabib78@gmail.com>
2025-04-05 16:48:13 +0200

    table

--- src/features/dashboard/doctor/Patients/PatientList/Table.jsx
+++ src/features/dashboard/doctor/Patients/PatientList/Table.jsx
@@ -1,9 +1,7 @@
-import { Link } from "react-router-dom";
-
 function Table({ children }) {
   return (
     <div className="table-responsive">
-      <table className="table border-0 custom-table comman-table datatable mb-0">
+      <table className="table mb-0 border-0 datatable custom-table">
         {children}
       </table>
     </div>
